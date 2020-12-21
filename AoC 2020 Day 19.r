4019ebe837178e6b4e92d3671cec23e9850f6cde
Paul Foster <paulfstr@gmail.com>
2020-12-21 11:51:47 +0000

    2020 day 19 WIP

--- AoC 2020 Day 19.r
+++ AoC 2020 Day 19.r
@@ -23,10 +23,6 @@ aaaabbb
 
 # COMMAND ----------
 
-input %>% str_split("\n\n") %>% unlist() %>% keep(2)
-
-# COMMAND ----------
-
 split_strs <- input %>% str_split("\n\n") %>% unlist() %>% map(read_lines)
 split_strs
 
@@ -58,7 +54,9 @@ nested_rules <-
     value = str_split(value, fixed(" | "))
   ) %>%
   unnest(value) %>%
-  mutate(value = str_split(value, " "))
+  mutate(
+    value = str_split(value, " ") %>% map(as.integer)
+  )
 nested_rules
 
 # COMMAND ----------
@@ -71,7 +69,7 @@ messages
 check_match <- function(message_str, check_rule_id) {
   if (check_rule_id %in% char_rules$rule_id) {
     match_char <- char_rules %>% filter(rule_id == check_rule_id) %>% pull(value)
-    return(list(
+    return(tibble(
       is_match = str_detect(message_str, match_char),
       message_str = str_replace(message_str, match_char, "")
     ))
@@ -88,6 +86,57 @@ check_match <- function(message_str, check_rule_id) {
     is_done <- TRUE
     
     for (individual_rule in unlist((slice(nested_rules_df, rule_group)$value))) {
+      
+      message(paste0(individual_rule, ": ", cur_message_str))
+      #debug <- paste0(individual_rule, ": ", cur_message_str)
+      
+      result <- check_match(cur_message_str, individual_rule)
+      
+      #debug <- paste0(debug, "; ", result$is_match) # This will display stuff out of order
+      #message(debug)
+      
+      if (result$is_match) {
+        cur_message_str <- result$message_str
+      } else {
+        is_done <- FALSE
+        break
+      }
+    }
+    if (nchar(cur_message_str) == 0 && is_done) {
+      return(tibble(
+        is_match = TRUE,
+        message_str = cur_message_str
+      ))
+    }
+  }
+  
+  tibble(
+    is_match = FALSE,
+    message_str = NA
+  )
+}
+# The issue is that cur_message_str is restoring characters when it shouldnt'. Not sure why though
+
+# COMMAND ----------
+
+# MAGIC %md I think a better approach is to just figure out how many a's etc for each rule.
+
+# COMMAND ----------
+
+check_match(messages$value[[1]], 0)
+
+# COMMAND ----------
+
+message_str <- messages$value[[1]]
+rule_group = 0
+message_str
+
+# COMMAND ----------
+
+cur_message_str <- message_str
+    is_done <- TRUE
+    
+    for (individual_rule in unlist((slice(nested_rules_df, rule_group)$value))) {
       result <- check_match(cur_message_str, individual_rule)
       
       if (result$is_match) {
@@ -98,19 +147,40 @@ check_match <- function(message_str, check_rule_id) {
       }
     }
     if (nchar(cur_message_str) == 0 && is_done) {
-      return(list(
+      return(tibble(
         is_match = TRUE,
         message_str = cur_message_str
       ))
     }
-  }
-  
-  list(
-    is_match = FALSE,
-    message_str = NA
-  )
-}
 
 # COMMAND ----------
 
-check_match(messages[[1]], 0)
+check_match(messages$value[[1]], 3)
+
+# COMMAND ----------
+
+message_str
+
+# COMMAND ----------
+
+nested_rules_df <- nested_rules %>% filter(rule_id %in% 0)
+
+# COMMAND ----------
+
+unlist((slice(nested_rules_df, 1)$value))
+
+# COMMAND ----------
+
+nested_rules_df
+
+# COMMAND ----------
+
+check_match(messages$value[[1]], 0)
+
+# COMMAND ----------
+
+result <- map_dfr(messages$value, check_match, check_rule_id = 0)
+
+# COMMAND ----------
+
+result
