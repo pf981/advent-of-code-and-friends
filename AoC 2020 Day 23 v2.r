2f2d4a56559a995188eb311ab33e63f180ee6ee4
Paul Foster <paulfstr@gmail.com>
2020-12-26 18:06:13 +0000

    Complete 2020 day 23

--- AoC 2020 Day 23 v2.r
+++ AoC 2020 Day 23 v2.r
@@ -29,22 +29,22 @@ struct Node {
 // [[Rcpp::export]]
 std::vector<int64_t> simulate_moves(std::vector<int64_t> cups, int64_t n_cups,int64_t n_moves) {
   // Create the circular linked list
-  std::vector<Node> nodes;
-  for (int64_t cup : cups) {
-    nodes.push_back({cup, nullptr});
-  }
-  for (int64_t i = nodes.size() + 1; i < n_cups; ++i) {
-    nodes.push_back({i, nullptr});
+  std::vector<Node> nodes(n_cups); // 1:n_cups
+  for (int64_t i = 1; i <= n_cups; ++i) {
+    nodes[i - 1] = {i, nullptr};
   }
 
-  Node *head = &nodes[0];
-  Node *tail = head;
-  for (int64_t i = 1; i < nodes.size(); ++i) {
-    tail->next = &nodes[i];
-    tail = tail->next;
+  Node *head = &nodes[cups[0] - 1];
+  for (auto cup_it = cups.begin() + 1; cup_it != cups.end(); ++cup_it) {
+    head->next = &nodes[*cup_it - 1];
+    head = head->next;
   }
-  tail->next = head;
-
+  for (int64_t i = cups.size() + 1; i <= n_cups; ++i) {
+    head->next = &nodes[i - 1];
+    head = head->next;
+  }
+  head->next = &nodes[cups[0] - 1];
+  head = head->next;
 
 
   // Do the moves
@@ -55,297 +55,37 @@ std::vector<int64_t> simulate_moves(std::vector<int64_t> cups, int64_t n_cups,in
     pick_up = head->next;
     head->next = pick_up->next->next->next;
 
-    dest = head->next;
-    for (Node* node = head->next->next; node != head; node = node->next) {
-      if (
-          (node->value > head->value && dest->value > head->value && node->value > dest->value)
-          || (node->value < head->value && dest->value < head->value && node->value > dest->value)
-          || (node->value < head->value && dest->value > head->value)
-        ) {
-        dest = node;
+    int64_t dest_value = head->value;
+    do {
+      dest_value = dest_value - 1;
+      if (dest_value == 0) {
+        dest_value = n_cups;
       }
-    }
+    } while (dest_value == pick_up->value || dest_value == pick_up->next->value || dest_value == pick_up->next->next->value);
+
+    dest = &nodes[dest_value - 1];
+
     pick_up->next->next->next = dest->next;
     dest->next = pick_up;
   }
 
 
-  // Return the result
-  // while (head->value != 1) {
-  //   head = head->next;
-  // }
-  // return head->next->value * head->next->next->value;
-
-  // debug
+  // Return the result in the correct order
   std::vector<int64_t> result;
-  Node* n = head;
+  while (head->value != 1) {
+    head = head->next;
+  }
   do {
-    result.push_back(n->value);
-    n = n->next;
-  } while(n != head);
+    result.push_back(head->next->value);
+    head = head->next;
+  } while(head->next->value != 1);
   return result;
 }
 ")
 
 # COMMAND ----------
 
-simulate_moves(
-  "389125467" %>% str_split("") %>% unlist() %>% as.numeric(),
-  nchar("389125467"),
-  10
-)
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-rotate <- function(x, n = 1) {
-  if (n == 0) x else c(tail(x, -n), head(x, n))
-}
-
-# COMMAND ----------
-
-simulate_moves <- function(cups, n_moves = 1, debug = FALSE) {
-  cur_i <- 1
-  for (move in seq_len(n_moves)) {
-    current_cup <- cups[cur_i]
-    picked_up <- cups[((cur_i + seq_len(3) - 1) %% length(cups)) + 1]
-
-    candidates <- cups[!(cups %in% picked_up)]
-    destination <- max(candidates[candidates < current_cup])
-    if (!is.finite(destination)) {
-      destination <- max(candidates)
-    }
-    destination_i <- which(candidates == destination)
-
-    if (debug) {
-      message(glue::glue('-- move {move} --
-cups: {paste0(cups, ifelse(seq_along(cups) == cur_i, "*", ""), collapse = " ")}
-pick up: {paste0(picked_up, collapse = " ")}
-destination: {destination} (i: {destination_i})
-
-'))
-    }
-    
-    cups <- append(candidates, picked_up, after = destination_i)
-    
-    # Need which(cups == current_cup) == cur_i
-    #cups <- rotate(cups, which(cups == current_cup) - cur_i)
-
-    #cur_i <- (cur_i %% length(cups)) + 1
-    cur_i <- (which(cups == current_cup) %% length(cups)) + 1
-  }
-  
-  cups
-}
-
-# COMMAND ----------
-
-# Rcpp::cppFunction("
-# struct Node {
-#     int64_t value;
-#     Node* next; // Node* next {nullptr};
-# };
-
-# // [[Rcpp::export]]
-# std::vector<int64_t> simulate_moves(std::vector<int64_t> cups, int64_t n_cups,int64_t n_moves) {
-#   // Create the circular linked list
-#   std::vector<Node> nodes;
-#   for (int64_t cup : cups) {
-#     nodes.push_back({cup, nullptr});
-#   }
-#   for (int64_t i = nodes.size() + 1; i < n_cups; ++i) {
-#     nodes.push_back({i, nullptr});
-#   }
-
-#   Node *head = &nodes[0];
-#   Node *tail = head;
-#   for (int64_t i = 1; i < nodes.size(); ++i) {
-#     tail->next = &nodes[i];
-#     tail = tail->next;
-#   }
-#   tail->next = head;
- 
-#   // debug
-#   std::vector<int64_t> result;
-#   Node* n = head;
-#   do {
-#     result.push_back(n->value);
-#     n = n->next;
-#   } while(n != head);
-#   return result;
-
-#   Node* pick_up;
-#   Node* dest;
-
-#   // Do the moves
-#   for (int64_t move = 0; move < n_moves; ++move) {
-#     pick_up = head->next;
-#     head->next = pick_up->next->next->next;
-
-#     dest = head->next;
-#     for (Node* node = head->next->next; node != head; node = node->next) {
-#       if (
-#           (node->value > head->value && node->value > dest->value)
-#           || (node->value < head->value && dest->value < head->value && node->value > dest->value)
-#         ) {
-#         dest = node;
-#       }
-#     }
-#     pick_up->next->next->next = dest->next;
-#     dest->next = pick_up;
-#   }
-
-#   while (head->value != 1) {
-#     head = head->next;
-#   }
-
-#   //std::vector<int64_t> result;
-#   while (head->next->value != 1) {
-#     result.push_back(head->value);
-#     head = head->next;
-#   }
-
-#   // return head->next->value * head->next->next->value;
-#   return result;
-# }
-# ")
-
-# COMMAND ----------
-
-# Rcpp::cppFunction("
-# struct Node {
-#     int64_t value;
-#     Node* next; // Node* next {nullptr};
-# };
-
-# // [[Rcpp::export]]
-# int64_t simulate_moves(std::vector<int64_t> cups, int64_t n_cups,int64_t n_moves) {
-#   // Create the circular linked list
-#   std::vector<Node> nodes;
-#   for (int64_t cup : cups) {
-#     nodes.push_back({cup, nullptr});
-#   }
-#   for (int64_t i = nodes.size() + 1; i < n_cups; ++i) {
-#     nodes.push_back({i, nullptr});
-#   }
-
-#   Node *head = &nodes[0];
-#   Node *tail = head;
-#   for (int64_t i = 1; i < nodes.size(); ++i) {
-#     tail->next = &nodes[i];
-#     tail = tail->next;
-#   }
-#   tail->next = head;
-
-#   Node* pick_up;
-#   Node* dest;
-
-#   // Do the moves
-#   for (int64_t move = 0; move < n_moves; ++move) {
-#     pick_up = head->next;
-#     head->next = pick_up->next->next->next;
-
-#     dest = head->next;
-#     for (Node* node = head->next->next; node != head; node = node->next) {
-#       if (
-#           (node->value > head->value && node->value > dest->value)
-#           || (node->value < head->value && dest->value < head->value && node->value > dest->value)
-#         ) {
-#         dest = node;
-#       }
-#     }
-#     pick_up->next->next->next = dest->next;
-#     dest->next = pick_up;
-#   }
-
-#   while (head->value != 1) {
-#     head = head->next;
-#   }
-
-#   return head->next->value * head->next->next->value;
-# }
-# ")
-
-# COMMAND ----------
-
-# Rcpp::cppFunction("
-# struct Node {
-#     int64_t value;
-#     Node* next; // Node* next {nullptr};
-# };
-
-# // [[Rcpp::export]]
-# int64_t simulate_moves(std::vector<int64_t> cups, int64_t n_cups,int64_t n_moves) {
-#   int64_t cur_i = 0;
-#   std::vector<Node> nodes;
-#   Node head = {int64_t(), nullptr};
-#   Node tail;
-
-#   //for (int64_t i = 0; i < cups.size(); ++i) {
-# //  for (int64_t cup : cups) {
-# //    // Node node = {cup, nullptr};
-# //Node node(cup);
-# //    nodes.push_back(node);
-# //  }
-#   //for (int64_t i = l.size() + 1; i < n_cups; ++i) {
-#   //  l.push_back(i);
-#   //}
-# //
-#   //for (int64_t move = 0; move < n_moves; ++move) {
-#   //  
-#   //}
-
-#   return cups[0];
-# }
-# ")
-# # simulate_moves <- function(cups, n_moves = 1, debug = FALSE) {
-# #   cur_i <- 1
-# #   for (move in seq_len(n_moves)) {
-# #     current_cup <- cups[cur_i]
-# #     picked_up <- cups[((cur_i + seq_len(3) - 1) %% length(cups)) + 1]
-
-# #     candidates <- cups[!(cups %in% picked_up)]
-# #     destination <- max(candidates[candidates < current_cup])
-# #     if (!is.finite(destination)) {
-# #       destination <- max(candidates)
-# #     }
-# #     destination_i <- which(candidates == destination)
-
-# #     if (debug) {
-# #       message(glue::glue('-- move {move} --
-# # cups: {paste0(cups, ifelse(seq_along(cups) == cur_i, "*", ""), collapse = " ")}
-# # pick up: {paste0(picked_up, collapse = " ")}
-# # destination: {destination} (i: {destination_i})
-
-# # '))
-# #     }
-    
-# #     cups <- append(candidates, picked_up, after = destination_i)
-    
-# #     # Need which(cups == current_cup) == cur_i
-# #     #cups <- rotate(cups, which(cups == current_cup) - cur_i)
-
-# #     #cur_i <- (cur_i %% length(cups)) + 1
-# #     cur_i <- (which(cups == current_cup) %% length(cups)) + 1
-# #   }
-  
-# #   cups
-# # }
-
-# COMMAND ----------
-
-simulate_moves(1:10, 5)
-
-# COMMAND ----------
-
-result <- simulate_moves(cups, 100)
-
-rotate(result, which(result == 1) - 1) %>%
-  tail(-1) %>%
-  paste0(collapse = "")
+simulate_moves(cups, length(cups), 100) %>% str_c(collapse = "")
 
 # COMMAND ----------
 
@@ -353,27 +93,80 @@ rotate(result, which(result == 1) - 1) %>%
 
 # COMMAND ----------
 
-result <- simulate_moves(
-  c(cups, seq(from = max(cups), to = 1000000, by = 1)),
-  10000000
-)
-
-# first_two <-
-#   rotate(result, which(result == 1) - 1) %>%
-#   tail(-1) %>%
-#   head(2)
-
-# first_two[1] * first_two[2]
+# result <- simulate_moves(cups, 100, 100)
 
 # COMMAND ----------
 
-result
+result <- simulate_moves(cups, 1000000, 10000000)
+result[1] * result[2]
 
 # COMMAND ----------
 
-first_two <-
-  rotate(result, which(result == 1) - 1) %>%
-  tail(-1) %>%
-  head(2)
+# Rcpp::cppFunction("
+# struct Node {
+#   int64_t value;
+#   Node* next;
+# };
 
-first_two[1] * first_two[2]
+# // [[Rcpp::export]]
+# std::vector<int64_t> simulate_moves(std::vector<int64_t> cups, int64_t n_cups,int64_t n_moves) {
+#   // Create the circular linked list
+#   std::vector<Node> nodes(n_cups); // 1:n_cups
+#   for (int64_t i = 1; i <= n_cups; ++i) {
+#     nodes[i - 1] = {i, nullptr};
+#   }
+
+#   Node *head = &nodes[cups[0] - 1];
+#   for (auto cup_it = cups.begin() + 1; cup_it != cups.end(); ++cup_it) {
+#     head->next = &nodes[*cup_it - 1];
+#     head = head->next;
+#   }
+#   for (int64_t i = cups.size() + 1; i <= n_cups; ++i) {
+#     head->next = &nodes[i - 1];
+#     head = head->next;
+#   }
+#   head->next = &nodes[cups[0] - 1];
+#   head = head->next;
+
+
+#   // Do the moves
+#   Node* pick_up;
+#   Node* dest;
+
+#   for (int64_t move = 0; move < n_moves; ++move, head = head->next) {
+#     pick_up = head->next;
+#     head->next = pick_up->next->next->next;
+
+#     int64_t dest_value = head->value;
+#     do {
+#       dest_value = dest_value - 1;
+#       if (dest_value == 0) {
+#         dest_value = n_cups;
+#       }
+#     } while (dest_value == pick_up->value || dest_value == pick_up->next->value || dest_value == pick_up->next->next->value);
+
+#     dest = &nodes[dest_value - 1];
+
+#     pick_up->next->next->next = dest->next;
+#     dest->next = pick_up;
+#   }
+
+
+#   // Return the result in the correct order
+#   while (head->value != 1) {
+#     head = head->next;
+#   }
+
+#   //return head->next->value * head->next->next->value;
+
+
+#   // DEBUG
+#   std::vector<int64_t> result;
+#   Node *node = head;
+#   do {
+#     result.push_back(node->value);
+#     node = node->next;
+#   } while(node != head);
+#   return result;
+# }
+# ")
