4cdb83d4e35760637478cd0bb36f58d4ae3b4d58
Paul Foster <paulfstr@gmail.com>
2020-12-27 09:08:08 +0000

    Complete 2020 day 17

--- AoC 2020 Day 17.r
+++ AoC 2020 Day 17.r
@@ -150,3 +150,71 @@ result
 # COMMAND ----------
 
 nrow(result)
+
+# COMMAND ----------
+
+# MAGIC %md ## Part 2
+
+# COMMAND ----------
+
+active <- active %>% mutate(w = 0)
+active
+
+# COMMAND ----------
+
+neighbors_offset <-
+  expand_grid(
+    delta_x = seq(from = -1, to = 1, by = 1),
+    delta_y = seq(from = -1, to = 1, by = 1),
+    delta_z = seq(from = -1, to = 1, by = 1),
+    delta_w = seq(from = -1, to = 1, by = 1)
+  ) %>%
+  filter(!(delta_x == 0 & delta_y == 0 & delta_z == 0 & delta_w == 0))
+neighbors_offset
+
+# COMMAND ----------
+
+get_neighbors <- function(active) {
+  active %>%
+    cross_join(neighbors_offset) %>%
+    transmute(
+      x = x + delta_x,
+      y = y + delta_y,
+      z = z + delta_z,
+      w = w + delta_w
+    )
+}
+
+# COMMAND ----------
+
+sim <- function(active) {
+  neighbors <- get_neighbors(active)
+  
+  new_active1 <- inner_join(
+    active,
+    neighbors %>% count(x, y, z, w) %>% filter(n %in% c(2, 3))
+  )
+  
+  new_active2 <- anti_join(
+    neighbors %>% count(x, y, z, w) %>% filter(n == 3),
+    active
+  )
+  
+  bind_rows(new_active1, new_active2) %>% distinct() %>% select(-n) # I don't think I need distinct but won't hurt
+}
+
+# COMMAND ----------
+
+print_state(sim(active) %>% mutate(z = paste(z, w)))
+
+# COMMAND ----------
+
+result <- active
+for (i in seq_len(6)) {
+  result <- sim(result)
+}
+result
+
+# COMMAND ----------
+
+nrow(result)
