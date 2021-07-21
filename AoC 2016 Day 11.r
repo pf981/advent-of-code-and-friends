d005fc4347e57b565568a0c020e67fc58fa65c96
Paul Foster <paulfstr@gmail.com>
2021-07-21 22:27:45 +0000

    Tidy 2016 day 11

--- AoC 2016 Day 11.r
+++ AoC 2016 Day 11.r
@@ -254,749 +254,102 @@ The fourth floor contains nothing relevant.
 
 # COMMAND ----------
 
-elements <- str_extract_all(input, "[\\w+]+(?=-compatible microchip| generator)") %>% unlist() %>% unique()
-elements
-
-# COMMAND ----------
-
-lookup <- seq_len(2 * length(elements))
-
-names(lookup) <- c(
-  str_c(elements, "-compatible microchip"),
-  str_c(elements, " generator")
-)
-
-floor_to_int <- function(floor) {
-  lookup[floor]
-}
-
-floors_to_int <- function(floors) map(floors, floor_to_int)
-
-# COMMAND ----------
-
-floors <-
+start_state <-
   read_lines(input) %>% 
   str_extract_all("[\\w+-]+( microchip| generator)") %>%
-  floors_to_int() %>%
-  map(sort)
+  enframe(name = "floor") %>%
+  unnest() %>%
+  separate(value, into = c("element", "item_type"), sep = " |-[a-z]+ ", extra = "merge") %>%
+  pivot_wider(names_from = item_type, values_from = floor) %>%
+  select(-element)
 
-floors
-
-# COMMAND ----------
-
-elevator_col <- 2 * length(elements) + 1
-microchip_col <- seq(from = 1, to = length(elements), by = 1)
-generator_col <- seq(from = length(elements) + 1, to = 2 * length(elements), by = 1)
-
-lst(elevator_col, microchip_col, generator_col)
+start_state
 
 # COMMAND ----------
 
-m_start <-
-  floors %>%
-  map(function(x) {
-    v <- rep(FALSE, 2 * length(elements))
-    v[x] <- TRUE
-    v
-  }) %>%
-  simplify2array() %>%
-  t() %>%
-  cbind(c(TRUE, rep(FALSE, nrow(.) - 1)))
-
-colnames(m_start) <- c(str_c(elements, "-M"), str_c(elements, "-G"), "E")
-rownames(m_start) <- str_c("F", seq_len(nrow(m_start)))
-m_start
-
-# COMMAND ----------
-
-apply(m_start, 1, function(x) str_c(as.integer(x), collapse = "")) %>% str_c(collapse = "\n") %>% cat()
-
-# COMMAND ----------
-
-str_length("111100011110001")
-
-# COMMAND ----------
-
-Rcpp::sourceCpp(
-  code = '
-#include <algorithm>
-#include <bitset>
-#include <set>
-#include <string>
-#include <vector>
-
-struct State {
-    std::bitset<7> microchips[4];
-    std::bitset<7> generators[4];
-    int elevator;
-    int moves;
-};
-
-bool is_valid(const State& state) {
-    for (int i = 0; i < 4; ++i) {
-      if ((state.microchips[i] & ~state.generators[i]).any() && state.generators[i].any()) {
-          return false;
-      }
-    }
-    return true;
-}
-
-bool is_solved(const State& state) {
-    return state.microchips[3].all() && state.generators[3].all();
-}
-
-ulong hash(const State& state) {
-    // Dont include moves in hash
-    ulong result = state.elevator;
-    ulong multiplier = 1;
-    for (int i = 0; i < 4; ++i) {
-        multiplier *= 256;
-        result += multiplier * state.microchips[i].to_ulong();
-        multiplier *= 256;
-        result += multiplier * state.generators[i].to_ulong();
-    }
-    return result;
-}
-
-bool itemExists(const State& s, int elevator, int item) {
-    if (item < 7) {
-        return s.microchips[elevator][item];
-    }
-    if (item < 14) {
-        return s.generators[elevator][item - 7];
-    }
-    if (item == 14) {
-        return true; // 14 means no item
-    }
-}
-
-void setItem(State& s, int elevator, int item, bool item_value) {
-    if (item < 7) {
-        s.microchips[elevator].set(item, item_value);
-    }
-    else if (item < 14) {
-        s.generators[elevator].set(item - 7, item_value);
-    }
-    else if (item == 14) {
-        // Do nothing. 14 means there is no item
-    }
-}
-
-// [[Rcpp::export]]
-int solve_cpp() 
-{
-    State start_state = {
-      {
-          std::bitset<7>("11110001"),
-          std::bitset<7>("00001110"),
-          std::bitset<7>("00000000"),
-          std::bitset<7>("00000000")
-      },
-      {
-          std::bitset<7>("11110001"),
-          std::bitset<7>("00001110"),
-          std::bitset<7>("00000000"),
-          std::bitset<7>("00000000")
-      },
-      0,
-      0
-    };
-
-    
-    auto cmp = [](const State& left, const State& right) { return left.moves < right.moves; };
-
-    std::vector<State> states;
-    states.push_back(start_state);
-    std::push_heap(states.begin(), states.end(), cmp);
-    std::set<ulong > done_states;
-    
-    while (true) {
-        State s = states.front();
-        std::pop_heap(states.begin(), states.end(), cmp);
-        states.pop_back(); // Actually remove the element
-
-        for (int new_elevator : {s.elevator - 1, s.elevator + 1}) {
-            if (new_elevator < 0 || new_elevator >= 4) continue;
-            for (int item1 = 0; item1 < 14; ++item1) {
-                if (!itemExists(s, new_elevator, item1)) continue;
-                for (int item2 = item1 + 1; item2 <= 14; ++item2) {
-                    if (!itemExists(s, new_elevator, item2)) continue;
-                
-                    State new_state(s);
-                    new_state.elevator = new_elevator;
-                    new_state.moves = new_state.moves + 1;
-                    setItem(new_state, s.elevator, item1, false);
-                    setItem(new_state, s.elevator, item2, false);
-                    setItem(new_state, new_state.elevator, item1, true);
-                    setItem(new_state, new_state.elevator, item2, true);
-
-                    ulong new_hash = hash(new_state);
-                    if (!done_states.count(new_hash)) continue;
-                    done_states.insert(new_hash);
-
-                    if (!is_valid(new_state)) continue;
-
-                    if (is_solved(new_state)) return new_state.moves;
-      
-                    states.push_back(new_state);
-                    std::push_heap(states.begin(), states.end(), cmp);
-                }
-            }
-        }
-    }
-
-    return -1;
-} 
-')
-
-# COMMAND ----------
-
-solve_cpp()
-
-# COMMAND ----------
-
-# Ideas for R - check the hash first.
-# Maybe if you store it as
-# element1 = c(chip_floor, generator_floor)
-# 
-# The you can sort 1, 2 to get hash - so order of elements doesn't matter
-
-# COMMAND ----------
-
-Rcpp::sourceCpp(
-  code = '
-#include <algorithm>
-#include <bitset>
-#include <set>
-#include <string>
-#include <vector>
-
-struct State {
-    std::bitset<7> microchips[4];
-    std::bitset<7> generators[4];
-    int elevator;
-    int moves;
-};
-
-bool is_valid(const State& state) {
-    for (int i = 0; i < 4; ++i) {
-      if ((state.microchips[i] & ~state.generators[i]).any() && state.generators[i].any()) {
-          return false;
-      }
-    }
-    return true;
-}
-
-ulong hash(const State& state) {
-    // Dont include moves in hash
-    ulong result = state.elevator;
-    ulong multiplier = 1;
-    for (int i = 0; i < 4; ++i) {
-        multiplier *= 256;
-        result += multiplier * state.microchips[i].to_ulong();
-        multiplier *= 256;
-        result += multiplier * state.generators[i].to_ulong();
-    }
-    return result;
-}
-
-// std::bitset::reference getItem(State& s, int elevator, int item) {
-//     if (item < 7) {
-//         return s.microchips[elevator][item];
-//     }
-//     if (item < 14) {
-//         return s.generators[elevator][item - 7];
-//     }
-//     return ??? I think I need optional?
-// }
-
-// bool itemExists(const State& s, int elevator, int item) {
-//     if (item < 7) {
-//         return s.microchips[elevator][item];
-//     }
-//     if (item < 14) {
-//         return s.generators[elevator][item - 7];
-//     }
-//     if (item == 14) {
-//         return true; // 14 means no item
-//     }
-// }
-
-// [[Rcpp::export]]
-int solve_cpp() 
-{
-    State start_state = {
-      {
-          std::bitset<7>("11110001"),
-          std::bitset<7>("00001110"),
-          std::bitset<7>("00000000"),
-          std::bitset<7>("00000000")
-      },
-      {
-          std::bitset<7>("11110001"),
-          std::bitset<7>("00001110"),
-          std::bitset<7>("00000000"),
-          std::bitset<7>("00000000")
-      },
-      0,
-      0
-    };
-
-    std::vector<State> states;
-    states.push_back(start_state);
-    std::push_heap(states.begin(), states.end(), [](const State& left, const State& right) { return left.moves < right.moves; });
-    std::set<ulong > done_states;
-    
-    while (true) {
-        State s = states.front();
-        std::pop_heap(states.begin(), states.end());
-
-        // for (int new_elevator : {s.elevator - 1, s.elevator + 1}) {
-        //     if (new_elevator < 0 || new_elevator >= 4) continue;
-        //     for (int item1 = 0; item1 < 14; ++item1) {
-        //         if (!itemExists(s, new_elevator, item1)) continue; // Check if item1 exists
-        //         for (int item2 = item1 + 1; item2 <= 14; ++item2) {
-        //             if (!itemExists(s, new_elevator, item2)) continue; // Check if item2 exists
-        //         
-        //         }
-        //     }
-        // }
-
-        break;
-    }
-
-    return 0;
-} 
-')
-
-# COMMAND ----------
-
-Rcpp::sourceCpp(
-  code = '
-#include <bitset>
-#include <string>
-#include <set>
-#include <vector>
-
-struct State {
-    std::bitset<7> microchips[4];
-    std::bitset<7> generators[4];
-    int elevator;
-};
-
-bool is_valid(const State& state) {
-    for (int i = 0; i < 4; ++i) {
-      //if ((state.microchips[i] & state.generators[i].flip()).any() && state.generators[i].any()) {
-      std::bitset<7> not_generators(state.generators);
-      not_generators.flip();
-      if ((state.microchips[i] & not_generators).any() && state.generators[i].any()) {
-          return false;
-      }
-    }
-    return true;
-}
-
-ulong hash(const State& state) {
-    ulong result = state.elevator;
-    ulong multiplier = 1;
-    for (int i = 0; i < 4; ++i) {
-        multiplier *= 256;
-        result += multiplier * state.microchips[i].to_ulong();
-        multiplier *= 256;
-        result += multiplier * state.generators[i].to_ulong();
-    }
-    return result;
-}
-
-// [[Rcpp::export]]
-int solve_cpp() 
-{
-    std::bitset<7> microchips[] = {
-        std::bitset<7>("11110001"),
-        std::bitset<7>("00001110"),
-        std::bitset<7>("00000000"),
-        std::bitset<7>("00000000")
-    };
-    std::bitset<7> generators[] = {
-        std::bitset<7>("11110001"),
-        std::bitset<7>("00001110"),
-        std::bitset<7>("00000000"),
-        std::bitset<7>("00000000")
-    };
-
-    std::set<ulong > done_states;
-    // done_states.insert()
-
-    State state = {
-      {
-          std::bitset<7>("11110001"),
-          std::bitset<7>("00001110"),
-          std::bitset<7>("00000000"),
-          std::bitset<7>("00000000")
-      },
-      {
-          std::bitset<7>("11110001"),
-          std::bitset<7>("00001110"),
-          std::bitset<7>("00000000"),
-          std::bitset<7>("00000000")
-      },
-      0
-    };
-
-
-    int elevator = 0;
-    // return is_valid(microchips, generators);
-    // return hash(state);
-    return is_valid(state);
-} 
-')
-
-# COMMAND ----------
-
-Rcpp::sourceCpp(
-  code = '
-#include <bitset>
-#include <string>
-#include <set>
-#include <vector>
-
-bool is_valid(std::bitset<7> microchips[4], std::bitset<7> generators[4]) {
-    for (int i = 0; i < 4; ++i) {
-      if ((microchips[i] & generators[i].flip()).any() && generators[i].any()) {
-          return false;
-      }
-    }
-    return true;
-}
-
-ulong hash(std::bitset<7> microchips[4], std::bitset<7> generators[4], int elevator) {
-    ulong result = elevator;
-    ulong multiplier = 1;
-    for (int i = 0; i < 4; ++i) {
-        multiplier *= 256;
-        result += multiplier * microchips[i].to_ulong();
-        multiplier *= 256;
-        result += multiplier * generators[i].to_ulong();
-    }
-    return result;
-}
-
-// [[Rcpp::export]]
-int solve_cpp() 
-{
-    std::bitset<7> microchips[] = {
-        std::bitset<7>("11110001"),
-        std::bitset<7>("00001110"),
-        std::bitset<7>("00000000"),
-        std::bitset<7>("00000000")
-    };
-    std::bitset<7> generators[] = {
-        std::bitset<7>("11110001"),
-        std::bitset<7>("00001110"),
-        std::bitset<7>("00000000"),
-        std::bitset<7>("00000000")
-    };
-
-    std::set<ulong > done_states;
-    // done_states.insert()
-
-
-    int elevator = 0;
-    // return is_valid(microchips, generators);
-    return hash(microchips, generators, elevator);
-} 
-')
-
-# COMMAND ----------
-
-solve_cpp()
-
-# COMMAND ----------
-
-# MAGIC %md ## Scratch
-
-# COMMAND ----------
-
-Rcpp::sourceCpp(
-  code = '
-#include <bitset>
-#include <string>
-#include <vector>
-
-int solve_cpp() 
-{
-    std::bitset<15> state[1] = {std::bitset<15>("111100011110001")};
-    return 1;
-} 
-')
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-Rcpp::sourceCpp(
-  code = '
-#include <bitset>
-#include <string>
-#include <vector>
-
-int solve_cpp() 
-{
-    std::bitset<15> state("111100011110001");
-    return 1;
-} 
-')
-
-# COMMAND ----------
-
-Rcpp::sourceCpp(
-  code = '
-#include <bitset>
-#include <string>
-#include <vector>
-
-int solve_cpp() 
-{
-    std::bitset<15> state(std::string("111100011110001"));
-    return 1;
-} 
-')
-
-# COMMAND ----------
-
-Rcpp::sourceCpp(
-  code = '
-#include <bitset>
-#include <string>
-#include <vector>
-
-int solve_cpp() 
-{
-    std::bitset<15> state = std::string("111100011110001");
-    return 1;
-} 
-')
-
-# COMMAND ----------
-
-Rcpp::sourceCpp(
-  code = '
-#include <bitset>
-#include <vector>
-
-int solve_cpp() 
-{
-    std::bitset<15> state[4] = {};
-    return 1;
-} 
-')
-
-# COMMAND ----------
-
-Rcpp::sourceCpp(
-  code = '
-#include <bitset>
-#include <vector>
-
-int solve_cpp() 
-{
-    std::bitset<15> state[] = {
-        std::string("111100011110001")
-    };
-    return 1;
-} 
-')
-
-# COMMAND ----------
-
-Rcpp::sourceCpp(
-  code = '
-#include <bitset>
-#include <vector>
-
-int solve_cpp() 
-{
-    std::bitset<15> state[] = {
-        "111100011110001",
-        "000001100001110",
-        "000010000000000",
-        "000000000000000"
-    };
-    return 1;
-} 
-')
-
-# COMMAND ----------
-
-Rcpp::sourceCpp(
-  code = '
-#include <bitset>
-#include <vector>
-
-int solve_cpp() 
-{
-    std::bitset<15> state[] = {
-        {"111100011110001"},
-        {"000001100001110"},
-        {"000010000000000"},
-        {"000000000000000"}
-    };
-    return 1;
-} 
-')
-
-# COMMAND ----------
-
-Rcpp::sourceCpp(
-  code = '
-#include <bitset>
-#include <vector>
-
-// [[Rcpp::export]]
-int solve_cpp() 
-{
-    std::vector<std::bitset<15> > state{{"111100011110001"}, {"000001100001110"}, {"000010000000000"}, {"000000000000000"}};
-    return 1;
-} 
-')
-
-# COMMAND ----------
-
-Rcpp::sourceCpp
-
-# COMMAND ----------
-
-Rcpp::cppFunction('
-int solve_cpp() 
-{
-    std::bitset<15> a;
-    return 1;
-} 
-')
-
-# COMMAND ----------
-
-Rcpp::cppFunction(
-  include = "bitset",
-'
-int solve_cpp() 
-{
-    std::vector<std::bitset<15> > state{"111100011110001", "000001100001110", "000010000000000", "000000000000000"};
-    return 1;
-} 
-')
-
-# COMMAND ----------
-
-?cppFunction
-
-# COMMAND ----------
-
-std::bitset<4> b3{"0011"}
-
-# COMMAND ----------
-
-# answer <- solve()
-# answer # This took ?? hrs
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-# MAGIC %md ## Try c++
-
-# COMMAND ----------
-
-Rcpp::cppFunction('
-int solve_cpp(int64_t n, std::vector<int64_t> init) 
-{ 
-    std::unordered_map<int64_t, int64_t> hashtable;
-    int64_t v_prev = init.back();
-    int64_t v;
-    
-    for (int i = 1; i <= init.size(); ++i) {
-      hashtable[init[i - 1]] = i;
-    }
-    
-    for (int i = init.size() + 1; i <= n; ++i) {
-      v = hashtable[v_prev] ? i - 1 - hashtable[v_prev] : 0;
-      hashtable[v_prev] = i - 1;
-      v_prev = v;
-    }
-    
-    return v;
-} 
-')
-
-# COMMAND ----------
-
-is_valid <- function(m) {
-  microchips <- m[,microchip_col]
-  generators <- m[,generator_col]
-
-  # Microchips without a corresponding generator
-  exposed_microchips <- microchips & !generators
-
-  # Filtered to floors that contain a generator
-  !any(exposed_microchips[as.logical(apply(generators, 1, FUN=max)),])
-}
-
-hash_m <- function(m) str_c(m, collapse = "")
-
-solve <- function(m = m_start) {
-  ms <- list(m)
-  n_moves <- c(0)
+is_valid <- function(state) {
+  floors_with_exposed_chip <-
+    state %>%
+    filter(microchip != generator) %>%
+    pull(microchip) %>%
+    unique()
   
-  done_states <- map_chr(ms, hash_m)
+  floors_with_generator <-
+    state %>%
+    pull(generator) %>%
+    unique()
+  
+  !any(floors_with_exposed_chip %in% floors_with_generator)
+}
+
+hash <- function(state, elevator) {
+  list(
+    state = state %>% arrange(microchip, generator), # Sorting the state means that element names are interchangable - dramatically reducing the state space
+    elevator = elevator
+  ) %>%
+    digest::digest()
+}
+
+solve <- function(state = start_state) {
+  states <- list(state)
+  n_moves <- c(0)
+  elevators <- c(1)
+  
+  done_states <- c(hash(state, 1))
   
   repeat {
     # Choose the state with the least moves
     i <- which.min(n_moves)
-    m <- ms[[i]]
+    state <- states[[i]]
     n_move <- n_moves[[i]]
+    elevator <- elevators[[i]]
     
-    ms[[i]] <- NULL
+    states[[i]] <- NULL
     n_moves <- n_moves[-i]
-
+    elevators <- elevators[-i]
 
     # All possible moves
-    elevator <- which(m[, elevator_col])
-    possble_items <- which(m[elevator, -elevator_col])
+    possible_items <- which(state == elevator, arr.ind = TRUE) %>% asplit(1)
     for (new_elevator in c(elevator + 1, elevator - 1)) {
-      if (new_elevator < 1 || new_elevator > nrow(m)) {
+      if (new_elevator < 1 || new_elevator > 4) {
         next
       }
       
-      for (item1 in possble_items) {
-        for (item2 in c(0, possble_items[possble_items > item1])) {
-          items <- c(item1, item2, elevator_col)
+      i_possible <- seq_along(possible_items)
+      for (item1 in c(0, i_possible)) {
+        for (item2 in i_possible[i_possible > item1]) {
+          new_state <- state
+          for (item in c(item1, item2)) {
+            if (item > 0) {
+              new_state[possible_items[[item]][["row"]], possible_items[[item]][["col"]]] <- new_elevator
+            }
+          }
           
-          new_m <- m
-          new_m[elevator, items] <- FALSE
-          new_m[new_elevator, items] <- TRUE
-          
-          if (!is_valid(m)) {
+          state_hash <- hash(new_state, new_elevator)
+          if (state_hash %in% done_states) {
             next
           }
           
+          if (!is_valid(new_state)) {
+            next
+          }
           
           # Check if the problem is solved
-          if (all(new_m[nrow(new_m),])) {
+          if (all(new_state == 4)) {
             return(n_move + 1)
           }
           
-          state_hash <- hash_m(new_m)
-          if (!(state_hash %in% done_states)) { # If the state hasn't been reached before
-            ms <- c(ms, list(new_m))
-            n_moves <- c(n_moves, n_move + 1)
-            done_states <- c(done_states, state_hash)
-          }
+          states <- c(states, list(new_state))
+          n_moves <- c(n_moves, n_move + 1)
+          elevators <- c(elevators, new_elevator)
+          done_states <- c(done_states, state_hash)
         }
       }
     }
   }
 }
+
+# COMMAND ----------
+
+answer <- solve()
+answer # 1.6 hrs
