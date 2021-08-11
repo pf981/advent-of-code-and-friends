0badda23b236b71b899ea3c9cf28aa56855a8d13
Paul Foster <paulfstr@gmail.com>
2021-08-11 08:59:53 +0000

    Tidy 2019 day 2

--- AoC 2019 Day 02.r
+++ AoC 2019 Day 02.r
@@ -43,39 +43,30 @@
 
 # COMMAND ----------
 
-input <- "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,19,9,23,1,5,23,27,1,27,9,31,1,6,31,35,2,35,9,39,1,39,6,43,2,9,43,47,1,47,6,51,2,51,9,55,1,5,55,59,2,59,6,63,1,9,63,67,1,67,10,71,1,71,13,75,2,13,75,79,1,6,79,83,2,9,83,87,1,87,6,91,2,10,91,95,2,13,95,99,1,9,99,103,1,5,103,107,2,9,107,111,1,111,5,115,1,115,5,119,1,10,119,123,1,13,123,127,1,2,127,131,1,131,13,0,99,2,14,0,0
-"
+library(tidyverse)
 
 # COMMAND ----------
 
-library("tidyverse")
+input <- "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,19,9,23,1,5,23,27,1,27,9,31,1,6,31,35,2,35,9,39,1,39,6,43,2,9,43,47,1,47,6,51,2,51,9,55,1,5,55,59,2,59,6,63,1,9,63,67,1,67,10,71,1,71,13,75,2,13,75,79,1,6,79,83,2,9,83,87,1,87,6,91,2,10,91,95,2,13,95,99,1,9,99,103,1,5,103,107,2,9,107,111,1,111,5,115,1,115,5,119,1,10,119,123,1,13,123,127,1,2,127,131,1,131,13,0,99,2,14,0,0"
 
 # COMMAND ----------
 
 instructions_original <-
   input %>%
-  read_csv(col_names = FALSE) %>%
-  unlist(use.names = FALSE)
+  str_split(",") %>%
+  first() %>%
+  parse_integer()
+instructions_original
+
+# COMMAND ----------
 
 instructions <- instructions_original
-
-# COMMAND ----------
-
-instructions
-
-# COMMAND ----------
-
-# MAGIC %md > before running the program, replace position 1 with the value 12 and replace position 2 with the value 2. What value is left at position 0 after the program halts?
-
-# COMMAND ----------
-
 instructions[[1 + 1]] <- 12
 instructions[[2 + 1]] <- 2
 
 # COMMAND ----------
 
 i <- 1
-
 while (instructions[[i]] != 99) {
   op_code <- instructions[[i]]
   fn <- if (op_code == 1) {
@@ -89,14 +80,8 @@ while (instructions[[i]] != 99) {
   
   i <- i + 4
 }
-
-# COMMAND ----------
-
-instructions
-
-# COMMAND ----------
-
-instructions[[1]]
+answer <- instructions[[1]]
+answer
 
 # COMMAND ----------
 
@@ -115,36 +100,29 @@ instructions[[1]]
 
 run_instructions <- function(instructions) {
   i <- 1
-
   while (instructions[[i]] != 99) {
     op_code <- instructions[[i]]
-    fn <- if (op_code == 1) {
-      `+`
-    } else if (op_code == 2) {
-      `*`
-    } else {
-      stop(paste0('Invalid op code: ', op_code, ' at position ', i))
-    }
-    instructions[[instructions[[i + 3]] + 1]] <- fn(instructions[[instructions[[i + 1]] + 1]], instructions[[instructions[[i + 2]] + 1]])
+    fn <- list(`+`, `*`)[[op_code]]
+    
+    instructions[[instructions[[i + 3]] + 1]] <- fn(
+      instructions[[instructions[[i + 1]] + 1]],
+      instructions[[instructions[[i + 2]] + 1]]
+    )
 
     i <- i + 4
   }
   instructions
 }
 
-run_instructions_modified <- function(instructions, pos_1, pos_2) {
-  instructions[[1 + 1]] <- pos_1
-  instructions[[2 + 1]] <- pos_2
-  run_instructions(instructions)
-}
-
-# COMMAND ----------
-
-find_solution <- function() {
+find_solution <- function(instructions_original) {
   for (pos_1 in seq(from = 0, to = 99)) {
     for (pos_2 in seq(from = 0, to = 99)) {
       instructions <- instructions_original
-      if (run_instructions_modified(instructions, pos_1, pos_2)[[1]] == 19690720) {
+      instructions[[1 + 1]] <- pos_1
+      instructions[[2 + 1]] <- pos_2
+      
+      result <- run_instructions(instructions)
+      if (result[[1]] == 19690720) {
         return(list(pos_1, pos_2))
       }
     }
@@ -153,10 +131,6 @@ find_solution <- function() {
 
 # COMMAND ----------
 
-solution <- find_solution()
-
-solution
-
-# COMMAND ----------
-
-100 * solution[[1]] + solution[[2]]
+solution <- find_solution(instructions_original)
+answer <- 100 * solution[[1]] + solution[[2]]
+answer
