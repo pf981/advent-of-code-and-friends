361b580b6f73d40c9758f04228d3af541a8ba257
Paul Foster <paulfstr@gmail.com>
2021-08-15 10:15:11 +0000

    Tidy 2020 day 10

--- AoC 2020 Day 10.r
+++ AoC 2020 Day 10.r
@@ -1,9 +1,9 @@
 # Databricks notebook source
 # MAGIC %md https://adventofcode.com/2020/day/10
-# MAGIC 
-# MAGIC <main>
-# MAGIC <script>window.addEventListener('click', function(e,s,r){if(e.target.nodeName==='CODE'&&e.detail===3){s=window.getSelection();s.removeAllRanges();r=document.createRange();r.selectNodeContents(e.target);s.addRange(r);}});</script>
-# MAGIC <article class="day-desc"><h2>--- Day 10: Adapter Array ---</h2><p>Patched into the aircraft's data port, you discover weather forecasts of a massive tropical storm. Before you can figure out whether it will impact your vacation plans, however, your device suddenly turns off!</p>
+
+# COMMAND ----------
+
+# MAGIC %md <article class="day-desc"><h2>--- Day 10: Adapter Array ---</h2><p>Patched into the aircraft's data port, you discover weather forecasts of a massive tropical storm. Before you can figure out whether it will impact your vacation plans, however, your device suddenly turns off!</p>
 # MAGIC <p>Its battery is dead.</p>
 # MAGIC <p>You'll need to plug it in. There's only one problem: the charging outlet near your seat produces the wrong number of <em>jolts</em>. Always prepared, you make a list of all of the joltage adapters in your bag.</p>
 # MAGIC <p>Each of your joltage adapters is rated for a specific <em>output joltage</em> (your puzzle input). Any given adapter can take an input <code>1</code>, <code>2</code>, or <code>3</code> jolts <em>lower</em> than its rating and still produce its rated output joltage.</p>
@@ -73,60 +73,6 @@
 # MAGIC <p>In this larger example, in a chain that uses all of the adapters, there are <em><code>22</code></em> differences of 1 jolt and <em><code>10</code></em> differences of 3 jolts.</p>
 # MAGIC <p>Find a chain that uses all of your adapters to connect the charging outlet to your device's built-in adapter and count the joltage differences between the charging outlet, the adapters, and your device. <em>What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?</em></p>
 # MAGIC </article>
-# MAGIC <p>Your puzzle answer was <code>2201</code>.</p><article class="day-desc"><h2 id="part2">--- Part Two ---</h2><p>To completely determine whether you have enough adapters, you'll need to figure out how many different ways they can be arranged. Every arrangement needs to connect the charging outlet to your device. The previous rules about when adapters can successfully connect still apply.</p>
-# MAGIC <p>The first example above (the one that starts with <code>16</code>, <code>10</code>, <code>15</code>) supports the following arrangements:</p>
-# MAGIC <pre><code>(0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
-# MAGIC (0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)
-# MAGIC (0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)
-# MAGIC (0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)
-# MAGIC (0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)
-# MAGIC (0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
-# MAGIC (0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
-# MAGIC (0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
-# MAGIC </code></pre>
-# MAGIC <p>(The charging outlet and your device's built-in adapter are shown in parentheses.) Given the adapters from the first example, the total number of arrangements that connect the charging outlet to your device is <em><code>8</code></em>.</p>
-# MAGIC <p>The second example above (the one that starts with <code>28</code>, <code>33</code>, <code>18</code>) has many arrangements. Here are a few:</p>
-# MAGIC <pre><code>(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
-# MAGIC 32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 48, 49, (52)
-# MAGIC 
-# MAGIC (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
-# MAGIC 32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 49, (52)
-# MAGIC 
-# MAGIC (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
-# MAGIC 32, 33, 34, 35, 38, 39, 42, 45, 46, 48, 49, (52)
-# MAGIC 
-# MAGIC (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
-# MAGIC 32, 33, 34, 35, 38, 39, 42, 45, 46, 49, (52)
-# MAGIC 
-# MAGIC (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
-# MAGIC 32, 33, 34, 35, 38, 39, 42, 45, 47, 48, 49, (52)
-# MAGIC 
-# MAGIC (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
-# MAGIC 46, 48, 49, (52)
-# MAGIC 
-# MAGIC (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
-# MAGIC 46, 49, (52)
-# MAGIC 
-# MAGIC (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
-# MAGIC 47, 48, 49, (52)
-# MAGIC 
-# MAGIC (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
-# MAGIC 47, 49, (52)
-# MAGIC 
-# MAGIC (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
-# MAGIC 48, 49, (52)
-# MAGIC </code></pre>
-# MAGIC <p>In total, this set of adapters can connect the charging outlet to your device in <em><code>19208</code></em> distinct arrangements.</p>
-# MAGIC <p>You glance back down at your bag and try to remember why you brought so many adapters; there must be <em>more than a trillion</em> valid ways to arrange them! Surely, there must be <span title="Definitely itertools.">an efficient way</span> to count the arrangements.</p>
-# MAGIC <p><em>What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?</em></p>
-# MAGIC </article>
-# MAGIC <p>Your puzzle answer was <code>169255295254528</code>.</p><p class="day-success">Both parts of this puzzle are complete! They provide two gold stars: **</p>
-# MAGIC <p>At this point, you should <a href="/2020">return to your Advent calendar</a> and try another puzzle.</p>
-# MAGIC <p>If you still want to see it, you can <a href="10/input" target="_blank">get your puzzle input</a>.</p>
-# MAGIC <p>You can also <span class="share">[Share<span class="share-content">on
-# MAGIC   <a href="https://twitter.com/intent/tweet?text=I%27ve+completed+%22Adapter+Array%22+%2D+Day+10+%2D+Advent+of+Code+2020&amp;url=https%3A%2F%2Fadventofcode%2Ecom%2F2020%2Fday%2F10&amp;related=ericwastl&amp;hashtags=AdventOfCode" target="_blank">Twitter</a>
-# MAGIC   <a href="javascript:void(0);" onclick="var mastodon_instance=prompt('Mastodon Instance / Server Name?'); if(typeof mastodon_instance==='string' &amp;&amp; mastodon_instance.length){this.href='https://'+mastodon_instance+'/share?text=I%27ve+completed+%22Adapter+Array%22+%2D+Day+10+%2D+Advent+of+Code+2020+%23AdventOfCode+https%3A%2F%2Fadventofcode%2Ecom%2F2020%2Fday%2F10'}else{return false;}" target="_blank">Mastodon</a></span>]</span> this puzzle.</p>
-# MAGIC </main>
 
 # COMMAND ----------
 
@@ -239,30 +185,60 @@ input <- "104
 
 # COMMAND ----------
 
-# input <- "16
-# 10
-# 15
-# 5
-# 1
-# 11
-# 7
-# 19
-# 6
-# 12
-# 4
-# "
-
-# COMMAND ----------
-
 nums <- input %>% read_lines() %>% parse_integer()
-
 sequence <- sort(c(0, nums, max(nums) + 3))
-
-sum(diff(sequence) == 1) * sum(diff(sequence) == 3)
+answer <- sum(diff(sequence) == 1) * sum(diff(sequence) == 3)
+answer
 
 # COMMAND ----------
 
-# MAGIC %md ## Part 2
+# MAGIC %md <article class="day-desc"><h2 id="part2">--- Part Two ---</h2><p>To completely determine whether you have enough adapters, you'll need to figure out how many different ways they can be arranged. Every arrangement needs to connect the charging outlet to your device. The previous rules about when adapters can successfully connect still apply.</p>
+# MAGIC <p>The first example above (the one that starts with <code>16</code>, <code>10</code>, <code>15</code>) supports the following arrangements:</p>
+# MAGIC <pre><code>(0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+# MAGIC (0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)
+# MAGIC (0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)
+# MAGIC (0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)
+# MAGIC (0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+# MAGIC (0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
+# MAGIC (0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
+# MAGIC (0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
+# MAGIC </code></pre>
+# MAGIC <p>(The charging outlet and your device's built-in adapter are shown in parentheses.) Given the adapters from the first example, the total number of arrangements that connect the charging outlet to your device is <em><code>8</code></em>.</p>
+# MAGIC <p>The second example above (the one that starts with <code>28</code>, <code>33</code>, <code>18</code>) has many arrangements. Here are a few:</p>
+# MAGIC <pre><code>(0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+# MAGIC 32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 48, 49, (52)
+# MAGIC 
+# MAGIC (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+# MAGIC 32, 33, 34, 35, 38, 39, 42, 45, 46, 47, 49, (52)
+# MAGIC 
+# MAGIC (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+# MAGIC 32, 33, 34, 35, 38, 39, 42, 45, 46, 48, 49, (52)
+# MAGIC 
+# MAGIC (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+# MAGIC 32, 33, 34, 35, 38, 39, 42, 45, 46, 49, (52)
+# MAGIC 
+# MAGIC (0), 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 17, 18, 19, 20, 23, 24, 25, 28, 31,
+# MAGIC 32, 33, 34, 35, 38, 39, 42, 45, 47, 48, 49, (52)
+# MAGIC 
+# MAGIC (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+# MAGIC 46, 48, 49, (52)
+# MAGIC 
+# MAGIC (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+# MAGIC 46, 49, (52)
+# MAGIC 
+# MAGIC (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+# MAGIC 47, 48, 49, (52)
+# MAGIC 
+# MAGIC (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+# MAGIC 47, 49, (52)
+# MAGIC 
+# MAGIC (0), 3, 4, 7, 10, 11, 14, 17, 20, 23, 25, 28, 31, 34, 35, 38, 39, 42, 45,
+# MAGIC 48, 49, (52)
+# MAGIC </code></pre>
+# MAGIC <p>In total, this set of adapters can connect the charging outlet to your device in <em><code>19208</code></em> distinct arrangements.</p>
+# MAGIC <p>You glance back down at your bag and try to remember why you brought so many adapters; there must be <em>more than a trillion</em> valid ways to arrange them! Surely, there must be <span title="Definitely itertools.">an efficient way</span> to count the arrangements.</p>
+# MAGIC <p><em>What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?</em></p>
+# MAGIC </article>
 
 # COMMAND ----------
 
@@ -274,6 +250,5 @@ for (num in rev(sequence)[-1]) {
   ways[num + 1] <- sum(ways[next_nums + 1])
 }
 
-# COMMAND ----------
-
-format(ways[[1]], scientific = FALSE)
+answer <- format(ways[[1]], scientific = FALSE)
+answer
