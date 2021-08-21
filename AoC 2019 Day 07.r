d9639d0c0da88c0cf8f5deb2bb31484edf679af2
Paul Foster <paulfstr@gmail.com>
2021-08-21 01:45:32 +0000

    WIP 2019 day 7 part 2 progress

--- AoC 2019 Day 07.r
+++ AoC 2019 Day 07.r
@@ -134,7 +134,13 @@ run_instructions <- function(instructions, phase, input) {
 
 # COMMAND ----------
 
-get_thruster_output <- function(instructions, a_phase, b_phase, c_phase, d_phase, e_phase, a_input = 0) {
+get_thruster_output <- function(instructions, phases, a_input = 0) {
+  a_phase <- phases[[1]]
+  b_phase <- phases[[2]]
+  c_phase <- phases[[3]]
+  d_phase <- phases[[4]]
+  e_phase <- phases[[5]]
+  
   a_output <- run_instructions(
     instructions,
     phase = a_phase,
@@ -165,35 +171,31 @@ get_thruster_output <- function(instructions, a_phase, b_phase, c_phase, d_phase
   thruster_output
 }
 
-get_thruster_output2 <- function(instructions, phases, a_input = 0) {
-  get_thruster_output(instructions, phases[[1]], phases[[2]], phases[[3]], phases[[4]], phases[[5]], a_input)
-}
-
 # COMMAND ----------
 
 # MAGIC %md ### Tests
 
 # COMMAND ----------
 
-library("testthat")
+library(testthat)
 
 test_that("thruster output works", {
   expect_equal(
-    get_thruster_output2(
+    get_thruster_output(
       instructions = c(3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0),
       c(4, 3, 2, 1, 0)
     ),
     43210
   )
   expect_equal(
-    get_thruster_output2(
+    get_thruster_output(
       instructions = c(3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0),
       c(0,1,2,3,4)
     ),
     54321
   )
   expect_equal(
-    get_thruster_output2(
+    get_thruster_output(
       instructions = c(3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0),
       c(1,0,4,3,2)
     ),
@@ -261,6 +263,213 @@ answer
 # MAGIC </ul>
 # MAGIC <p>Try every combination of the new phase settings on the amplifier feedback loop.  <em>What is the highest signal that can be sent to the thrusters?</em></p>
 # MAGIC </article>
+
+# COMMAND ----------
+
+run_amp <- function(amp, input) {
+  while (amp$instructions[list(amp$i, TRUE)] != 99) {
+    value <- amp$instructions[list(amp$i, TRUE)]
+    
+    op_code <- value %% 100
+    p1_is_immediate <- value %% 1000 %/% 100
+    p2_is_immediate <- value %% 10000 %/% 1000
+    p3_is_immediate <- value %% 100000 %/% 10000 # Unused
+    
+    p1 <- list(amp$i + 1, p1_is_immediate)
+    p2 <- list(amp$i + 2, p2_is_immediate)
+    p3 <- list(amp$i + 3, p3_is_immediate)
+    
+    if (op_code == 1) {
+      amp$instructions[p3] <- amp$instructions[p1] + amp$instructions[p2]
+    } else if (op_code == 2) {
+      amp$instructions[p3] <- amp$instructions[p1] * amp$instructions[p2]
+    } else if (op_code == 3) {
+      # The first input instruction (opcode 3) is the phase setting
+      if (amp$first_input) {
+        amp$instructions[p1] <- amp$phase
+        amp$first_input <- FALSE
+      } else if (!is.na(input)) {
+        amp$instructions[p1] <- input
+        input <- NA # Only use the input once
+      } else {
+        break # No input, so go to next amplifier
+      }
+    } else if (op_code == 4) {
+      amp$output <- amp$instructions[p1]
+    } else if (op_code == 5) {
+      if (amp$instructions[p1] != 0) {
+        amp$i <- amp$instructions[p2] 
+        next
+      }
+    } else if (op_code == 6) {
+      if (amp$instructions[p1] == 0) {
+        amp$i = get_index(amp$instructions, p2) - 1
+        amp$i <- amp$instructions[p2] 
+        next
+      }
+    } else if (op_code == 7) {
+      amp$instructions[p3] <- amp$instructions[p1] < amp$instructions[p2]
+    } else if (op_code == 8) {
+      amp$instructions[p3] <- amp$instructions[p1] == amp$instructions[p2]
+    } else {
+      stop(paste0('Invalid op code: ', op_code, ' at position ', i, '\n', paste0(amp$instructions, collapse = ', ')))
+    }
+    
+    num_params <- case_when(
+      op_code %in% c(3, 4) ~ 1,
+      op_code %in% c(5, 6) ~ 2,
+      op_code %in% c(1, 2, 7, 8) ~ 3
+    )
+    amp$i <- amp$i + 1 + num_params
+  }
+  
+  #if (amp$instructions[list(amp$i, TRUE)] == 99) stop("ENDED!")# FIXME: DEBUG
+  if (amp$instructions[list(amp$i, TRUE)] == 99) {
+    amp$is_halted <- TRUE
+  }
+  
+  amp
+}
+
+# COMMAND ----------
+
+create_amp <- function(instructions, phase) {
+  list(
+    instructions = structure(instructions, class = "special_index"),
+    phase = phase,
+    i = 0,
+    first_input = TRUE,
+    is_halted = FALSE,
+    output = NULL
+  )
+}
+
+# get_thruster_output2 <- function(instructions, phases, a_input = 0) {
+#   amp_a <- create_amp(instructions, phases[[1]])
+#   amp_b <- create_amp(instructions, phases[[2]])
+#   amp_c <- create_amp(instructions, phases[[3]])
+#   amp_d <- create_amp(instructions, phases[[4]])
+#   amp_e <- create_amp(instructions, phases[[5]])
+  
+#   repeat { # FIXME: Is it possible for just one amp to halt but not the others?
+#     amp_a <- run_amp(amp_a, a_input)     ; if (amp_a$is_halted) break
+#     amp_b <- run_amp(amp_b, amp_a$output); if (amp_b$is_halted) break
+#     amp_c <- run_amp(amp_c, amp_b$output); if (amp_c$is_halted) break
+#     amp_d <- run_amp(amp_d, amp_c$output); if (amp_d$is_halted) break
+#     amp_e <- run_amp(amp_e, amp_d$output); if (amp_e$is_halted) break
+
+#     a_input <- amp_e$output
+#   }
+
+#   thruster_output <- amp_e$output
+#   thruster_output
+# }
+
+get_thruster_output2 <- function(instructions, phases, a_input = 0) {
+  amp_a <- create_amp(instructions, phases[[1]])
+  amp_b <- create_amp(instructions, phases[[2]])
+  amp_c <- create_amp(instructions, phases[[3]])
+  amp_d <- create_amp(instructions, phases[[4]])
+  amp_e <- create_amp(instructions, phases[[5]])
+  
+  while (!amp_e$is_halted) { # FIXME: Is it possible for just one amp to halt but not the others?
+    amp_a <- run_amp(amp_a, a_input)
+    amp_b <- run_amp(amp_b, amp_a$output)
+    amp_c <- run_amp(amp_c, amp_b$output)
+    amp_d <- run_amp(amp_d, amp_c$output)
+    amp_e <- run_amp(amp_e, amp_d$output)
+
+    a_input <- amp_e$output
+  }
+
+  thruster_output <- amp_e$output
+  thruster_output
+}
+
+# COMMAND ----------
+
+# instructions = c(3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5)
+# phases <- c(9, 8, 7, 6, 5)
+# a_input <- 0
+
+# amp_a <- create_amp(instructions, phases[[1]])
+# amp_b <- create_amp(instructions, phases[[2]])
+# amp_c <- create_amp(instructions, phases[[3]])
+# amp_d <- create_amp(instructions, phases[[4]])
+# amp_e <- create_amp(instructions, phases[[5]])
+
+#     amp_a <- run_amp(amp_a, a_input)
+# #     amp_b <- run_amp(amp_b, amp_a$output)
+# #     amp_c <- run_amp(amp_c, amp_b$output)
+# #     amp_d <- run_amp(amp_d, amp_c$output)
+# #     amp_e <- run_amp(amp_e, amp_d$output)
+
+# #     a_input <- amp_e$output
+
+# COMMAND ----------
+
+get_thruster_output2(
+      instructions = c(3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5),
+      c(9, 8, 7, 6, 5)
+    )
+
+# COMMAND ----------
+
+test_that("part2 thruster output works", {
+  expect_equal(
+    get_thruster_output2(
+      instructions = c(3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5),
+      c(9, 8, 7, 6, 5)
+    ),
+    139629729
+  )
+})
+
+# COMMAND ----------
+
+test_that("part2 max thruster output works", {
+  expect_equal(
+    get_max_thruster_output(c(3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0)),
+    43210
+  )
+})
+
+# COMMAND ----------
+
+get_thruster_output2 <- function(instructions, a_phase, b_phase, c_phase, d_phase, e_phase, a_input = 0) {
+  a_output <- run_instructions(
+    instructions,
+    phase = a_phase,
+    input = a_input
+  )
+  b_output <- run_instructions(
+    instructions,
+    phase = b_phase,
+    input = a_output
+  )
+  c_output <- run_instructions(
+    instructions,
+    phase = c_phase,
+    input = b_output
+  )
+  d_output <- run_instructions(
+    instructions,
+    phase = d_phase,
+    input = c_output
+  )
+  e_output <- run_instructions(
+    instructions,
+    phase = e_phase,
+    input = d_output
+  )
+
+  thruster_output <- e_output
+  thruster_output
+}
+
+get_thruster_output2 <- function(instructions, phases, a_input = 0) {
+  get_thruster_output(instructions, phases[[1]], phases[[2]], phases[[3]], phases[[4]], phases[[5]], a_input)
+}
 
 # COMMAND ----------
 
