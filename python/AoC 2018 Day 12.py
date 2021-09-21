c5ff86a28b8856c7e75c02269c1ea90c62207279
Paul Foster <paulfstr@gmail.com>
2021-09-21 09:55:06 +0000

    Complete 2018 day 12

---
+++ python/AoC 2018 Day 12.py
@@ -0,0 +1,140 @@
+# Databricks notebook source
+# MAGIC %md https://adventofcode.com/2018/day/12
+
+# COMMAND ----------
+
+# MAGIC %md <article class="day-desc"><h2>--- Day 12: Subterranean Sustainability ---</h2><p>The year 518 is significantly more underground than your history books implied.  Either that, or you've arrived in a <span title="It's probably this one. Can never be too sure, though.">vast cavern network</span> under the North Pole.</p>
+# MAGIC <p>After exploring a little, you discover a long tunnel that contains a row of small pots as far as you can see to your left and right.  A few of them contain plants - someone is trying to grow things in these geothermally-heated caves.</p>
+# MAGIC <p>The pots are numbered, with <code>0</code> in front of you.  To the left, the pots are numbered <code>-1</code>, <code>-2</code>, <code>-3</code>, and so on; to the right, <code>1</code>, <code>2</code>, <code>3</code>.... Your puzzle input contains a list of pots from <code>0</code> to the right and whether they do (<code>#</code>) or do not (<code>.</code>) currently contain a plant, the <em>initial state</em>. (No other pots currently contain plants.) For example, an initial state of <code>#..##....</code> indicates that pots <code>0</code>, <code>3</code>, and <code>4</code> currently contain plants.</p>
+# MAGIC <p>Your puzzle input also contains some notes you find on a nearby table: someone has been trying to figure out how these plants <em>spread</em> to nearby pots.  Based on the notes, for each generation of plants, a given pot has or does not have a plant based on whether that pot (and the two pots on either side of it) had a plant in the last generation. These are written as <code>LLCRR =&gt; N</code>, where <code>L</code> are pots to the left, <code>C</code> is the current pot being considered, <code>R</code> are the pots to the right, and <code>N</code> is whether the current pot will have a plant in the next generation. For example:</p>
+# MAGIC <ul>
+# MAGIC <li>A note like <code>..#.. =&gt; .</code> means that a pot that contains a plant but with no plants within two pots of it will not have a plant in it during the next generation.</li>
+# MAGIC <li>A note like <code>##.## =&gt; .</code> means that an empty pot with two plants on each side of it will remain empty in the next generation.</li>
+# MAGIC <li>A note like <code>.##.# =&gt; #</code> means that a pot has a plant in a given generation if, in the previous generation, there were plants in that pot, the one immediately to the left, and the one two pots to the right, but not in the ones immediately to the right and two to the left.</li>
+# MAGIC </ul>
+# MAGIC <p>It's not clear what these plants are for, but you're sure it's important, so you'd like to make sure the current configuration of plants is sustainable by determining what will happen after <em><code>20</code> generations</em>.</p>
+# MAGIC <p>For example, given the following input:</p>
+# MAGIC <pre><code>initial state: #..#.#..##......###...###
+# MAGIC 
+# MAGIC ...## =&gt; #
+# MAGIC ..#.. =&gt; #
+# MAGIC .#... =&gt; #
+# MAGIC .#.#. =&gt; #
+# MAGIC .#.## =&gt; #
+# MAGIC .##.. =&gt; #
+# MAGIC .#### =&gt; #
+# MAGIC #.#.# =&gt; #
+# MAGIC #.### =&gt; #
+# MAGIC ##.#. =&gt; #
+# MAGIC ##.## =&gt; #
+# MAGIC ###.. =&gt; #
+# MAGIC ###.# =&gt; #
+# MAGIC ####. =&gt; #
+# MAGIC </code></pre>
+# MAGIC <p>For brevity, in this example, only the combinations which do produce a plant are listed. (Your input includes all possible combinations.) Then, the next 20 generations will look like this:</p>
+# MAGIC <pre><code>                 1         2         3     
+# MAGIC        0         0         0         0     
+# MAGIC  0: ...#..#.#..##......###...###...........
+# MAGIC  1: ...#...#....#.....#..#..#..#...........
+# MAGIC  2: ...##..##...##....#..#..#..##..........
+# MAGIC  3: ..#.#...#..#.#....#..#..#...#..........
+# MAGIC  4: ...#.#..#...#.#...#..#..##..##.........
+# MAGIC  5: ....#...##...#.#..#..#...#...#.........
+# MAGIC  6: ....##.#.#....#...#..##..##..##........
+# MAGIC  7: ...#..###.#...##..#...#...#...#........
+# MAGIC  8: ...#....##.#.#.#..##..##..##..##.......
+# MAGIC  9: ...##..#..#####....#...#...#...#.......
+# MAGIC 10: ..#.#..#...#.##....##..##..##..##......
+# MAGIC 11: ...#...##...#.#...#.#...#...#...#......
+# MAGIC 12: ...##.#.#....#.#...#.#..##..##..##.....
+# MAGIC 13: ..#..###.#....#.#...#....#...#...#.....
+# MAGIC 14: ..#....##.#....#.#..##...##..##..##....
+# MAGIC 15: ..##..#..#.#....#....#..#.#...#...#....
+# MAGIC 16: .#.#..#...#.#...##...#...#.#..##..##...
+# MAGIC 17: ..#...##...#.#.#.#...##...#....#...#...
+# MAGIC 18: ..##.#.#....#####.#.#.#...##...##..##..
+# MAGIC 19: .#..###.#..#.#.#######.#.#.#..#.#...#..
+# MAGIC 20: .#....##....#####...#######....#.#..##.
+# MAGIC </code></pre>
+# MAGIC <p>The generation is shown along the left, where <code>0</code> is the initial state.  The pot numbers are shown along the top, where <code>0</code> labels the center pot, negative-numbered pots extend to the left, and positive pots extend toward the right. Remember, the initial state begins at pot <code>0</code>, which is not the leftmost pot used in this example.</p>
+# MAGIC <p>After one generation, only seven plants remain.  The one in pot <code>0</code> matched the rule looking for <code>..#..</code>, the one in pot <code>4</code> matched the rule looking for <code>.#.#.</code>, pot <code>9</code> matched <code>.##..</code>, and so on.</p>
+# MAGIC <p>In this example, after 20 generations, the pots shown as <code>#</code> contain plants, the furthest left of which is pot <code>-2</code>, and the furthest right of which is pot <code>34</code>. Adding up all the numbers of plant-containing pots after the 20th generation produces <code><em>325</em></code>.</p>
+# MAGIC <p><em>After <code>20</code> generations, what is the sum of the numbers of all pots which contain a plant?</em></p>
+# MAGIC </article>
+
+# COMMAND ----------
+
+inp = '''initial state: ##.###.......#..#.##..#####...#...#######....##.##.##.##..#.#.##########...##.##..##.##...####..####
+
+#.#.# => #
+.##.. => .
+#.#.. => .
+..### => #
+.#..# => #
+..#.. => .
+####. => #
+###.. => #
+#.... => .
+.#.#. => #
+....# => .
+#...# => #
+..#.# => #
+#..#. => #
+.#... => #
+##..# => .
+##... => .
+#..## => .
+.#.## => #
+.##.# => .
+#.##. => #
+.#### => .
+.###. => .
+..##. => .
+##.#. => .
+...## => #
+...#. => .
+..... => .
+##.## => .
+###.# => #
+##### => #
+#.### => .'''
+
+# COMMAND ----------
+
+import re
+
+def simulate(s, rules, n):
+  r = re.compile('|'.join(f'(?<={x[0]}{x[1]}){x[2]}(?={x[3]}{x[4]})' for x, after in rules if after == '#'))
+  for _ in range(n):
+    s = '____' + s + '____'
+    new_s = list(s.replace('#', '_'))
+    
+    for it in r.finditer(s):
+      new_s[it.start(0)] = '#'
+    s = ''.join(new_s)
+  return sum(i - n * 4 for i, x in enumerate(s) if x == '#')
+
+initial_state, rules = inp.replace('.', '_').split('\n\n')
+initial_state = initial_state.split(': ')[1]
+rules = [line.split(' => ') for line in rules.splitlines()]
+
+answer = simulate(initial_state, rules, 20)
+print(answer)
+
+# COMMAND ----------
+
+# MAGIC %md <article class="day-desc"><h2 id="part2">--- Part Two ---</h2><p>You realize that 20 generations aren't enough.  After all, these plants will need to last another 1500 years to even reach your timeline, not to mention your future.</p>
+# MAGIC <p><em>After fifty billion (<code>50000000000</code>) generations, what is the sum of the numbers of all pots which contain a plant?</em></p>
+# MAGIC </article>
+
+# COMMAND ----------
+
+import numpy as np
+import sklearn as sk
+
+X = np.array(range(200, 250))[:,np.newaxis]
+y = [simulate(initial_state, rules, n) for n in x]
+m = sk.linear_model.LinearRegression().fit(X, y)
+
+answer = int(m.predict([[50000000000]])[0])
+print(answer)
