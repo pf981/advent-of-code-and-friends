af22192a9139eb9a51c7ceba145e61eba13f947e
Paul Foster <paulfstr@gmail.com>
2021-12-10 05:42:06 +0000

    Simplify

--- python/AoC 2021 Day 10.py
+++ python/AoC 2021 Day 10.py
@@ -160,19 +160,34 @@ open_to_close = {
   '{': '}',
   '<': '>',
 }
+close_to_score = {
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4,
+}
 
-error_score = 0
-for line in inp.splitlines():
+def get_scores(line):
   expected_closes = []
   for c in line:
     if c in ')]}>':
       if c != expected_closes.pop():
-        error_score += close_to_error_score[c]
-        break
+        return close_to_error_score[c], None
     else:
       expected_closes += open_to_close[c]
-
-answer = error_score
+      
+  score = 0
+  for c in expected_closes[::-1]:
+    score *= 5
+    score += close_to_score[c]
+  
+  return None, score
+      
+scores = [get_scores(line) for line in inp.splitlines()]
+error_scores = [error_score for error_score, _ in scores if error_score]
+close_scores = [close_score for _, close_score in scores if close_score]
+      
+answer = sum(error_scores)
 print(answer)
 
 # COMMAND ----------
@@ -219,31 +234,5 @@ print(answer)
 
 import statistics
 
-close_to_score = {
-  ')': 1,
-  ']': 2,
-  '}': 3,
-  '>': 4,
-}
-
-scores = []
-for i, line in enumerate(inp.splitlines()):
-  expected_closes = []
-  has_error = False
-  for c in line:
-    if c in ')]}>':
-      if c != expected_closes.pop():
-        has_error = True
-        break
-    else:
-      expected_closes += open_to_close[c]
-      
-  if not has_error:
-    score = 0
-    for c in expected_closes[::-1]:
-      score *= 5
-      score += close_to_score[c]
-    scores.append(score)
-
-answer = statistics.median(scores)
+answer = statistics.median(close_scores)
 print(answer)
