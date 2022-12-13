21b04ad3447b37d7ff3658957c801d6820914afe
Paul Foster <paulfstr@gmail.com>
2022-12-13 06:30:23 +0000

    Make return value compatible with cmp_to_key

--- python/AoC 2022 Day 13.py
+++ python/AoC 2022 Day 13.py
@@ -564,38 +564,35 @@ inp = '''[[[10],7,3,[[6,4,4,1],[],[2,6,7,10]],6],[]]
 
 import copy
 
-def is_right_order(a, b):
+def cmp(a, b):
   a = copy.deepcopy(a)
   b = copy.deepcopy(b)
   
   if isinstance(a, int) and isinstance(b, int):
     if a < b:
-      return True
+      return 1
     if a > b:
-      return False
-    return None
+      return -1
+    return 0
   
   if isinstance(a, list) and isinstance(b, list):
     if not a and not b:
-      return None
+      return 0
     if not a:
-      return True
+      return 1
     if not b:
-      return False
+      return -1
     
-    result = is_right_order(a.pop(0), b.pop(0))
-    if result is None:
-      return is_right_order(a, b)
-    return result
+    return cmp(a.pop(0), b.pop(0)) or cmp(a, b)
   
   if isinstance(a, int):
-    return is_right_order([a], b)
-  return is_right_order(a, [b])
+    return cmp([a], b)
+  return cmp(a, [b])
 
 
 pairs = [[eval(s) for s in pair.splitlines()] for pair in inp.split('\n\n')]
 
-answer = sum(i for i, (a, b) in enumerate(pairs, 1) if is_right_order(a, b))
+answer = sum(i for i, (a, b) in enumerate(pairs, 1) if cmp(a, b) == 1)
 print(answer)
 
 # COMMAND ----------
@@ -638,7 +635,6 @@ packets = [packet for pair in pairs for packet in pair]
 packets.append([[2]])
 packets.append([[6]])
 
-cmp = lambda a, b : 1 if is_right_order(a, b) else -1 if is_right_order(b, a) else 0
 packets.sort(key=functools.cmp_to_key(cmp), reverse=True)
 
 answer = (packets.index([[2]]) + 1) * (packets.index([[6]]) + 1)
