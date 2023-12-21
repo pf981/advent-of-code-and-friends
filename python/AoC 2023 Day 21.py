ce153b82818b0ad3bed8774f73db040170ac2e23
Paul Foster <paulfstr@gmail.com>
2023-12-21 11:30:06 +0000

    Simplify code

--- python/AoC 2023 Day 21.py
+++ python/AoC 2023 Day 21.py
@@ -1,20 +1,5 @@
 # Databricks notebook source
-# %pip install z3-solver
-
-# COMMAND ----------
-
-inp = '''...........
-.....###.#.
-.###.##..#.
-..#.#...#..
-....#.#....
-.##..S####.
-.##..#...#.
-.......##..
-.##.#.####.
-.##..##.##.
-...........
-'''
+# MAGIC %md https://adventofcode.com/2023/day/21
 
 # COMMAND ----------
 
@@ -153,371 +138,94 @@ inp = '''.......................................................................
 
 # COMMAND ----------
 
-# plots = {(row, col) for row, line in enumerate(inp.splitlines()) for col, c in enumerate(line) if c in 'S.'}
-# start_pos = next((row, col) for row, line in enumerate(inp.splitlines()) for col, c in enumerate(line) if c == 'S')
-
-# stack = [(start_pos, 0)]
-# options = set()
-# while stack:
-#   pos, steps = stack.pop()
-#   if pos not in plots:
-#     continue
-
-#   # if steps == 6: #
-#   if steps == 64:
-#     options.add(pos)
-#     continue
-
-#   for d in 'NESW':
-#     new_pos = (
-#       pos[0] + (d == 'S') - (d == 'N'),
-#       pos[1] + (d == 'E') - (d == 'W')
-#     )
-#     stack.append((new_pos, steps + 1))
-
-# len(options)
+# MAGIC %md <article class="day-desc"><h2>--- Day 21: Step Counter ---</h2><p>You manage to catch the <a href="7">airship</a> right as it's dropping someone else off on their all-expenses-paid trip to Desert Island! It even helpfully drops you off near the <a href="5">gardener</a> and his massive farm.</p>
+# MAGIC <p>"You got the sand flowing again! Great work! Now we just need to wait until we have enough sand to filter the water for Snow Island and we'll have snow again in no time."</p>
+# MAGIC <p>While you wait, one of the Elves that works with the gardener heard how good you are at solving problems and would like your help. He needs to get his <a href="https://en.wikipedia.org/wiki/Pedometer" target="_blank">steps</a> in for the day, and so he'd like to know <em>which garden plots he can reach with exactly his remaining <code>64</code> steps</em>.</p>
+# MAGIC <p>He gives you an up-to-date map (your puzzle input) of his starting position (<code>S</code>), garden plots (<code>.</code>), and rocks (<code>#</code>). For example:</p>
+# MAGIC <pre><code>...........
+# MAGIC .....###.#.
+# MAGIC .###.##..#.
+# MAGIC ..#.#...#..
+# MAGIC ....#.#....
+# MAGIC .##..S####.
+# MAGIC .##..#...#.
+# MAGIC .......##..
+# MAGIC .##.#.####.
+# MAGIC .##..##.##.
+# MAGIC ...........
+# MAGIC </code></pre>
+# MAGIC <p>The Elf starts at the starting position (<code>S</code>) which also counts as a garden plot. Then, he can take one step north, south, east, or west, but only onto tiles that are garden plots. This would allow him to reach any of the tiles marked <code>O</code>:</p>
+# MAGIC <pre><code>...........
+# MAGIC .....###.#.
+# MAGIC .###.##..#.
+# MAGIC ..#.#...#..
+# MAGIC ....#O#....
+# MAGIC .##.OS####.
+# MAGIC .##..#...#.
+# MAGIC .......##..
+# MAGIC .##.#.####.
+# MAGIC .##..##.##.
+# MAGIC ...........
+# MAGIC </code></pre>
+# MAGIC <p>Then, he takes a second step. Since at this point he could be at <em>either</em> tile marked <code>O</code>, his second step would allow him to reach any garden plot that is one step north, south, east, or west of <em>any</em> tile that he could have reached after the first step:</p>
+# MAGIC <pre><code>...........
+# MAGIC .....###.#.
+# MAGIC .###.##..#.
+# MAGIC ..#.#O..#..
+# MAGIC ....#.#....
+# MAGIC .##O.O####.
+# MAGIC .##.O#...#.
+# MAGIC .......##..
+# MAGIC .##.#.####.
+# MAGIC .##..##.##.
+# MAGIC ...........
+# MAGIC </code></pre>
+# MAGIC <p>After two steps, he could be at any of the tiles marked <code>O</code> above, including the starting position (either by going north-then-south or by going west-then-east).</p>
+# MAGIC <p>A single third step leads to even more possibilities:</p>
+# MAGIC <pre><code>...........
+# MAGIC .....###.#.
+# MAGIC .###.##..#.
+# MAGIC ..#.#.O.#..
+# MAGIC ...O#O#....
+# MAGIC .##.OS####.
+# MAGIC .##O.#...#.
+# MAGIC ....O..##..
+# MAGIC .##.#.####.
+# MAGIC .##..##.##.
+# MAGIC ...........
+# MAGIC </code></pre>
+# MAGIC <p>He will continue like this until his steps for the day have been exhausted. After a total of <code>6</code> steps, he could reach any of the garden plots marked <code>O</code>:</p>
+# MAGIC <pre><code>...........
+# MAGIC .....###.#.
+# MAGIC .###.##.O#.
+# MAGIC .O#O#O.O#..
+# MAGIC O.O.#.#.O..
+# MAGIC .##O.O####.
+# MAGIC .##.O#O..#.
+# MAGIC .O.O.O.##..
+# MAGIC .##.#.####.
+# MAGIC .##O.##.##.
+# MAGIC ...........
+# MAGIC </code></pre>
+# MAGIC <p>In this example, if the Elf's goal was to get exactly <code>6</code> more steps today, he could use them to reach any of <code><em>16</em></code> garden plots.</p>
+# MAGIC <p>However, the Elf <em>actually needs to get <code>64</code> steps today</em>, and the map he's handed you is much larger than the example map.</p>
+# MAGIC <p>Starting from the garden plot marked <code>S</code> on your map, <em>how many garden plots could the Elf reach in exactly <code>64</code> steps?</em></p>
+# MAGIC </article>
 
 # COMMAND ----------
 
 import collections
-plots = {(row, col) for row, line in enumerate(inp.splitlines()) for col, c in enumerate(line) if c in 'S.'}
-start_pos = next((row, col) for row, line in enumerate(inp.splitlines()) for col, c in enumerate(line) if c == 'S')
-
-q = collections.deque([(start_pos, 0)])
-can_reach = set() # steps%2, pos
-while q:
-  pos, steps = q.popleft()
-  #if steps == 6: print(pos)
-
-  if pos not in plots:
-    continue
-
-  if (steps%2, pos) in can_reach:
-    #print(pos)
-    continue
-  can_reach.add((steps%2, pos))
-
-  #if steps > 6:
-  if steps > 64:
-    #options.add(pos)
-    continue
-
-  for d in 'NESW':
-    new_pos = (
-      pos[0] + (d == 'S') - (d == 'N'),
-      pos[1] + (d == 'E') - (d == 'W')
-    )
-    q.append((new_pos, steps + 1))
-
-len({pos for i, pos in can_reach if i == 0})
-
-# COMMAND ----------
-
-# PART 2
-import collections
-plots = {(row, col) for row, line in enumerate(inp.splitlines()) for col, c in enumerate(line) if c in 'S.'}
-start_pos = next((row, col) for row, line in enumerate(inp.splitlines()) for col, c in enumerate(line) if c == 'S')
 
 lines = inp.splitlines()
-n_rows = len(lines)
-n_cols = len(lines[0])
-
-q = collections.deque([(start_pos, 0)])
-can_reach = set() # steps%2, pos
-
-step = 0
-reach_counts = []
-while q:
-  pos, steps = q.popleft()
-  #if steps == 6: print(pos)
-
-  if steps > step:
-    # print(steps)
-    reach_counts.append(len({pos for i, pos in can_reach if i == step % 2})) # Something like this
-    step = steps
-    # reach_counts.append(len({pos for i, pos in can_reach}))
-
-
-  base_pos = (pos[0] % n_rows, pos[1] % n_cols)
-
-  if base_pos not in plots:
-    continue
-
-  if (steps%2, pos) in can_reach:
-    #print(pos)
-    continue
-  can_reach.add((steps%2, pos))
-
-  # if steps > 6:
-  if steps > 100:
-  # if steps > 500:# Too big
-  # if steps > 5000:
-    #options.add(pos)
-    continue
-
-
-
-  for d in 'NESW':
-    new_pos = (
-      pos[0] + (d == 'S') - (d == 'N'),
-      pos[1] + (d == 'E') - (d == 'W')
-    )
-    q.append((new_pos, steps + 1))
-
-len({pos for i, pos in can_reach if i == 0})
-
-# COMMAND ----------
-
-reach_counts
-
-# COMMAND ----------
-
-reach_counts[50]
-
-# COMMAND ----------
-
-reach_counts[100]
-
-# COMMAND ----------
-
-# MAGIC %pip install fedot
-
-# COMMAND ----------
-
-# FEDOT api
-from fedot.api.main import Fedot
-# Tasks to solve
-from fedot.core.repository.tasks import Task, TaskTypesEnum, TsForecastingParams
-# Input data for fit and predict
-from fedot.core.data.data import InputData
-# Train and test split
-# from fedot.core.data.data import train_test_data_setup
-
-forecast_length = 144
-task = Task(TaskTypesEnum.ts_forecasting,
-            TsForecastingParams(forecast_length=forecast_length))
-
-# COMMAND ----------
-
-train_data_raw = [1,
- 1,
- 4,
- 4,
- 9,
- 9,
- 16,
- 16,
- 30,
- 30,
- 50,
- 50,
- 74,
- 74,
- 99,
- 99,
- 129,
- 129,
- 165,
- 165,
- 216,
- 216,
- 261,
- 261,
- 326,
- 326,
- 395,
- 395,
- 460,
- 460,
- 537,
- 537,
- 605,
- 605,
- 689,
- 689,
- 784,
- 784,
- 894,
- 894,
- 989,
- 989,
- 1107,
- 1107,
- 1196,
- 1196,
- 1324,
- 1324,
- 1464,
- 1464,
- 1594,
- 1594,
- 1735,
- 1735,
- 1853,
- 1853,
- 1988,
- 1988,
- 2145,
- 2145,
- 2324,
- 2324,
- 2479,
- 2479,
- 2665,
- 2665,
- 2794,
- 2794,
- 2982,
- 2982,
- 3186,
- 3186,
- 3380,
- 3380,
- 3585,
- 3585,
- 3753,
- 3753,
- 3938,
- 3938,
- 4155,
- 4155,
- 4402,
- 4402,
- 4617,
- 4617,
- 4871,
- 4871,
- 5040,
- 5040,
- 5288,
- 5288,
- 5556,
- 5556,
- 5814,
- 5814,
- 6083,
- 6083,
- 6301,
- 6301,
- 6536]
-
-# COMMAND ----------
-
-[b - a for a, b in zip(train_data_raw[:-1], train_data_raw[1:])]
-
-# COMMAND ----------
-
-l = [train_data_raw[i*2] for i in range(len(train_data_raw) // 2)]
-[b - a for a, b in zip(l[:-1], l[1:])]
-
-# COMMAND ----------
-
-[train_data_raw[i*2] for i in range(len(train_data_raw) // 2)]
-
-# COMMAND ----------
-
-from statsmodels.tsa.arima.model import ARIMA
-
-model = ARIMA(train_data_raw, order=(5, 1, 0))
-model_fit = model.fit()
-print(model_fit.summary())
-
-# COMMAND ----------
-
-print('\n'.join(str(x) for x in train_data_raw))
-
-# COMMAND ----------
-
-import numpy as np
-# Init model for the time series forecasting
-model = Fedot(problem='ts_forecasting', task_params=task.task_params)
-# Run AutoML model design
-chain = model.fit(features=np.array(train_data_raw))
-
-# COMMAND ----------
-
-chain
-
-# COMMAND ----------
-
-# # PART 2
-# import collections
-# plots = {(row, col) for row, line in enumerate(inp.splitlines()) for col, c in enumerate(line) if c in 'S.'}
-# start_pos = next((row, col) for row, line in enumerate(inp.splitlines()) for col, c in enumerate(line) if c == 'S')
-
-# lines = inp.splitlines()
-# n_rows = len(lines)
-# n_cols = len(lines[0])
-
-# q = collections.deque([(start_pos, 0)])
-# can_reach = set() # steps%2, pos
-# while q:
-#   pos, steps = q.popleft()
-#   #if steps == 6: print(pos)
-
-#   base_pos = (pos[0] % n_rows, pos[1] % n_cols)
-
-#   if base_pos not in plots:
-#     continue
-
-#   if (steps%2, pos) in can_reach:
-#     #print(pos)
-#     continue
-#   can_reach.add((steps%2, pos))
-
-#   #if steps > 6:
-#   if steps > 5000:
-#     #options.add(pos)
-#     continue
-
-#   for d in 'NESW':
-#     new_pos = (
-#       pos[0] + (d == 'S') - (d == 'N'),
-#       pos[1] + (d == 'E') - (d == 'W')
-#     )
-#     q.append((new_pos, steps + 1))
-
-# len({pos for i, pos in can_reach if i == 0})
-
-# COMMAND ----------
-
-len([pos for i, pos in can_reach if i == 0])
-
-# COMMAND ----------
-
-start_pos
-
-# COMMAND ----------
-
-Os = {pos for i, pos in can_reach if i == 0}
-lines = inp.splitlines()
-for row, line in enumerate(lines):
-  for col, c in enumerate(line):
-    print('O' if (row, col) in Os else c, end='')
-  print()
-
-# COMMAND ----------
-
-lines = inp.splitlines()
+plots = {(row, col) for row, line in enumerate(lines) for col, c in enumerate(line) if c in 'S.'}
+start_pos = next((row, col) for row, line in enumerate(lines) for col, c in enumerate(line) if c == 'S')
 
 side_len = len(lines) # Assume square
 target = 26501365
 remainder = 26501365 % side_len
 
-# COMMAND ----------
-
-# PART 2
-import collections
-
-plots = {(row, col) for row, line in enumerate(inp.splitlines()) for col, c in enumerate(line) if c in 'S.'}
-start_pos = next((row, col) for row, line in enumerate(inp.splitlines()) for col, c in enumerate(line) if c == 'S')
-
-lines = inp.splitlines()
-n_rows = len(lines)
-n_cols = len(lines[0])
-
 q = collections.deque([(start_pos, 0)])
-can_reach = set() # steps%2, pos
+seen = set() # (steps % 2, pos)
 
 cur_step = 0
 reach_counts = []
@@ -526,28 +234,17 @@ while q:
 
   if steps > cur_step:
     if cur_step == 64:
-      print(f'{len({pos for i, pos in can_reach if i == cur_step % 2})=}')
+      answer1 = len({pos for i, pos in seen if i == cur_step % 2})
     if cur_step % side_len == remainder:
-      reach_counts.append(len({pos for i, pos in can_reach if i == cur_step % 2}))
+      reach_counts.append(len({pos for i, pos in seen if i == cur_step % 2}))
     cur_step = steps
 
-  if (pos[0] % side_len, pos[1] % side_len) not in plots:
+  if (pos[0] % side_len, pos[1] % side_len) not in plots or (steps % 2, pos) in seen:
     continue
+  seen.add((steps%2, pos))
 
-  if (steps%2, pos) in can_reach:
-    #print(pos)
-    continue
-  can_reach.add((steps%2, pos))
-
-  # if steps > 6:
-  # if cur_step > 100:
-  #   #options.add(pos)
-  #   continue
   if len(reach_counts) == 3:
-    print(cur_step)
     break
-
-
 
   for d in 'NESW':
     new_pos = (
@@ -556,27 +253,75 @@ while q:
     )
     q.append((new_pos, steps + 1))
 
-len({pos for i, pos in can_reach if i == 0})
+print(answer1)
 
 # COMMAND ----------
 
-reach_counts
+# MAGIC %md <article class="day-desc"><h2 id="part2">--- Part Two ---</h2><p>The Elf seems confused by your answer until he realizes his mistake: he was reading from a <span title="Next up: 729.">list</span> of his favorite numbers that are both perfect squares and perfect cubes, not his step counter.</p>
+# MAGIC <p>The <em>actual</em> number of steps he needs to get today is exactly <code><em>26501365</em></code>.</p>
+# MAGIC <p>He also points out that the garden plots and rocks are set up so that the map <em>repeats infinitely</em> in every direction.</p>
+# MAGIC <p>So, if you were to look one additional map-width or map-height out from the edge of the example map above, you would find that it keeps repeating:</p>
+# MAGIC <pre><code>.................................
+# MAGIC .....###.#......###.#......###.#.
+# MAGIC .###.##..#..###.##..#..###.##..#.
+# MAGIC ..#.#...#....#.#...#....#.#...#..
+# MAGIC ....#.#........#.#........#.#....
+# MAGIC .##...####..##...####..##...####.
+# MAGIC .##..#...#..##..#...#..##..#...#.
+# MAGIC .......##.........##.........##..
+# MAGIC .##.#.####..##.#.####..##.#.####.
+# MAGIC .##..##.##..##..##.##..##..##.##.
+# MAGIC .................................
+# MAGIC .................................
+# MAGIC .....###.#......###.#......###.#.
+# MAGIC .###.##..#..###.##..#..###.##..#.
+# MAGIC ..#.#...#....#.#...#....#.#...#..
+# MAGIC ....#.#........#.#........#.#....
+# MAGIC .##...####..##..S####..##...####.
+# MAGIC .##..#...#..##..#...#..##..#...#.
+# MAGIC .......##.........##.........##..
+# MAGIC .##.#.####..##.#.####..##.#.####.
+# MAGIC .##..##.##..##..##.##..##..##.##.
+# MAGIC .................................
+# MAGIC .................................
+# MAGIC .....###.#......###.#......###.#.
+# MAGIC .###.##..#..###.##..#..###.##..#.
+# MAGIC ..#.#...#....#.#...#....#.#...#..
+# MAGIC ....#.#........#.#........#.#....
+# MAGIC .##...####..##...####..##...####.
+# MAGIC .##..#...#..##..#...#..##..#...#.
+# MAGIC .......##.........##.........##..
+# MAGIC .##.#.####..##.#.####..##.#.####.
+# MAGIC .##..##.##..##..##.##..##..##.##.
+# MAGIC .................................
+# MAGIC </code></pre>
+# MAGIC <p>This is just a tiny three-map-by-three-map slice of the inexplicably-infinite farm layout; garden plots and rocks repeat as far as you can see. The Elf still starts on the one middle tile marked <code>S</code>, though - every other repeated <code>S</code> is replaced with a normal garden plot (<code>.</code>).</p>
+# MAGIC <p>Here are the number of reachable garden plots in this new infinite version of the example map for different numbers of steps:</p>
+# MAGIC <ul>
+# MAGIC <li>In exactly <code>6</code> steps, he can still reach <code><em>16</em></code> garden plots.</li>
+# MAGIC <li>In exactly <code>10</code> steps, he can reach any of <code><em>50</em></code> garden plots.</li>
+# MAGIC <li>In exactly <code>50</code> steps, he can reach <code><em>1594</em></code> garden plots.</li>
+# MAGIC <li>In exactly <code>100</code> steps, he can reach <code><em>6536</em></code> garden plots.</li>
+# MAGIC <li>In exactly <code>500</code> steps, he can reach <code><em>167004</em></code> garden plots.</li>
+# MAGIC <li>In exactly <code>1000</code> steps, he can reach <code><em>668697</em></code> garden plots.</li>
+# MAGIC <li>In exactly <code>5000</code> steps, he can reach <code><em>16733044</em></code> garden plots.</li>
+# MAGIC </ul>
+# MAGIC <p>However, the step count the Elf needs is much larger! Starting from the garden plot marked <code>S</code> on your infinite map, <em>how many garden plots could the Elf reach in exactly <code>26501365</code> steps?</em></p>
+# MAGIC </article>
 
 # COMMAND ----------
 
-def f(x):
-  return 3724 - 14934 * x + 15024 * x**2
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 
-f((target - remainder) // side_len)
+X = np.array([0, 1, 2]).reshape(-1, 1)
+Y = np.array(reach_counts).reshape(-1, 1)
+X_poly = PolynomialFeatures(degree=2).fit_transform(X)
+model = LinearRegression()
+model.fit(X_poly, Y)
 
-# COMMAND ----------
-
-f(3)
-
-# COMMAND ----------
-
-f(202300+1)
-
-# COMMAND ----------
-
-(target - remainder) // side_len
+z = target // side_len
+X2 = PolynomialFeatures(degree=2).fit_transform(np.array(z).reshape(1, -1))
+answer2 = int(model.predict(X2)[0][0])
+print(answer2)
