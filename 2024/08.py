37b935051d33781c0dc2e953c17699ad10c666bc
Paul Foster <paulfstr@gmail.com>
2024-12-01 10:48:53 +1100

    Complete 2024 quest 8

---
+++ 2024/08.py
@@ -0,0 +1,91 @@
+with open("./2024/input/everybody_codes_e2024_q08_p1.txt") as f:
+    lines = f.read().splitlines()
+
+# lines = '13'.splitlines() # FIXME
+
+target = int(lines[0])
+
+blocks = 1
+width = 1
+while blocks < target:
+    width += 2
+    blocks += width
+
+remaining = blocks - target
+answer1 = remaining * width
+print(answer1)
+
+
+# Part 2
+
+
+with open("./2024/input/everybody_codes_e2024_q08_p2.txt") as f:
+    lines = f.read().splitlines()
+
+mod = 1111
+target = 20240000
+priests = int(lines[0])
+
+# # FIXME: REMOVE
+# mod = 5
+# target = 50
+# priests = 3
+
+blocks = 1
+width = 1
+extra_height = 1
+while blocks < target:
+    extra_height = (extra_height * priests) % mod
+    width += 2
+    blocks += width * extra_height
+    # FIXME: REMOVE
+    # if width >= 100:
+    #     break
+
+remaining = blocks - target
+answer2 = remaining * width
+
+print(answer2)
+
+
+# Part 3
+
+
+import collections
+
+
+with open("./2024/input/everybody_codes_e2024_q08_p3.txt") as f:
+    lines = f.read().splitlines()
+
+extra = mod = 10
+target = 202400000
+priests = int(lines[0])
+
+# # FIXME: REMOVE
+# extra = mod = 5
+# target = 160
+# priests = 2
+
+heights = collections.deque([1])
+
+blocks = 1
+width = 1
+extra_height = 1
+while blocks < target:
+    extra_height = (extra_height * priests) % mod + extra # AKA thickness
+    width += 2
+    blocks += width * extra_height
+    
+    heights.appendleft(0)
+    heights.append(0)
+    for i in range(len(heights)):
+        heights[i] += extra_height
+    
+to_remove = 0
+for i in range(1, len(heights) - 1):
+    to_remove += (priests * width * heights[i]) % mod
+
+required_to_complete_shell = blocks - to_remove
+
+answer3 = target - required_to_complete_shell
+print(answer3)
