5b12b94f7bb192e9e096fc06e3f8fbb9e3d046ce
Paul Foster <paulfstr@gmail.com>
2024-12-01 01:21:01 +1100

    Simplify 2024 quest 7

--- 2024/07.py
+++ 2024/07.py
@@ -1,10 +1,63 @@
 import itertools
 
 
+with open("./2024/input/everybody_codes_e2024_q07_p1.txt") as f:
+    lines = f.read().splitlines()
+
+def calculate_score(ops):
+    total = 0
+    score = 10
+    it = itertools.cycle(ops)
+    for _ in range(10):
+        op = next(it)
+        if op == '+':
+            score += 1
+        elif op == '-':
+            score -= 1
+        score = max(score, 0)
+        total += score
+    return total
+
+devices = [] # (score, name)
+for line in lines:
+    name, ops = line.split(':')
+    score = calculate_score(ops.split(','))
+    devices.append((score, name))
+
+devices.sort(reverse=True)
+answer1 = ''.join(name for _, name in devices)
+print(answer1)
+
+
+# Part 2
+
+
+def parse_track(track_lines):
+    r, c = 0, 1
+    prev = 0, 0
+    result = []
+    while track_lines[r][c] != 'S':
+        result.append(track_lines[r][c])
+        for dr, dc in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+            r2 = r + dr
+            c2 = c + dc
+            if not (0 <= r2 < len(track_lines)) or not (0 <= c2 < len(track_lines[r2])):
+                continue
+            if (r2, c2) == prev or track_lines[r2][c2] == ' ':
+                continue
+            prev = r, c
+            r, c = r2, c2
+            break
+        else:
+            print(f'UNABLE TO FIND NEXT POSITION')
+            return
+    return result + ['=']
+
+
 with open("./2024/input/everybody_codes_e2024_q07_p2.txt") as f:
     lines = f.read().splitlines()
 
-track = '''S-=++=-==++=++=-=+=-=+=+=--=-=++=-==++=-+=-=+=-=+=+=++=-+==++=++=-=-=--
+track_lines = '''S-=++=-==++=++=-=+=-=+=+=--=-=++=-==++=-+=-=+=-=+=+=++=-+==++=++=-=-=--
 -                                                                     -
 =                                                                     =
 +                                                                     +
@@ -14,30 +67,14 @@ track = '''S-=++=-==++=++=-=+=-=+=+=--=-=++=-==++=-+=-=+=-=+=+=++=-+==++=++=-=-=
 -                                                                     -
 --==++++==+=+++-=+=-=+=-+-=+-=+-=+=-=+=--=+++=++=+++==++==--=+=++==+++-'''.splitlines()
 
-# # FIXME: Remove
-# lines = '''A:+,-,=,=
-# B:+,=,-,+
-# C:=,-,+,+
-# D:=,=,=,+'''.splitlines()
-# track = '''S+===
-# -   +
-# =+=-+'''.splitlines()
-
-track_s = track[0][1:]
-for i in range(1, len(track) - 1):
-    track_s += track[i][-1]
-track_s += track[len(track) - 1][::-1]
-for i in reversed(range(1, len(track) - 1)):
-    track_s += track[i][0]
-track_s += '='
+track = parse_track(track_lines)
 
 def calculate_score(ops):
     total = 0
     score = 10
     it = itertools.cycle(ops)
-    # for _ in range(1): # FIXME: TEST
     for _ in range(10):
-        for op in track_s:
+        for op in track:
             t = next(it)
             if op == '=':
                 op = t
@@ -47,11 +84,8 @@ def calculate_score(ops):
             elif op == '-':
                 score -= 1
             score = max(score, 0)
-            # print(f'{score=}')
             total += score
     return total
-
-# calculate_score('+,-,=,='.split(','))
 
 devices = [] # (score, name)
 for line in lines:
@@ -64,14 +98,13 @@ answer2 = ''.join(name for _, name in devices)
 print(answer2)
 
 
-
-# # Part 3
+# Part 3
 
 
 with open("./2024/input/everybody_codes_e2024_q07_p3.txt") as f:
     lines = f.read().splitlines()
 
-track = '''S+= +=-== +=++=     =+=+=--=    =-= ++=     +=-  =+=++=-+==+ =++=-=-=--
+track_lines = '''S+= +=-== +=++=     =+=+=--=    =-= ++=     +=-  =+=++=-+==+ =++=-=-=--
 - + +   + =   =     =      =   == = - -     - =  =         =-=        -
 = + + +-- =-= ==-==-= --++ +  == == = +     - =  =    ==++=    =++=-=++
 + + + =     +         =  + + == == ++ =     = =  ==   =   = =++=
@@ -82,29 +115,7 @@ track = '''S+= +=-== +=++=     =+=+=--=    =-= ++=     +=-  =+=++=-+==+ =++=-=-=
 -               = + + =   +  -  = + = = +   =        +     =          -
 --==++++==+=+++-= =-= =-+-=  =+-= =-= =--   +=++=+++==     -=+=++==+++-'''.splitlines()
 
-def parse_track(track):
-    r, c = 0, 1
-    prev = 0, 0
-    result = []
-    while track[r][c] != 'S':
-        result.append(track[r][c])
-        print(r, c)
-        for dr, dc in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
-            r2 = r + dr
-            c2 = c + dc
-            if not (0 <= r2 < len(track)) or not (0 <= c2 < len(track[r2])):
-                continue
-            if (r2, c2) == prev or track[r2][c2] == ' ':
-                continue
-            prev = r, c
-            r, c = r2, c2
-            break
-        else:
-            print(f'UNABLE TO FIND NEXT POSITION')
-            return
-    return result + ['=']
-
-track_s = parse_track(track)
+track = parse_track(track_lines)
 
 
 def calculate_score(ops):
@@ -113,7 +124,7 @@ def calculate_score(ops):
     it = itertools.cycle(ops)
     # for _ in range(2024):
     for _ in range(11): # 2024 is divisible by 11
-        for op in track_s:
+        for op in track:
             t = next(it)
             if op == '=':
                 op = t
@@ -160,8 +171,6 @@ def backtrack():
 
 backtrack()
 
-# len(combinations)
-# len({tuple(comb) for comb in combinations})
 answer3 = 0
 for comb in combinations:
     score = calculate_score(comb)
