660554fe764ee51256a07c5191498e91d58c6e20
Paul Foster <paulfstr@gmail.com>
2024-11-30 21:07:01 +1100

    Simplify 2024 quest 4

--- 2024/04.py
+++ 2024/04.py
@@ -2,14 +2,8 @@
 with open("./2024/input/everybody_codes_e2024_q04_p1.txt") as f:
     lines = f.read().splitlines()
 
-# lines = '''3
-# 4
-# 7
-# 8'''.splitlines()
-
-nums = [int(x) for x in lines]
-answer1 = sum(x - min(nums) for x in nums)
-
+nums = [int(s) for s in lines]
+answer1 = sum(num - min(nums) for num in nums)
 print(answer1)
 
 
@@ -19,8 +13,8 @@ print(answer1)
 with open("./2024/input/everybody_codes_e2024_q04_p2.txt") as f:
     lines = f.read().splitlines()
 
-nums = [int(x) for x in lines]
-answer2 = sum(x - min(nums) for x in nums)
+nums = [int(line) for line in lines]
+answer2 = sum(num - min(nums) for num in nums)
 print(answer2)
 
 
@@ -30,22 +24,6 @@ print(answer2)
 with open("./2024/input/everybody_codes_e2024_q04_p3.txt") as f:
     lines = f.read().splitlines()
 
-# lines = '''2
-# 4
-# 5
-# 6
-# 8'''.splitlines()
-
-nums = [int(x) for x in lines]
-# z = sum(nums) // len(nums)
-# answer3 = sum(abs(x - z) for x in nums)
-
-answer3 = float('inf')
-for z in range(min(nums), max(nums) + 1):
-    s = sum(abs(x - z) for x in nums)
-    answer3 = min(answer3, s)
-
-
-# [abs(x - z) for x in nums]
-
+nums = [int(line) for line in lines]
+answer3 = min(sum(abs(num - target) for num in nums) for target in set(nums))
 print(answer3)
