8ee07abefad31bb2639909ad69b69fff4fefb8b0
Paul Foster <paulfstr@gmail.com>
2024-12-01 00:10:59 +1100

    Simplify 2024 quest 6

--- 2024/06.py
+++ 2024/06.py
@@ -1,41 +1,33 @@
 import collections
 
+
+def find_best_path(lines: list[str]) -> tuple[str]:
+    m = collections.defaultdict(list)
+    for line in lines:
+        a, b = line.split(':')
+        for c in b.split(','):
+            m[a].append(c)
+
+    q = collections.deque([('RR',)])
+    while q:
+        candidates = []
+        for _ in range(len(q)):
+            path = q.popleft()
+            node = path[-1]
+            if node == '@':
+                candidates.append(path)
+                continue
+            for node2 in m[node]:
+                q.append(path + (node2,))
+        if len(candidates) == 1:
+            return candidates[0]
+
+
 with open("./2024/input/everybody_codes_e2024_q06_p1.txt") as f:
     lines = f.read().splitlines()
 
-# lines = '''RR:A,B,C
-# A:D,E
-# B:F,@
-# C:G,H
-# D:@
-# E:@
-# F:@
-# G:@
-# H:@'''.splitlines()
-
-m = collections.defaultdict(list)
-for line in lines:
-    a, b = line.split(':')
-    for c in b.split(','):
-        m[a].append(c)
-print(f'{m=}')
-
-q = collections.deque([('RR',)])
-while q:
-    candidates = []
-    for _ in range(len(q)):
-        path = q.popleft()
-        node = path[-1]
-        if node == '@':
-            candidates.append(path)
-            continue
-        for node2 in m[node]:
-            q.append(path + (node2,))
-    if len(candidates) == 1:
-        answer1 = candidates[0]
-        break
-
-answer1 = ''.join(answer1)
+path = find_best_path(lines)
+answer1 = ''.join(path)
 print(answer1)
 
 
@@ -45,32 +37,8 @@ print(answer1)
 with open("./2024/input/everybody_codes_e2024_q06_p2.txt") as f:
     lines = f.read().splitlines()
 
-
-m = collections.defaultdict(list)
-for line in lines:
-    a, b = line.split(':')
-    for c in b.split(','):
-        m[a].append(c)
-# print(f'{m=}')
-
-q = collections.deque([('RR',)])
-while q:
-    candidates = []
-    for _ in range(len(q)):
-        path = q.popleft()
-        node = path[-1]
-        if node == '@':
-            candidates.append(path)
-            continue
-        for node2 in m[node]:
-            q.append(path + (node2,))
-    if len(candidates) == 1:
-        answer2 = candidates[0]
-        break
-
-answer2 = ''.join(c[0] for c in answer2)
-
-
+path = find_best_path(lines)
+answer2 = ''.join(node[0] for node in path)
 print(answer2)
 
 
@@ -80,27 +48,6 @@ print(answer2)
 with open("./2024/input/everybody_codes_e2024_q06_p3.txt") as f:
     lines = f.read().splitlines()
 
-m = collections.defaultdict(list)
-for line in lines:
-    a, b = line.split(':')
-    for c in b.split(','):
-        m[a].append(c)
-# print(f'{m=}')
-
-q = collections.deque([('RR',)])
-while q:
-    candidates = []
-    for _ in range(len(q)):
-        path = q.popleft()
-        node = path[-1]
-        if node == '@':
-            candidates.append(path)
-            continue
-        for node2 in m[node]:
-            q.append(path + (node2,))
-    if len(candidates) == 1:
-        answer3 = candidates[0]
-        break
-
-answer3 = ''.join(c[0] for c in answer3)
+path = find_best_path(lines)
+answer3 = ''.join(node[0] for node in path)
 print(answer3)
