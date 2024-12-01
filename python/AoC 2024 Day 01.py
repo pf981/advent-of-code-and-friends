c7f8f6f4ab1adbe465b0fd1694515e82d9150a72
Paul Foster <paulfstr@gmail.com>
2024-12-01 16:09:54 +1100

    Tidy code

--- python/AoC 2024 Day 01.py
+++ python/AoC 2024 Day 01.py
@@ -1,15 +1,7 @@
 from aocd import get_data, submit
 
+
 inp = get_data(day=1, year=2024)
-
-# FIXME: REMOVE
-# inp = '''3   4
-# 4   3
-# 2   5
-# 1   3
-# 3   9
-# 3   3'''
-
 lines = inp.splitlines()
 
 l = []
@@ -22,22 +14,20 @@ for line in lines:
 l.sort()
 r.sort()
 
-answer1 = 0
-for a, b in zip(l, r):
-    answer1 += abs(b - a)
-
+answer1 = sum(abs(b - a) for a, b in zip(l, r))
 print(answer1)
 
-# submit(answer1, part='a', day=1, year=2024)
+submit(answer1, part='a', day=1, year=2024)
 
 
 # Part 2
-import collections
-answer2 = 0
-cnts = collections.Counter(r)
-for a in l:
-    answer2 += abs(a * cnts[a])
 
+
+import collections
+
+
+counts = collections.Counter(r)
+answer2 = sum(abs(a * counts[a]) for a in l)
 print(answer2)
 
-# submit(answer2, part='b', day=1, year=2024)
+submit(answer2, part='b', day=1, year=2024)
