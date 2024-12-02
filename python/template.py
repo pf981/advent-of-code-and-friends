acb7b1c54dd9bc670a6160ca3386fd04a37ea335
Paul Foster <paulfstr@gmail.com>
2024-12-02 16:10:17 +1100

    Add template

---
+++ python/template.py
@@ -0,0 +1,33 @@
+from aocd import get_data, submit
+
+
+inp = get_data(day=x, year=2024)
+lines = inp.splitlines()
+
+l = []
+r = []
+for line in lines:
+    a, b = line.split()
+    l.append(int(a))
+    r.append(int(b))
+
+l.sort()
+r.sort()
+
+answer1 = sum(abs(b - a) for a, b in zip(l, r))
+print(answer1)
+
+submit(answer1, part='a', day=x, year=2024)
+
+
+# Part 2
+
+
+import collections
+
+
+counts = collections.Counter(r)
+answer2 = sum(abs(a * counts[a]) for a in l)
+print(answer2)
+
+submit(answer2, part='b', day=x, year=2024)
