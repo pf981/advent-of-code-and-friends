bd604ca4d4e4768257e48faebd7afe2b5c9510bf
Paul Foster <paulfstr@gmail.com>
2024-12-02 18:16:12 +1100

    Some progress on 2024 quest 10 part 3

--- 2024/10.py
+++ 2024/10.py
@@ -129,11 +129,44 @@ def get_word_impl(i: int, candidates: list[set[int]], seen: set[int]) -> str | N
         seen.remove(candidate)
     
     return None
-        
 
+
+def get_words_impl(i: int, candidates: list[set[int]], seen: set[int]) -> list[str]:
+    if i == len(candidates):
+        return ['']
+    
+    words = []
+    for candidate in candidates[i]:
+        if candidate in seen:
+            continue
+
+        seen.add(candidate)
+        
+        for result in get_words_impl(i + 1, candidates, seen):
+            words.append(candidate + result)
+        
+        seen.remove(candidate)
+    
+    return words
+        
 def get_candidates(grid: list[str]) -> list[set] | None:
     candidates = [set() for _ in range(16)]
 
+    # Check for duplicates
+    all_rows = collections.Counter()
+    all_cols = collections.Counter()
+    for i in range(2, 6):
+        for val in [grid[i][0], grid[i][1], grid[i][-1], grid[i][-2]]:
+            all_rows[val] += 1
+        for val in [grid[0][i], grid[1][i], grid[-1][i], grid[-2][i]]:
+            all_cols[val] += 1
+    all_rows['?'] = 1
+    all_cols['?'] = 1
+    print(f'{all_rows=}')
+    print(f'{all_cols=}')
+    if any(count != 1 for count in all_rows.values()) or any(count != 1 for count in all_cols.values()):
+        return None
+    
     for i in range(16):
         r = (i // 4) + 2
         c = (i % 4) + 2
@@ -154,6 +187,7 @@ def get_candidates(grid: list[str]) -> list[set] | None:
             cur_candidates.update(row_candidates)
         if '?' in row_candidates:
             cur_candidates.update(col_candidates)
+        cur_candidates.discard('?')
 
         candidates[i] = cur_candidates
 
@@ -174,6 +208,15 @@ def get_word(grid: list[str]) -> str | None:
     return get_word_impl(0, candidates, set())
 
 
+def get_words(grid: list[str]) -> list[str]:
+    candidates = get_candidates(grid)
+
+    if not candidates:
+        return []
+
+    return get_words_impl(0, candidates, set())
+
+
 def get_power(word: str) -> int:
     power = 0
     for i, ch in enumerate(word, 1):
@@ -190,15 +233,70 @@ for r in range(0, len(lines) - 2, 6):
             grid.append(lines[r2][c:c + 8])
         grids.append(grid)
 
+# grid_words = [get_words(grid) for grid in grids]
+# # print(grid_words)
+# for words in grid_words:
+#     print(words)
+
+good = 0
+missing = 0
+for i, grid in enumerate(grids):
+    candidates = get_candidates(grid)
+    if not candidates:
+        continue
+    word = get_word_impl(0, candidates, set())
+    good += 1
+    if not word:
+        missing += 1
+        print(f'MISSING WORD {i=}')
+        # for line in grid:
+        #     print(line)
+print(good, missing) # 116 2; now it's 114, 0 as required
+
+grids[121]
+
+# ['** VNZ? **',
+#  '** TRWY **',
+
+#  'WH .... BV',
+#  'FC .... ?G',
+#  'XB .... Z?',
+#  'LJ .... ?R',
+
+#  '** SBJ? **',
+#  '** KCPY **']
+get_candidates(grids[121])
+
+
 answer3 = 0
-# i=0
 for grid in grids:
     word = get_word(grid)
-    # print(f'{i} {word} {get_power(word) if word else 0}')
-    # i+=1
     if word:
         answer3 += get_power(word)
 print(answer3)
+
+# 2 ys
+# ** VNZ? **
+# ** TRWY **
+
+# WH ..w. BV
+# FC ...y ?G
+# XB ..z. Z?
+# LJ ...y ?R
+
+# ** SBJ? **
+# ** KCPY **
+
+# **LSCY**
+# **HBR?**
+
+# ?M....Z?
+# JW....?K
+# CS....?M
+# TX....?N
+
+# **VWY?**
+# **FQZK**
 
 # 211408
 # Your answer length is: correct
@@ -206,73 +304,10 @@ print(answer3)
 
 # 211801
 # That's not the right answer...
-
 # Your answer length is: correct
 # The first character of your answer is: correct
 
-
-# get_word(grids[-1])
-# grids[-1]
-# # ['**PVTN**',
-# #  '**SJQH**',
-# #  'RW....PB',
-# #  'TS....GY',
-# #  'VL....XS',
-# #  'BH....?Z',
-# #  '**T?XP**',
-# #  '**RYJW**']
-
-# get_power('LWGVXSHBPJQKNFZM')
-# get_power('DQWLXCNHVKJTGFSZ')
-
-grids[21]
-# ['**JRVG**',
-#  '**NZMD**',
-#  'TP....TX',
-#  'VJ....?L',
-#  'GC....YW',
-#  'RL....CS',
-#  '**HTZK**',
-#  '**XJLG**']
-
-grids[22]
-# ['** WMRS **',
-#  '** JPBF **',
-
-#  'TX .... M?',
-#  '?L .... HK',
-#  'YW W... JY',
-#  'CS W... WX',
-
-#  '** NLPR **',
-#  '** WJQH **']
-
-
-grids[23]
-# ['** KRBG **',
-#  '** TDZM **',
-
-#  'M? .... QN',
-#  'HK .... K?',
-#  'JY .... PJ',
-#  'WX .... TY',
-
-#  '** HJVC **',
-#  '** RSMB **']
-
-
-grids[181]
-# ['** SDRC **',
-#  '** G?TY **',
-
-#  'JK .... MD',
-#  'NX .... WT',
-#  'VW .... RG',
-#  'TH .... HP',
-
-#  '** SYMD **',
-#  '** Q?PX **']
-
-# "There cannot be two identical symbols in the runic word."
-
-# NOTE: The issue is that each ? maps to EXACTLY one symbol. So tiles which share a ? must have that ? be the same character
+# 209611
+# That's not the right answer...
+# Your answer length is: correct
+# The first character of your answer is: correct
