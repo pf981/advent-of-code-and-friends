de3e7a23e8b8e4686d904d0c6371ffd6dac59ab7
Paul Foster <paulfstr@gmail.com>
2024-12-05 16:30:37 +1100

    Tidy code

--- python/AoC 2024 Day 05.py
+++ python/AoC 2024 Day 05.py
@@ -1,84 +1,49 @@
 from aocd import get_data, submit
+import collections
 
 
 inp = get_data(day=5, year=2024)
+rules_lines, numbers_lines = inp.split('\n\n')
 
-# inp = '''47|53
-# 97|13
-# 97|61
-# 97|47
-# 75|29
-# 61|13
-# 75|53
-# 29|13
-# 97|29
-# 53|29
-# 61|53
-# 97|53
-# 61|29
-# 47|13
-# 75|47
-# 97|75
-# 47|61
-# 75|61
-# 47|29
-# 75|13
-# 53|13\n\n75,47,61,53,29
-# 97,61,53,29,13
-# 75,29,13
-# 75,97,47,61,53
-# 61,13,29
-# 97,13,75,29,47
-# '''
-
-len(inp.split('\n\n'))
-
-a, b = inp.split('\n\n')
-a = a.splitlines()
-b = b.splitlines()
-# lines = inp.splitlines()
-import collections
-bad = collections.defaultdict(list)
-
-for line in a:
-    x, y = line.split('|')
-    # int(x)
-    bad[int(x)].append(int(y)) # If you see x before y, it is bad
+m = collections.defaultdict(list)
+for line in rules_lines.splitlines():
+    a, b = line.split('|')
+    m[int(a)].append(int(b))
 
 
-def is_bad(nums):
+def is_valid(nums):
     seen = set()
     for num in nums:
-        for z in bad[num]:
+        for z in m[num]:
             if z in seen:
-                return True
+                return False
         seen.add(num)
-    return False
+    return True
+
 
 incorrectly_ordered = []
 result = []
-for line in b:
-    nums = [int(x) for x in line.split(',')]
-    if is_bad(nums):
+for line in numbers_lines.splitlines():
+    nums = [int(num) for num in line.split(',')]
+    if is_valid(nums):
+        result.append(nums[len(nums) // 2])
+    else:
         incorrectly_ordered.append(nums)
-        continue
-    result.append(nums[len(nums) // 2])
-    
-# result
-
 
 answer1 = sum(result)
 print(answer1)
 
-# submit(answer1, part='a', day=5, year=2024)
+submit(answer1, part='a', day=5, year=2024)
 
 
 # Part 2
+
+
 def reorder_impl(num, nums, seen, res):
     if num in seen:
         return
     seen.add(num)
-    for num2 in bad[num]:
+    for num2 in m[num]:
         if num2 in nums:
             reorder_impl(num2, nums, seen, res)
     res.append(num)
@@ -87,19 +52,16 @@ def reorder_impl(num, nums, seen, res):
 def reorder(nums):
     seen = set()
     res = []
-    for i in nums:
-        reorder_impl(i, nums, seen, res)
+    for num in nums:
+        reorder_impl(num, nums, seen, res)
     return res
 
-result2 = []
+result = []
 for nums in incorrectly_ordered:
-    l = reorder(nums.copy())
-    print(l)
-    result2.append(l[len(l) // 2])
+    l = reorder(nums)
+    result.append(l[len(l) // 2])
 
-# reorder([75, 97, 47, 61, 53])
-
-answer2 = sum(result2)
+answer2 = sum(result)
 print(answer2)
 
-# submit(answer2, part='b', day=5, year=2024)
+submit(answer2, part='b', day=5, year=2024)
