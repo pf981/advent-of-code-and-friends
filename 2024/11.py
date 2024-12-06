0e44340d7685524fcda71541a29d2db3144accd7
Paul Foster <paulfstr@gmail.com>
2024-12-06 13:44:19 +1100

    Simplify

--- 2024/11.py
+++ 2024/11.py
@@ -1,29 +1,32 @@
 import collections
 
 
+def parse_lines(lines: list[str]) -> dict[str, list[str]]:
+    m = {}
+    for line in lines:
+        a, b = line.split(':')
+        m[a] = collections.Counter(b.split(','))
+    return m
+
+
+def simulate(start: str, n: int, m: dict[str, list[str]]) -> int:
+    counts = collections.Counter([start])
+
+    for _ in range(n):
+        counts2 = collections.Counter()
+        for c, cnt in counts.items():
+            for c2, cnt2 in m[c].items():
+                counts2[c2] += cnt * cnt2
+        counts = counts2
+
+    return sum(counts.values())
+
+
 with open("./2024/input/everybody_codes_e2024_q11_p1.txt") as f:
     lines = f.read().splitlines()
 
-# lines = '''A:B,C
-# B:C,A
-# C:A'''.splitlines()
-
-m = {}
-for line in lines:
-    # print(line)
-    a, b = line.split(':')
-    m[a] = collections.Counter(b.split(','))
-
-counts = collections.Counter('A')
-
-for _ in range(4):
-    counts2 = collections.Counter()
-    for c, cnt in counts.items():
-        for c2, cnt2 in m[c].items():
-            counts2[c2] += cnt * cnt2
-    counts = counts2
-
-answer1 = sum(counts.values())
+m = parse_lines(lines)
+answer1 = simulate('A', 4, m)
 print(answer1)
 
 
@@ -33,22 +36,8 @@ print(answer1)
 with open("./2024/input/everybody_codes_e2024_q11_p2.txt") as f:
     lines = f.read().splitlines()
 
-m = {}
-for line in lines:
-    # print(line)
-    a, b = line.split(':')
-    m[a] = collections.Counter(b.split(','))
-
-counts = collections.Counter('Z')
-
-for _ in range(10):
-    counts2 = collections.Counter()
-    for c, cnt in counts.items():
-        for c2, cnt2 in m[c].items():
-            counts2[c2] += cnt * cnt2
-    counts = counts2
-
-answer2 = sum(counts.values())
+m = parse_lines(lines)
+answer2 = simulate('Z', 10, m)
 print(answer2)
 
 
@@ -58,33 +47,7 @@ print(answer2)
 with open("./2024/input/everybody_codes_e2024_q11_p3.txt") as f:
     lines = f.read().splitlines()
 
-# lines = '''A:B,C
-# B:C,A,A
-# C:A'''.splitlines()
-
-m = collections.defaultdict()
-for line in lines:
-    # print(line)
-    a, b = line.split(':')
-    m[a] = collections.Counter(b.split(','))
-
-
-def sim(start):
-    counts = collections.Counter([start])
-
-    for _ in range(20):
-        counts2 = collections.Counter()
-        for c, cnt in counts.items():
-            for c2, cnt2 in m[c].items():
-                counts2[c2] += cnt * cnt2
-        counts = counts2
-
-    return sum(counts.values())
-
-l = []
-for start in m:
-    l.append(sim(start))
-
-
-answer3 = max(l) - min(l)
+m = parse_lines(lines)
+populations = [simulate(start, 20, m) for start in m]
+answer3 = max(populations) - min(populations)
 print(answer3)
