b978844ec44ce2eac7c488f133ec0dcaab637240
Paul Foster <paulfstr@gmail.com>
2024-12-06 16:47:51 +1100

    Simplify 2024 day 6

--- python/AoC 2024 Day 06.py
+++ python/AoC 2024 Day 06.py
@@ -1,144 +1,59 @@
 from aocd import get_data, submit
 
 
+def simulate(lines: list[list[str]], start: tuple[int]) -> set[tuple[int]] | None:
+    seen_states = set()
+    seen_positions = set()
+    r, c  = start
+    direction = 'N'
+    while 0 <= r < nrows and 0 <= c < ncols:
+        seen_positions.add((r, c))
+
+        if (r, c, direction) in seen_states:
+            return None
+        seen_states.add((r, c, direction))
+
+        while True:
+            r2 = r + (direction == 'S') - (direction == 'N')
+            c2 = c + (direction == 'E') - (direction == 'W')
+
+            if not (0 <= r2 < nrows) or not ( 0 <= c2 < ncols):
+                break
+            if lines[r2][c2] != '#':
+                break
+            direction = {'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'}[direction]
+
+        r = r2
+        c = c2
+    return seen_positions
+
+
 inp = get_data(day=6, year=2024)
-
-# inp = '''....#.....
-# .........#
-# ..........
-# ..#.......
-# .......#..
-# ..........
-# .#..^.....
-# ........#.
-# #.........
-# ......#...
-# '''
-
-lines = inp.splitlines()
+lines = [list(line) for line in inp.splitlines()]
 nrows = len(lines)
 ncols = len(lines[0])
 
-start = 0, 0
-for row, line in enumerate(lines):
-    for col, c in enumerate(line):
-        if c == '^':
-            start = row, col
-            break
-    else:
-        continue
-    break
-seen = set()
-r, c  = start
-direction = 'N'
-rot = {
-    'N': 'E',
-    'E': 'S',
-    'S': 'W',
-    'W': 'N',
-}
+start = next((r, c) for r, line in enumerate(lines) for c, ch in enumerate(line) if ch == '^')
+positions = simulate(lines, start)
 
-while 0 <= r < nrows and 0 <= c < ncols:
-    seen.add((r, c))
-    
-    r2 = r + (direction == 'S') - (direction == 'N')
-    c2 = c + (direction == 'E') - (direction == 'W')
-    if not (0 <= r2 < nrows) or not ( 0 <= c2 < ncols):
-        break
-    i = 0
-    while lines[r2][c2] == '#':
-        if i == 10:
-            break
-        i += 1
-        # print(f'{r2=} {c2=}')
-        direction = rot[direction]
-        r2 = r + (direction == 'S') - (direction == 'N')
-        c2 = c + (direction == 'E') - (direction == 'W')
-    r = r2
-    c = c2
-
-answer1 = len(seen)
+answer1 = len(positions)
 print(answer1)
 
-# submit(answer1, part='a', day=6, year=2024)
+submit(answer1, part='a', day=6, year=2024)
 
 
 # Part 2
 
-inp = get_data(day=6, year=2024)
-
-# inp = '''....#.....
-# .........#
-# ..........
-# ..#.......
-# .......#..
-# ..........
-# .#..^.....
-# ........#.
-# #.........
-# ......#...
-# '''
-
-lines = inp.splitlines()
-lines = [list(line) for line in lines]
-nrows = len(lines)
-ncols = len(lines[0])
-
-import copy
-
-start = 0, 0
-for row, line in enumerate(lines):
-    for col, c in enumerate(line):
-        if c == '^':
-            start = row, col
-            break
-    else:
-        continue
-    break
-seen = set()
-r, c  = start
-direction = 'N'
-rot = {
-    'N': 'E',
-    'E': 'S',
-    'S': 'W',
-    'W': 'N',
-}
-
-
-def is_good(lines):
-    direction = 'N'
-    r, c  = start
-
-    states = set() # r, c, direction
-    while 0 <= r < nrows and 0 <= c < ncols:
-        if (r, c, direction) in  states:
-            return True
-        states.add((r,c,direction))
-        seen.add((r, c))
-        
-        r2 = r + (direction == 'S') - (direction == 'N')
-        c2 = c + (direction == 'E') - (direction == 'W')
-        if not (0 <= r2 < nrows) or not ( 0 <= c2 < ncols):
-            return False
-        while lines[r2][c2] == '#':
-            direction = rot[direction]
-            r2 = r + (direction == 'S') - (direction == 'N')
-            c2 = c + (direction == 'E') - (direction == 'W')
-        r = r2
-        c = c2
-    return False
 
 answer2 = 0
 for r in range(nrows):
     for c in range(ncols):
-        if lines[r][c] != '.':
+        if lines[r][c] != '.' or (r, c) not in positions:
             continue
         lines[r][c] = '#'
-        if is_good(lines):
+        if not simulate(lines, start):
             answer2 += 1
         lines[r][c] = '.'
-
 print(answer2)
 
 submit(answer2, part='b', day=6, year=2024)
