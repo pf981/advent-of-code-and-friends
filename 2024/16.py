12101bf2eb74fb161cd4c67b97a608c7daaa06ca
Paul Foster <paulfstr@gmail.com>
2024-12-08 09:37:48 +1100

    Complete 2024 quest 16 part 2

--- 2024/16.py
+++ 2024/16.py
@@ -30,6 +30,10 @@ print(answer1)
 # Part 2
 
 
+import collections
+import math
+
+
 with open("./2024/input/everybody_codes_e2024_q16_p2.txt") as f:
     lines = f.read().splitlines()
 
@@ -48,15 +52,8 @@ wheels = [[] for _ in range(len(jumps))]
 for line in wheels_lines:
     for i in range(len(jumps)):
         face = line[i*4:i*4+3].replace(' ', '')
-        # print(f'{i=} {face=}')
         if face:
             wheels[i].append(face)
-
-remainders = []
-for jump, wheel in zip(jumps, wheels):
-    print(f'{jump=} {len(wheel)=} {jump % len(wheel)=}')
-    remainders.append(jump % len(wheel))
-print(remainders)
 
 cycle_lengths = []
 for jump, wheel in zip(jumps, wheels):
@@ -70,8 +67,57 @@ for jump, wheel in zip(jumps, wheels):
     cycle_lengths.append(cycle_length)
 print(cycle_lengths)
 
-import math
-math.lcm(*cycle_lengths)
+full_cycle_length = math.lcm(*cycle_lengths)
+n_full_cycles = 202420242024 // full_cycle_length
+remaining = 202420242024 % full_cycle_length
+
+def count_coins(n_pulls: int, wheels, jumps) -> int:
+    coins = 0
+    indexes = [0 for _ in range(len(wheels))]
+
+    for _ in range(n_pulls):
+        s = ''
+        for i in range(len(wheels)):
+            indexes[i] = (indexes[i] + jumps[i]) % len(wheels[i])
+            face = wheels[i][indexes[i]]
+            s += face[0] + face[2]
+        
+        # print(f'{s} {[max(0, count - 2) for count in collections.Counter(s).values()]}')
+        for count in collections.Counter(s).values():
+            coins += max(0, count - 2)
+
+
+    return coins
+
+
+
+# count_coins(21, wheels, jumps)
+# count_coins(33, wheels, jumps)
+# count_coins(100, wheels, jumps)
+
+# count_coins(6, wheels, jumps)
+# count_coins(7, wheels, jumps)
+# count_coins(8, wheels, jumps)
+# count_coins(9, wheels, jumps)
+
+# count_coins(90, wheels, jumps)
+
+# count_coins(1, wheels, jumps)
+# count_coins(33, wheels, jumps)
+# count_coins(10, wheels, jumps) # This isn't correct
+# count_coins(100, wheels, jumps)
+
+a = count_coins(full_cycle_length, wheels, jumps)
+b = count_coins(remaining, wheels, jumps)
+
+answer2 = a * n_full_cycles + b
+print(answer2)
+
+# for jump, wheel in zip(jumps, wheels):
+#     n_wheel_cycles_per_full_cycle = full_cycle_length // len(wheel)
+#     n_wheel_cycles = n_wheel_cycles_per_full_cycle * n_full_cycles
+#     n_coins_per_cycle = sum(face == )
+
 # 54600
 
 # Calculate the number of coins per cycle
@@ -79,6 +125,7 @@ math.lcm(*cycle_lengths)
 # Also get remainnig coins not part of a full cycle
 
 # You can do this PER WHEEL - you don't have to combine first
+# Actually, if you don't want to do individual letters, you need to simulate a full cycle
 
 # answer2 = []
 # for jump, wheel in zip(jumps, wheels):
