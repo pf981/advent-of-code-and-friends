40acfe4f2b66515cb02e0a982f7f345a59ad75cf
Paul Foster <paulfstr@gmail.com>
2024-12-08 10:10:36 +1100

    Progress on 2024 quest 16 part 3

--- 2024/16.py
+++ 2024/16.py
@@ -89,69 +89,11 @@ def count_coins(n_pulls: int, wheels, jumps) -> int:
 
     return coins
 
-
-
-# count_coins(21, wheels, jumps)
-# count_coins(33, wheels, jumps)
-# count_coins(100, wheels, jumps)
-
-# count_coins(6, wheels, jumps)
-# count_coins(7, wheels, jumps)
-# count_coins(8, wheels, jumps)
-# count_coins(9, wheels, jumps)
-
-# count_coins(90, wheels, jumps)
-
-# count_coins(1, wheels, jumps)
-# count_coins(33, wheels, jumps)
-# count_coins(10, wheels, jumps) # This isn't correct
-# count_coins(100, wheels, jumps)
-
 a = count_coins(full_cycle_length, wheels, jumps)
 b = count_coins(remaining, wheels, jumps)
 
 answer2 = a * n_full_cycles + b
 print(answer2)
-
-# for jump, wheel in zip(jumps, wheels):
-#     n_wheel_cycles_per_full_cycle = full_cycle_length // len(wheel)
-#     n_wheel_cycles = n_wheel_cycles_per_full_cycle * n_full_cycles
-#     n_coins_per_cycle = sum(face == )
-
-# 54600
-
-# Calculate the number of coins per cycle
-# Calculate the number of full cycles
-# Also get remainnig coins not part of a full cycle
-
-# You can do this PER WHEEL - you don't have to combine first
-# Actually, if you don't want to do individual letters, you need to simulate a full cycle
-
-# answer2 = []
-# for jump, wheel in zip(jumps, wheels):
-#     cycle_length = 0
-#     coins_per_cycle = 0
-
-#     while True:
-#         if wheels[i] == ''
-#         cycle_length += 1
-#         i = jump % len(wheel)
-#         if i == 0:
-#             break
-
-#     n_cycles = ...
-#     answer2.append(wheel[(jump * 202420242024) % len(wheel)])
-# answer2 = ' '.join(answer2)
-
-answer2 = 'todo'
-print(answer2)
-
-
-# So, I think that I need to do a cycle simulation PER CHARACTER
-# Note that jumps are prime and distinct, so the LCM is enourmous. Actually, you need to look at it MOD wheel size
-# Actually, when looking at remainders, LCM is like 1MM so that is workable.
-# When looking at cycles, it's around 55k which is very workable.
-
 
 
 # Part 3
@@ -160,5 +102,81 @@ print(answer2)
 with open("./2024/input/everybody_codes_e2024_q16_p3.txt") as f:
     lines = f.read().splitlines()
 
-answer3 = 'todo'
+
+lines = '''1,2,3
+
+^_^ -.- ^,-
+>.- ^_^ >.<
+-_- -.- ^.^
+    -.^ >.<
+    >.>'''.splitlines()
+
+jumps, _, *wheels_lines = lines
+jumps = [int(jump) for jump in jumps.split(',')]
+
+wheels = [[] for _ in range(len(jumps))]
+for line in wheels_lines:
+    for i in range(len(jumps)):
+        face = line[i*4:i*4+3].replace(' ', '')
+        if face:
+            wheels[i].append(face)
+
+def min_max_coins(n_pulls, wheels, jumps):
+    indexes = [0 for _ in range(len(wheels))]
+    min_coins, max_coins = min_max_coins_impl(n_pulls, wheels, jumps, indexes, True)
+    return min_coins, max_coins
+
+def shift_one(indexes, wheels, shift):
+    indexes = indexes.copy()
+    for i in range(len(wheels)):
+        indexes[i] = (indexes[i] + shift) % len(wheels[i])
+    return indexes
+
+
+def min_max_coins_impl(n_pulls, wheels, jumps, indexes, can_shift) -> int:
+    if n_pulls == 0:
+        return 0, 0
+    
+    # print(f'{indexes=}')
+
+    indexes = indexes.copy()
+    min_coins = float('inf')
+    max_coins = 0
+    if can_shift:
+        a, b = min_max_coins_impl(n_pulls, wheels, jumps, shift_one(indexes, wheels, +1), False)
+        min_coins = min(min_coins, a)
+        max_coins = max(max_coins, b)
+
+        a, b = min_max_coins_impl(n_pulls, wheels, jumps, shift_one(indexes, wheels, -1), False)
+        min_coins = min(min_coins, a)
+        max_coins = max(max_coins, b)
+
+    coins = 0
+    for _ in range(1):
+        s = ''
+        for i in range(len(wheels)):
+            indexes[i] = (indexes[i] + jumps[i]) % len(wheels[i])
+            face = wheels[i][indexes[i]]
+            s += face[0] + face[2]
+        
+        for count in collections.Counter(s).values():
+            coins += max(0, count - 2)
+    # a, b = min_max_coins_impl(n_pulls - 1, wheels, jumps, indexes, can_shift)
+    a, b = min_max_coins_impl(n_pulls - 1, wheels, jumps, indexes, True) # Test
+    min_coins = min(min_coins, a + coins)
+    max_coins = max(max_coins, b + coins)
+
+    return min_coins, max_coins
+
+
+
+# min_max_coins(1, wheels, jumps)
+# min_max_coins(2, wheels, jumps)
+# min_max_coins(3, wheels, jumps)
+
+
+min_coins, max_coins = min_max_coins(256, wheels, jumps)
+answer3 = f'{max_coins} {min_coins}'
 print(answer3)
+
+# TODO: May be just prune?
