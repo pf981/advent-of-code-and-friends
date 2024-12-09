4bf0ff806afa52b19e8b57ae2c2241cef2648647
Paul Foster <paulfstr@gmail.com>
2024-12-09 17:43:05 +1100

    Simplify 2024 day 9 part 1

--- python/AoC 2024 Day 09.py
+++ python/AoC 2024 Day 09.py
@@ -1,20 +1,7 @@
 from aocd import get_data, submit
 
 
-def checksum(nums):
-    result = 0
-    for i, num in enumerate(nums):
-        if num == -1:
-            print('!!!')
-            break
-        result += i * num
-    return result
-
 inp = get_data(day=9, year=2024)
-
-# inp = '''2333133121414131402
-# '''
-
 lines = inp.splitlines()
 nums = [int(c) for c in lines[0]]
 
@@ -37,21 +24,20 @@ while i <= j:
         break
     while j >= 0 and data[j] == -1:
         j -= 1
-    # print(i, j, data[i], data[j])
     if not i <= j:
         break
     if i == -1:
         break
     data[i], data[j] = data[j], data[i]
 
-all(x == -1 for x in data[data.index(-1):])
-# 01234567890123456789012345678
-# 0099811188827773336446555566..............
-
-answer1 = checksum(data)
+answer1 = 0
+for i, num in enumerate(data):
+    if num == -1:
+        break
+    answer1 += i * num
 print(answer1)
 
-# submit(answer1, part='a', day=9, year=2024)
+submit(answer1, part='a', day=9, year=2024)
 
 
 # Part 2
@@ -59,9 +45,6 @@ print(answer1)
 
 import sortedcontainers
 
-
-inp = get_data(day=9, year=2024)
-nums = [int(c) for c in inp.splitlines()[0]]
 
 gaps = {gap_size: sortedcontainers.SortedList(key=lambda ind: -ind) for gap_size in range(1, 10)} # gap_size -> [(index, size), ...]
 files = [] # file_id -> [index, size]
