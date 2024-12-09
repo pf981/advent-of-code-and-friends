e096d9d1ffb82405b93c9da95bdbb3f4753f539e
Paul Foster <paulfstr@gmail.com>
2024-12-09 21:30:20 +1100

    Tidy code

--- 2024/18.py
+++ 2024/18.py
@@ -1,39 +1,36 @@
 import collections
 
 
+def bfs(lines: list[str], start_positions: list[tuple[int, int]]) -> int:
+    ncols = len(lines[0])
+
+    q = collections.deque(start_positions)
+    d = 0
+    remaining_p = sum(c == 'P' for line in lines for c in line)
+    seen = set((1, 0))
+    while q:
+        for _ in range(len(q)):
+            r, c = q.popleft()
+
+            if lines[r][c] == 'P':
+                remaining_p -= 1
+                if not remaining_p:
+                    return d
+            
+            for dr, dc in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+                r2 = r + dr
+                c2 = c + dc
+                if not (0 <= c2 < ncols) or (r2, c2) in seen or lines[r2][c2] == '#':
+                    continue
+                seen.add((r2, c2))
+                q.append((r2, c2))
+        d += 1
+
+
 with open("./2024/input/everybody_codes_e2024_q18_p1.txt") as f:
     lines = f.read().splitlines()
 
-# lines = '''##########
-# ..#......#
-# #.P.####P#
-# #.#...P#.#
-# ##########'''.splitlines()
-
-nrows = len(lines)
-ncols = len(lines[0])
-
-q = collections.deque([(1, 0)])
-d = 0
-remaining_p = sum(c == 'P' for line in lines for c in line)
-seen = set((1, 0))
-while remaining_p:
-    for _ in range(len(q)):
-        r, c = q.popleft()
-
-        if lines[r][c] == 'P':
-            remaining_p -= 1
-        
-        for dr, dc in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
-            r2 = r + dr
-            c2 = c + dc
-            if c2 < 0 or (r2, c2) in seen or lines[r2][c2] == '#':
-                continue
-            seen.add((r2, c2))
-            q.append((r2, c2))
-    d += 1
-
-answer1 = d - 1
+answer1 = bfs(lines, [(1, 0)])
 print(answer1)
 
 
@@ -43,41 +40,10 @@ print(answer1)
 with open("./2024/input/everybody_codes_e2024_q18_p2.txt") as f:
     lines = f.read().splitlines()
 
-
-# lines = '''#######################
-# ...P..P...#P....#.....#
-# #.#######.#.#.#.#####.#
-# #.....#...#P#.#..P....#
-# #.#####.#####.#########
-# #...P....P.P.P.....P#.#
-# #.#######.#####.#.#.#.#
-# #...#.....#P...P#.#....
-# #######################'''.splitlines()
-
 nrows = len(lines)
 ncols = len(lines[0])
 
-q = collections.deque([(1, 0), (nrows - 2, ncols - 1)])
-d = 0
-remaining_p = sum(c == 'P' for line in lines for c in line)
-seen = {(1, 0), (nrows - 2, ncols - 1)}
-while remaining_p:
-    for _ in range(len(q)):
-        r, c = q.popleft()
-
-        if lines[r][c] == 'P':
-            remaining_p -= 1
-        
-        for dr, dc in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
-            r2 = r + dr
-            c2 = c + dc
-            if not (0 <= c2 < ncols) or (r2, c2) in seen or lines[r2][c2] == '#':
-                continue
-            seen.add((r2, c2))
-            q.append((r2, c2))
-    d += 1
-
-answer2 = d - 1
+answer2 = bfs(lines, [(1, 0), (nrows - 2, ncols - 1)])
 print(answer2)
 
 
@@ -86,12 +52,6 @@ print(answer2)
 
 with open("./2024/input/everybody_codes_e2024_q18_p3.txt") as f:
     lines = f.read().splitlines()
-
-# lines = '''##########
-# #.#......#
-# #.P.####P#
-# #.#...P#.#
-# ##########'''.splitlines()
 
 nrows = len(lines)
 ncols = len(lines[0])
@@ -111,7 +71,7 @@ n_ps = p_index
 
 d = 0
 answer3 = float('inf')
-while q:
+while q and d <= answer3:
     for _ in range(len(q)):
         r, c, p_index = q.popleft()
 
@@ -125,9 +85,7 @@ while q:
                 continue
             seen[(r2, c2)][p_index] = d
             if len(seen[(r2, c2)]) == n_ps and lines[r2][c2] == '.':
-                # print(f'{r2=} {c2=} {seen[(r2, c2)]=} s={sum(seen[(r2, c2)].values())}')
                 answer3 = min(answer3, sum(dd + 1 for dd in seen[(r2, c2)].values()))
-                # answer3 = min(answer3, sum(seen[(r2, c2)].values()))
             q.append((r2, c2, p_index))
     d += 1
 
