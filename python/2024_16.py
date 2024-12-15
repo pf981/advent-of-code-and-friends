5640222a9017a285003afaf303b032a298629ed4
Paul Foster <paulfstr@gmail.com>
2024-12-15 17:00:17 +1100

    Add 2024 day 16 skeleton

---
+++ python/2024_16.py
@@ -0,0 +1,23 @@
+from aocd import get_data, submit
+
+
+inp = get_data(day=16, year=2024)
+
+# inp = '''
+# '''
+
+lines = inp.splitlines()
+
+answer1 = 'todo'
+print(answer1)
+
+# submit(answer1, part='a', day=16, year=2024)
+
+
+# Part 2
+
+
+answer2 = 'todo'
+print(answer2)
+
+# submit(answer2, part='b', day=16, year=2024)
