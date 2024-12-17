dec1e5e835512cb3f6b91cc2c4987a307125d1f5
Paul Foster <paulfstr@gmail.com>
2024-12-17 18:15:09 +1100

    Tidy code

--- python/2024_17.py
+++ python/2024_17.py
@@ -1,25 +1,8 @@
 from aocd import get_data, submit
 import re
 
-inp = get_data(day=17, year=2024)
 
-# inp = '''Register A: 729
-# Register B: 0
-# Register C: 0
-
-# Program: 0,1,5,4,3,0
-# '''
-
-# inp = '''Register A: 2024
-# Register B: 0
-# Register C: 0
-
-# Program: 0,3,5,4,3,0'''
-
-start_reg_a,start_reg_b,start_reg_c,*start_program = map(int, re.findall(r'-?[\d]+', inp))
-reg_a,reg_b,reg_c,program = start_reg_a,start_reg_b,start_reg_c,start_program.copy()
-
-def run(reg_a,reg_b,reg_c,program):
+def run(reg_a, reg_b, reg_c, program):
     output = []
     ip = 0
     while ip < len(program):
@@ -55,34 +38,20 @@ def run(reg_a,reg_b,reg_c,program):
         ip += 2
     return ','.join(str(x) for x in output)
 
-answer1 = run(reg_a,reg_b,reg_c,program)
+
+inp = get_data(day=17, year=2024)
+reg_a, reg_b, reg_c, *program = [int(s) for s in re.findall(r'-?[\d]+', inp)]
+answer1 = run(reg_a, reg_b, reg_c, program)
 print(answer1)
 
-# submit(answer1, part='a', day=17, year=2024)
+submit(answer1, part='a', day=17, year=2024)
 
 
 # # Part 2
-# import itertools
 
-# target = ','.join(str(x) for x in start_program)
-# for reg_a in itertools.count():
-#     reg_b,reg_c,program = start_reg_b,start_reg_c,start_program.copy()
-#     prog = run(reg_a,reg_b,reg_c,program)
-#     if prog == target:
-#         answer2 = reg_a
-#         break
-
-# print(answer2)
-
-# submit(answer2, part='b', day=17, year=2024)
-
-from aocd import get_data, submit
-
-program = [2,4,1,2,7,5,1,3,4,3,5,5,0,3,3,0]
 
 import z3
 
-o = z3.Optimize()
 
 def bit_vecs(s):
     l = []
@@ -90,12 +59,15 @@ def bit_vecs(s):
         l.append(z3.BitVec(ss, 64))
     return l
 
+
+o = z3.Optimize()
+
 a0, b0, c0, a1 = bit_vecs('a0 b0 c0 a1')
 o.add(b0 == 0)
 o.add(c0 == 0)
-# o.add(a1 == 64584136) # TEST
 o.add(a1 == a0)
 o.minimize(a0)
+
 for i in range(1, 17):
     a, b, c, bb, bbb, out = bit_vecs(f'a{i} b{i} c{i} bb{i} bbb{i} out{i}')
     if i != 1:
@@ -105,25 +77,11 @@ for i in range(1, 17):
     o.add(c == a >> b)
     o.add(bb == b ^ 3)
     o.add(bbb == bb ^ c)
-
     o.add(out == bbb % 8)
-    
-    # if i < 15: # TEST
     o.add(out == program[i - 1])
 
 o.check()
+answer2 = o.model()[a0].as_long()
+print(answer2)
 
-mod = o.model()
-
-
-def get(name, mod):
-    for obj_name in mod:
-        if str(obj_name) == name:
-            return mod[obj_name]
-
-for i in range(1, 17):
-    print(get(f'out{i}', mod))
-
-print(get(f'a0', mod))
-
-# 37221334433268
+submit(answer2, part='b', day=17, year=2024)
