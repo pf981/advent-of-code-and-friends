ce258df143260d38fe22bc7a65ace753575d6770
Paul Foster <paulfstr@gmail.com>
2024-12-19 16:07:51 +1100

    Tidy code

--- python/2024_19.py
+++ python/2024_19.py
@@ -2,19 +2,6 @@ from aocd import get_data, submit
 import re
 
 inp = get_data(day=19, year=2024)
-
-# inp = '''r, wr, b, g, bwu, rb, gb, br
-
-# brwrr
-# bggr
-# gbbr
-# rrbgbr
-# ubwu
-# bwurrg
-# brgr
-# bbrgwb
-# '''
-
 patterns, _, *designs = inp.splitlines()
 patterns = patterns.split(', ')
 
@@ -24,14 +11,16 @@ answer1 = 0
 for design in designs:
     if re.match(reg, design):
         answer1 += 1
-
 print(answer1)
 
 submit(answer1, part='a', day=19, year=2024)
 
 
 # Part 2
+
+
 import functools
+
 
 @functools.cache
 def count_ways(design):
@@ -43,14 +32,11 @@ def count_ways(design):
             ways += count_ways(design[len(pattern):])
     return ways
 
+
 answer2 = 0
 for design in designs:
     if re.match(reg, design):
         answer2 += count_ways(design)
-
 print(answer2)
-
-# import collections
-# collections.Counter(patterns)
 
 submit(answer2, part='b', day=19, year=2024)
