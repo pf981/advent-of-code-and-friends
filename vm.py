5ae911e196d8764fb8d99d056482ea2a05fae431
Paul Foster <paulfstr@gmail.com>
2024-12-28 17:30:25 +1100

    Update completed hashes

--- vm.py
+++ vm.py
@@ -282,6 +282,6 @@ if __name__ == "__main__":
 # 7997a3b2941eab92c1c0345d5747b420 DONE (successful self-test)
 # 186f842951c0dcfe8838af1e7222b7d4 DONE (tablet)
 # 2bf84e54b95ce97aefd9fc920451fc45
-# e09640936b3ef532b7b8e83ce8f125f4
+# e09640936b3ef532b7b8e83ce8f125f4 DONE (teleporter)
 # 4873cf6b76f62ac7d5a53605b2535a0c
 # d0c54d4ed7f943280ce3e19532dbb1a6
