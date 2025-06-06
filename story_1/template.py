cd975f0c6ca9fbda8b5425a10174e960fe820990
Paul Foster <paulfstr@gmail.com>
2025-06-07 09:11:00 +1000

    Update template

--- story_1/template.py
+++ story_1/template.py
@@ -1,19 +1,19 @@
-with open("./story_1/input/q02_p1.txt") as f:
-    enemies = f.read().splitlines()[0]
+with open("./story_1/input/q03_p1.txt") as f:
+    lines = f.read().splitlines()
 
 answer1 = "TODO"
 print(answer1)
 
 
-with open("./story_1/input/q02_p2.txt") as f:
-    enemies = f.read().splitlines()[0]
+with open("./story_1/input/q03_p2.txt") as f:
+    lines = f.read().splitlines()
 
 answer2 = "TODO"
 print(answer2)
 
 
-with open("./story_1/input/q02_p3.txt") as f:
-    enemies = f.read().splitlines()[0]
+with open("./story_1/input/q03_p3.txt") as f:
+    lines = f.read().splitlines()
 
 answer3 = "TODO"
 print(answer3)
