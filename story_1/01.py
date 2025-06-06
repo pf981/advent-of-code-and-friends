21db80e62e1c16995b11bead2bf197f07d252a7a
Paul Foster <paulfstr@gmail.com>
2025-06-06 23:25:25 +1000

    Add skeletons for story 1

---
+++ story_1/01.py
@@ -0,0 +1,19 @@
+with open("./story_1/input/q01_p1.txt") as f:
+    enemies = f.read().splitlines()[0]
+
+answer1 = "TODO"
+print(answer1)
+
+
+with open("./story_1/input/q01_p2.txt") as f:
+    enemies = f.read().splitlines()[0]
+
+answer2 = "TODO"
+print(answer2)
+
+
+with open("./story_1/input/q01_p3.txt") as f:
+    enemies = f.read().splitlines()[0]
+
+answer3 = "TODO"
+print(answer3)
