d0b9128f2f3ace0e64d87fcd026b3096d1b92e0b
Paul Foster <paulfstr@gmail.com>
2025-07-26 01:17:01 +1000

    Complete puzzle 1

---
+++ solutions/1.py
@@ -0,0 +1,22 @@
+with open("data/blank-sheet-of-paper.txt") as f:
+    blank = f.read().splitlines()
+
+with open("data/shredded-sheet-of-paper.txt") as f:
+    shredded = f.read().splitlines()
+
+
+m = {}
+for i, line in enumerate(blank):
+    m[line[:20]] = i
+
+result = [""] * len(blank)
+for line in shredded:
+    result[m[line[:20]]] = line
+
+
+with open("data/shredded-sheet-of-paper-recovered.txt", "w") as f:
+    f.write("\n".join(result))
+
+# For access to the secret tunnels use the code 143670892 DESTROY AFTER USE
+answer = 143670892
+print(answer)
