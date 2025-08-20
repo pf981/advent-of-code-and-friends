dea86387ef2caed4b693e2d9997ac1f6a4256fdd
Paul Foster <paulfstr@gmail.com>
2025-08-20 11:37:00 +1000

    advent-of-sql: Implement faster more vectorized solution for day 8

--- advent-of-sql/solutions/2024/08.R
+++ advent-of-sql/solutions/2024/08.R
@@ -10,18 +10,11 @@ read_file("./data/2024/advent_of_sql_day_8.sql") |>
   walk(DBI::dbExecute, conn = con)
 
 managers <- tbl(con, "staff") |> collect() |> pull(manager_id)
-
-dp <- rep_along(managers, NA_integer_)
-dp[1] <- 1
-
-get_path_length <- function(staff_id) {
-  if (is.na(dp[staff_id])) {
-    dp[staff_id] <- 1 + get_path_length(managers[staff_id])
-  }
-  return(dp[staff_id])
+staff_ids <- seq_along(managers)
+result <- 0
+while (!is_empty(staff_ids)) {
+  staff_ids <- managers[staff_ids] |> discard(is.na)
+  result <- result + 1
 }
-
-seq_along(managers) |>
-  map_int(get_path_length) |>
-  max()
+result
 # 24
