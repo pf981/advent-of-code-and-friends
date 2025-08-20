1709cb5b9c0aa0a94ce32a32b5de602f93148217
Paul Foster <paulfstr@gmail.com>
2025-08-20 16:59:05 +1000

    advent-of-sql: Complete day 18

---
+++ advent-of-sql/solutions/2024/18.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+
+con <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+
+read_file("./data/2024/advent_of_sql_day_18.sql") |>
+  str_replace_all("CASCADE", "") |>
+  str_split_1(";") |>
+  str_trim() |>
+  discard(str_equal, "") |>
+  walk(DBI::dbExecute, conn = con)
+
+managers <- tbl(con, "staff") |> collect() |> pull(manager_id)
+managers[1] <- 1
+
+staff_ids <- seq_along(managers)
+
+dp <- rep_along(managers, NA_integer_)
+dp[1] <- 1
+
+while (!is_empty(staff_ids)) {
+  dp[staff_ids] <- 1 + dp[managers[staff_ids]]
+  staff_ids <- staff_ids |> keep(\(staff_id) is.na(dp[staff_id]))
+}
+
+target <-
+  dp |>
+  enframe() |>
+  count(value) |>
+  slice_max(n) |>
+  pull(value)
+
+which(dp == target)[1]
+# 232
