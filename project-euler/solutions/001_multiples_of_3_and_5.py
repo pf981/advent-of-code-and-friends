901412d85d5471773467778c6ede1e97e9e3d82b
pf981 <paulfstr@gmail.com>
2025-08-22 00:39:06 +1000

    project-euler: WIP: Add pytest

--- project-euler/solutions/001_multiples_of_3_and_5.py
+++ project-euler/solutions/001_multiples_of_3_and_5.py
@@ -1,7 +1,2 @@
-def main():
-  answer = sum(x for x in range(1000) if x % 3 == 0 or x % 5 == 0)
-  print(answer)
-
-
-if __name__ == '__main__':
-  main()
+answer = sum(x for x in range(1000) if x % 3 == 0 or x % 5 == 0)
+print(answer)
