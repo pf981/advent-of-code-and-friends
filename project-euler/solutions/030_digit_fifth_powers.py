c9f8fdeb625abb48d75bea7345717c037fd95734
pf981 <paulfstr@gmail.com>
2025-08-25 20:38:55 +1000

    project-euler: 1.0.0: All tests passing

--- project-euler/solutions/030_digit_fifth_powers.py
+++ project-euler/solutions/030_digit_fifth_powers.py
@@ -1,10 +1,8 @@
-from helpers import helpers
-
 POWER = 5
 
 correct_numbers = []
-for i in range(2, 1000000):
-    if i == sum(x**POWER for x in helpers.int_to_digits(i)):
+for i in range(2, 1_000_000):
+    if i == sum(int(digit) ** POWER for digit in str(i)):
         correct_numbers.append(i)
 
 answer = sum(correct_numbers)
