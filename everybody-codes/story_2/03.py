2b775374cd195024a0416767e8fe8ac0fdc933b4
pf981 <paulfstr@gmail.com>
2025-08-26 19:41:39 +1000

    everybody-codes: Add skeleton for story 2

---
+++ everybody-codes/story_2/03.py
@@ -0,0 +1,19 @@
+with open("./story_2/input/everybody_codes_e2_q02_p1.txt") as f:
+    lines = f.read().splitlines()
+
+answer1 = "TODO"
+print(answer1)
+
+
+with open("./story_2/input/everybody_codes_e2_q02_p2.txt") as f:
+    lines = f.read().splitlines()
+
+answer2 = "TODO"
+print(answer2)
+
+
+with open("./story_2/input/everybody_codes_e2_q02_p3.txt") as f:
+    lines = f.read().splitlines()
+
+answer3 = "TODO"
+print(answer3)
