2f381ed0f19abe6c6aaa1129455a0332b983dfb3
pf981 <paulfstr@gmail.com>
2025-08-26 19:42:55 +1000

    everybody-codes: Fix file path for story 2 quest 3

--- everybody-codes/story_2/03.py
+++ everybody-codes/story_2/03.py
@@ -1,18 +1,18 @@
-with open("./story_2/input/everybody_codes_e2_q02_p1.txt") as f:
+with open("./story_2/input/everybody_codes_e2_q03_p1.txt") as f:
     lines = f.read().splitlines()
 
 answer1 = "TODO"
 print(answer1)
 
 
-with open("./story_2/input/everybody_codes_e2_q02_p2.txt") as f:
+with open("./story_2/input/everybody_codes_e2_q03_p2.txt") as f:
     lines = f.read().splitlines()
 
 answer2 = "TODO"
 print(answer2)
 
 
-with open("./story_2/input/everybody_codes_e2_q02_p3.txt") as f:
+with open("./story_2/input/everybody_codes_e2_q03_p3.txt") as f:
     lines = f.read().splitlines()
 
 answer3 = "TODO"
