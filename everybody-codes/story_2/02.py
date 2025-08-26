fef2829efbc1489a4fcc659acc4d62c19f08b742
Paul Foster <paulfstr@gmail.com>
2025-08-27 09:27:33 +1000

    everybody-codes: Update skeleton

--- everybody-codes/story_2/02.py
+++ everybody-codes/story_2/02.py
@@ -5,11 +5,17 @@ answer1 = "TODO"
 print(answer1)
 
 
+# Part 2
+
+
 with open("./story_2/input/everybody_codes_e2_q02_p2.txt") as f:
     lines = f.read().splitlines()
 
 answer2 = "TODO"
 print(answer2)
+
+
+# Part 3
 
 
 with open("./story_2/input/everybody_codes_e2_q02_p3.txt") as f:
