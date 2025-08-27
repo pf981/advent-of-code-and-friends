f2c737afebee740ba7a19d06d1865efbc3b9cf46
Paul Foster <paulfstr@gmail.com>
2025-08-28 08:50:21 +1000

    everybody-codes: Complete story 2 quest 2

--- everybody-codes/story_2/02.py
+++ everybody-codes/story_2/02.py
@@ -1,7 +1,27 @@
-with open("./story_2/input/everybody_codes_e2_q02_p1.txt") as f:
-    lines = f.read().splitlines()
+import collections
+import itertools
 
-answer1 = "TODO"
+with open("./story_2/input/everybody_codes_e2_q02_p1.txt") as f:
+    text = f.read()
+
+# text = "GRBGGGBBBRRRRRRRR"
+greens = [i for i, c in enumerate(text) if c == "G"]
+reds = [i for i, c in enumerate(text) if c == "R"]
+
+greens.reverse()
+reds.reverse()
+it = itertools.cycle("RG")
+answer1 = 0
+while greens and reds:
+    if next(it) == "G":
+        if reds:
+            reds.pop()
+    else:
+        if greens:
+            greens.pop()
+    answer1 += 1
+
+
 print(answer1)
 
 
@@ -9,17 +29,58 @@ print(answer1)
 
 
 with open("./story_2/input/everybody_codes_e2_q02_p2.txt") as f:
-    lines = f.read().splitlines()
+    text = f.read()
+repeat = 100
 
-answer2 = "TODO"
+# text = "GGBR"
+# repeat = 5
+# text = "BBRGGRRGBBRGGBRGBBRRBRRRBGGRRRBGBGG"
+# repeat = 10
+
+balloons = list(text) * repeat
+
+it = itertools.cycle("RGB")
+for answer2 in itertools.count(1):
+    col = next(it)
+    if col != balloons[0]:
+        balloons.pop(0)
+        # print(answer2, "\t", col, "\t", " ".join(balloons))
+        if not balloons:
+            break
+        continue
+
+    if len(balloons) % 2 == 0:  # and col != balloons[len(balloons) // 2]:
+        balloons.pop(len(balloons) // 2)
+    balloons.pop(0)
+    # print(answer2, "\t", col, "\t", " ".join(balloons))
+    if not balloons:
+        break
+
 print(answer2)
-
 
 # Part 3
 
 
 with open("./story_2/input/everybody_codes_e2_q02_p3.txt") as f:
-    lines = f.read().splitlines()
+    text = f.read()
+repeat = 100_000
 
-answer3 = "TODO"
+balloons = list(text) * repeat
+left = collections.deque(balloons[: len(balloons) // 2])
+right = collections.deque(balloons[len(balloons) // 2 :])
+
+it = itertools.cycle("RGB")
+for answer3 in itertools.count(1):
+    col = next(it)
+    if len(left) != len(right):
+        left.popleft()
+    elif left[0] == col:
+        left.popleft()
+        right.popleft()
+    else:
+        left.popleft()
+        left.append(right.popleft())
+    if not left and not right:
+        break
+
 print(answer3)
