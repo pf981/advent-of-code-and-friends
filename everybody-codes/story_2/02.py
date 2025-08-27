ceb880c7200451e734b73513050b1b6250dec4c0
Paul Foster <paulfstr@gmail.com>
2025-08-28 09:06:02 +1000

    everybody-codes: Tidy code

--- everybody-codes/story_2/02.py
+++ everybody-codes/story_2/02.py
@@ -4,20 +4,16 @@ import itertools
 with open("./story_2/input/everybody_codes_e2_q02_p1.txt") as f:
     text = f.read()
 
-greens = [i for i, c in enumerate(text) if c == "G"]
-reds = [i for i, c in enumerate(text) if c == "R"]
+greens = text.count("G")
+reds = len(text) - greens
 
-greens.reverse()
-reds.reverse()
 it = itertools.cycle("RG")
 answer1 = 0
-while greens and reds:
+while greens > 0 and reds > 0:
     if next(it) == "G":
-        if reds:
-            reds.pop()
+        reds -= 1
     else:
-        if greens:
-            greens.pop()
+        greens -= 1
     answer1 += 1
 
 print(answer1)
@@ -26,24 +22,35 @@ print(answer1)
 # Part 2
 
 
+def count_shots(balloons: list[str]) -> int:
+    left = collections.deque(balloons[: len(balloons) // 2])
+    right = collections.deque(balloons[len(balloons) // 2 :])
+
+    it = itertools.cycle("RGB")
+    result = 0
+    while right or left:
+        col = next(it)
+
+        if len(left) > len(right):
+            left.popleft()
+        elif len(right) > len(left):
+            left.append(right.popleft())
+            left.popleft()
+        else:
+            if left[0] == col:
+                right.popleft()
+            left.popleft()
+
+        result += 1
+
+    return result
+
+
 with open("./story_2/input/everybody_codes_e2_q02_p2.txt") as f:
     text = f.read()
 repeat = 100
 
-balloons = list(text) * repeat
-
-it = itertools.cycle("RGB")
-answer2 = 0
-while balloons:
-    col = next(it)
-
-    if col == balloons[0] and len(balloons) % 2 == 0:
-        balloons.pop(len(balloons) // 2)
-
-    balloons.pop(0)
-
-    answer2 += 1
-
+answer2 = count_shots(list(text) * repeat)
 print(answer2)
 
 # Part 3
@@ -53,25 +60,5 @@ with open("./story_2/input/everybody_codes_e2_q02_p3.txt") as f:
     text = f.read()
 repeat = 100_000
 
-balloons = list(text) * repeat
-left = collections.deque(balloons[: len(balloons) // 2])
-right = collections.deque(balloons[len(balloons) // 2 :])
-
-it = itertools.cycle("RGB")
-answer3 = 0
-while right or left:
-    col = next(it)
-
-    if len(left) > len(right):
-        left.popleft()
-    elif len(right) > len(left):
-        left.append(right.popleft())
-        left.popleft()
-    else:
-        if left[0] == col:
-            right.popleft()
-        left.popleft()
-
-    answer3 += 1
-
+answer3 = count_shots(list(text) * repeat)
 print(answer3)
