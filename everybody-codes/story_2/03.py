c5bcc49d12cae943aac425d7c961086c6e625d97
Paul Foster <paulfstr@gmail.com>
2025-08-29 13:46:25 +1000

    everybody-codes: Simplify solution

--- everybody-codes/story_2/03.py
+++ everybody-codes/story_2/03.py
@@ -1,44 +1,44 @@
+import collections
+import itertools
 import re
-import sys
+
+from collections.abc import Generator
 
 
-sys.setrecursionlimit(150000)
+def die(faces: list[int], seed: int) -> Generator[int, None, None]:
+    pulse = seed
+    face = 0
+    for roll_number in itertools.count(1):
+        spin = roll_number * pulse
+        pulse += spin
+        pulse %= seed
+        pulse += 1 + roll_number + seed
+        face = (face + spin) % len(faces)
+        yield faces[face]
 
 
-def parse_rolls(text: str, n_rolls: int) -> list[list[int]]:
-    all_rolls = []
+def parse_dice(text: str) -> list[Generator[int, None, None]]:
+    dice = []
     for line in text.splitlines():
         match = re.match(r"[0-9]+: faces=\[(.+)\] seed=([0-9]+)", line)
         assert match
         g = match.groups()
-        dice = [int(num) for num in g[0].split(",")]
-        pulse = seed = int(g[1])
-
-        face = 0
-        rolls = []
-
-        for roll_number in range(1, n_rolls + 1):
-            spin = roll_number * pulse
-            pulse = pulse + spin
-            pulse = pulse % seed
-            pulse = pulse + 1 + roll_number + seed
-            face = (face + spin) % len(dice)
-            rolls.append(dice[face])
-
-        all_rolls.append(rolls)
-
-    return all_rolls
+        faces = [int(num) for num in g[0].split(",")]
+        seed = int(g[1])
+        dice.append(die(faces, seed))
+    return dice
 
 
 with open("./story_2/input/everybody_codes_e2_q03_p1.txt") as f:
     text = f.read()
 
-all_rolls = parse_rolls(text, 1_000)
+dice = parse_dice(text)
+
 total = answer1 = 0
-for group_roll in zip(*all_rolls):
+for group_roll in zip(*dice):
     total += sum(group_roll)
     answer1 += 1
-    if total >= 10000:
+    if total >= 10_000:
         break
 
 print(answer1)
@@ -52,14 +52,14 @@ with open("./story_2/input/everybody_codes_e2_q03_p2.txt") as f:
 
 dice_str, track_str = text.split("\n\n")
 track = [int(x) for x in track_str]
-all_rolls = parse_rolls(dice_str, 50_000)
+dice = parse_dice(dice_str)
 
-track_pos = [0] * len(all_rolls)
+track_pos = [0] * len(dice)
 finishes = []  # [(finish_time, dice_i), ...]
-for dice_i, rolls in enumerate(all_rolls, 1):
+for dice_i, cur_die in enumerate(dice, 1):
     finish_time = 0
     for track_value in track:
-        while rolls[finish_time] != track_value:
+        while next(cur_die) != track_value:
             finish_time += 1
         finish_time += 1
     finishes.append((finish_time, dice_i))
@@ -74,44 +74,36 @@ print(answer2)
 with open("./story_2/input/everybody_codes_e2_q03_p3.txt") as f:
     text = f.read()
 
-
 dice_str, grid_str = text.split("\n\n")
 grid = [[int(num) for num in line] for line in grid_str.splitlines()]
+dice = parse_dice(dice_str)
 
 nrows = len(grid)
 ncols = len(grid[0])
 
-all_rolls = parse_rolls(dice_str, nrows * ncols * 3)
-
-good = set()  # {(r, c), ..}
-seen = set()  # {(r, c, dice_i, roll_i), ...}
-
-
-def dfs(r: int, c: int, dice_i: int, roll_i: int) -> None:
-    if (r, c, dice_i, roll_i) in seen:
-        return
-    seen.add(((r, c, dice_i, roll_i)))
-    if not (0 <= r < nrows and 0 <= c < ncols):
-        return
-
-    assert roll_i != len(all_rolls[dice_i])
-
-    if grid[r][c] != all_rolls[dice_i][roll_i]:
-        return
-
-    good.add((r, c))
-    roll_i += 1
-    dfs(r + 1, c, dice_i, roll_i)
-    dfs(r - 1, c, dice_i, roll_i)
-    dfs(r, c + 1, dice_i, roll_i)
-    dfs(r, c - 1, dice_i, roll_i)
-    dfs(r, c, dice_i, roll_i)
-
-
-for dice_i in range(len(all_rolls)):
+nodes = collections.defaultdict(set)  # dice_i -> {(r, c), ...}
+for dice_i in range(len(dice)):
+    val = next(dice[dice_i])
     for r in range(nrows):
         for c in range(ncols):
-            dfs(r, c, dice_i, 0)
+            if grid[r][c] == val:
+                nodes[dice_i].add((r, c))
+
+good = set()  # {(r, c), ..}
+while nodes:
+    next_nodes = collections.defaultdict(set)
+    for dice_i, positions in nodes.items():
+        val = next(dice[dice_i])
+        for r, c in positions:
+            good.add((r, c))
+            for dr, dc in [(-1, 0), (0, -1), (0, 0), (0, 1), (1, 0)]:
+                r2 = r + dr
+                c2 = c + dc
+                if not (0 <= r2 < nrows and 0 <= c2 < ncols) or grid[r2][c2] != val:
+                    continue
+                next_nodes[dice_i].add((r2, c2))
+
+    nodes = next_nodes
 
 answer3 = len(good)
 print(answer3)
