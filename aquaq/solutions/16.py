507dff887210123dc76ec1c2594fd51cf0930a32
pf981 <paulfstr@gmail.com>
2025-09-10 14:00:44 +1000

    aquaq: Complete puzzle 16

--- aquaq/solutions/16.py
+++ aquaq/solutions/16.py
@@ -1,33 +1,35 @@
-import collections
 import itertools
-import string
 
 with open("./input/16.txt") as f:
     text = f.read()
-
-text = "LTA"
 
 with open("./input/asciialphabet.txt") as f:
     ascii_str = f.read()
 
 ascii_letters = list(itertools.batched(ascii_str.splitlines(), 6))
-lefts = []
-rights = []
+left_gaps = []
+right_gaps = []
+widths = []
 for lines in ascii_letters:
-    lefts.append([line.index("#") for line in lines])
-    rights.append([len(line) - 1 - line[::-1].index("#") for line in lines])
+    left_gaps.append([line.index("#") for line in lines])
+    right_gaps.append([line[::-1].index("#") for line in lines])
+    widths.append(max(len(line) for line in lines))
 
 hash_counts = ["".join(lines).count("#") for lines in ascii_letters]
 
-# for ch in text
 width = hashes = 0
-for ch in text:
+prev = None
+for ch in text.strip():
     i = ord(ch) - ord("A")
 
-    width += 6
-    width -= 
+    kern = 0
+    if prev is not None:
+        gaps = [left + right for left, right in zip(left_gaps[i], right_gaps[prev])]
+        kern = min(gaps) - 1
 
+    width += widths[i] - kern
     hashes += hash_counts[i]
+    prev = i
 
 answer = 6 * width - hashes
 print(answer)
