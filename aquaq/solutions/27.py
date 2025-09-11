9b77d1f93f4e02dd3fd9e7c655e145b474de80d4
pf981 <paulfstr@gmail.com>
2025-09-11 23:52:04 +1000

    aquaq: Simplify solution

--- aquaq/solutions/27.py
+++ aquaq/solutions/27.py
@@ -2,20 +2,12 @@ with open("./input/27.txt") as f:
     text = f.read()
 
 grid = text.splitlines()
-nrows = len(grid)
-ncols = len(grid[0])
 
 answer = 0
-for row in grid:
+for row in grid + ["".join(col) for col in zip(*grid)]:
     answer += sum(
         sum(ord(c) - ord("a") + 1 for c in word) * len(word)
         for word in row.split()
-        if len(word) > 1
-    )
-for col in zip(*grid):
-    answer += sum(
-        sum(ord(c) - ord("a") + 1 for c in word) * len(word)
-        for word in "".join(col).split()
         if len(word) > 1
     )
 
