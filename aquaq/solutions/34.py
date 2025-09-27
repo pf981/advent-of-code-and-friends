265fe2ffae5b3dfc4f640bcfd703734b3d62cb65
pf981 <paulfstr@gmail.com>
2025-09-27 13:02:14 +1000

    aquaq: WIP: Progress on puzzle 34

--- aquaq/solutions/34.py
+++ aquaq/solutions/34.py
@@ -1,6 +1,6 @@
 import heapq
 
-with open("./input/33.txt") as f:
+with open("./input/34.txt") as f:
     text = f.read()
 
 text = """station,r1,r2,r3
@@ -12,7 +12,7 @@ d,00:46,00:51,00:47
 
 n_routes = len(text.splitlines()[0].split(",")) - 1
 routes: list[list[int | None]] = [[] for _ in range(n_routes)]
-starts = [None] * n_routes
+starts: list[int | None] = [None] * n_routes
 heap: list[tuple[int, int, int]] = []
 
 prevs: list[int | None] = [None] * n_routes
@@ -32,13 +32,16 @@ for line in text.splitlines()[1:]:
             routes[route].append(0)
 
             station = len(routes[route]) - 1
-            heapq.heappush(heap, (station, t, route))
+            heapq.heappush(heap, (t, station, route))
         else:
-            assert prevs[route] is not None
-            delta = t - prevs[route]
+            p = prevs[route]
+            assert p is not None
+            delta = t - p
             routes[route].append(delta)
 
         prevs[route] = t
+
+assert all(start is not None for start in starts)
 
 # for route, stops in enumerate(routes):
 #     for station, t in enumerate(stops):
@@ -49,8 +52,26 @@ for line in text.splitlines()[1:]:
 #         break
 ends = starts.copy()
 
+n_stations = len(routes[0])
+occupied_until = [0] * n_stations
+
 while heap:
-    station, t, route = heapq.heappop(heap)
+    t, station, route = heapq.heappop(heap)
+    print(f"{t=} {station=} {route=}")
+
+    if occupied_until[station] > t:
+        print(f"  Delayed until {occupied_until[station]}")
+        heapq.heappush(heap, (occupied_until[station], station, route))
+        continue
+
+    occupied_until[station] = t + 5
+
+    ends[route] = t
+    for station2 in range(station + 1, n_stations):
+        dt = routes[route][station2]
+        if dt is not None:
+            heapq.heappush(heap, (t + dt + 5, station2, route))
+            break
 
 
 answer = max(end - start for start, end in zip(starts, ends))
