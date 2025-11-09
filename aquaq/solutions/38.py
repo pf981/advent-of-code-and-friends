e829bc415cd3f9400f296b7042288f5a9d912c78
Paul Foster <paulfstr@gmail.com>
2025-11-09 21:50:32 +1100

    aquaq: Complete puzzle 38

--- aquaq/solutions/38.py
+++ aquaq/solutions/38.py
@@ -1,43 +1,26 @@
-import functools
-
-
-@functools.cache
-def get_streak_left(i: int, nums: tuple[int, ...]) -> tuple[int, ...]:
-    if i < 0:
-        return tuple()
-
-    return (nums[i],) + tuple(
-        streak + nums[i] for streak in get_streak_left(i - 1, nums)
-    )
-
-
-@functools.cache
-def get_streak_right(i: int, nums: tuple[int, ...]) -> tuple[int, ...]:
-    if i == len(nums):
-        return tuple()
-
-    return (nums[i],) + tuple(
-        streak + nums[i] for streak in get_streak_right(i + 1, nums)
-    )
-
-
-def get_score(i: int, nums: tuple[int, ...]) -> int:
-    score_left = score_right = 0
-    for score_left, streak in enumerate(get_streak_left(i - 1, nums), 1):
-        if streak % score_left:
-            break
-    for score_right, streak in enumerate(get_streak_right(i - 1, nums), 1):
-        if streak % score_right:
-            break
-    return max(score_left, score_right) - 1
-
+import collections
 
 with open("./input/38.txt") as f:
     text = f.read()
 
-text = """1 3 2"""
-
+answer = 0
 for line in text.splitlines():
     nums = tuple(int(s) for s in line.split())
-    for i in range(len(nums)):
-        print(get_score(i, nums))
+
+    highest_comfort: collections.defaultdict[int, int] = collections.defaultdict(
+        int
+    )  # i -> longest_comfortable_streak_length
+
+    for streak_length in range(1, len(nums) + 1):
+        for i in range(len(nums) - streak_length + 1):
+            subarray = nums[i : i + streak_length]
+
+            # Comfortable
+            if sum(subarray) % streak_length == 0:
+                for j in range(i, i + streak_length):
+                    if streak_length == highest_comfort[j] + 1:
+                        highest_comfort[j] += 1
+
+    answer += sum(highest_comfort.values())
+
+print(answer)
