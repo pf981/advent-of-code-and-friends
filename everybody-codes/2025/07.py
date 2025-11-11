1bce695d375aea8d1ede61d80ccc23a092b45192
Paul Foster <paulfstr@gmail.com>
2025-11-12 10:35:34 +1100

    everybody-codes: Complete 2025 quest 7

--- everybody-codes/2025/07.py
+++ everybody-codes/2025/07.py
@@ -1,7 +1,28 @@
-with open("./2025/input/everybody_codes_e2025_q07_p1.txt") as f:
-    lines = f.read().splitlines()
+def is_valid(name: str) -> bool:
+    for a, b in zip(name[:-1], name[1:]):
+        if b not in instructions[a]:
+            return False
+    return True
 
-answer1 = "todo"
+
+with open("./2025/input/everybody_codes_e2025_q07_p1.txt") as f:
+    text = f.read()
+
+names_str, instrs_str = text.split("\n\n")
+
+names = names_str.split(",")
+instructions = {}
+
+for line in instrs_str.splitlines():
+    a, b = line.split(" > ")
+    assert a not in instructions
+    instructions[a] = b.split(",")
+
+for name in names:
+    if is_valid(name):
+        answer1 = name
+        break
+
 print(answer1)
 
 
@@ -9,9 +30,19 @@ print(answer1)
 
 
 with open("./2025/input/everybody_codes_e2025_q07_p2.txt") as f:
-    lines = f.read().splitlines()
+    text = f.read()
 
-answer2 = "todo"
+names_str, instrs_str = text.split("\n\n")
+
+names = names_str.split(",")
+instructions = {}
+
+for line in instrs_str.splitlines():
+    a, b = line.split(" > ")
+    assert a not in instructions
+    instructions[a] = b.split(",")
+
+answer2 = sum(i for i, name in enumerate(names, 1) if is_valid(name))
 print(answer2)
 
 
@@ -19,7 +50,45 @@ print(answer2)
 
 
 with open("./2025/input/everybody_codes_e2025_q07_p3.txt") as f:
-    lines = f.read().splitlines()
+    text = f.read()
 
-answer3 = "todo"
+prefixes_str, instrs_str = text.split("\n\n")
+
+prefixes = prefixes_str.split(",")
+instructions = {}
+
+for line in instrs_str.splitlines():
+    a, b = line.split(" > ")
+    assert a not in instructions
+    instructions[a] = b.split(",")
+
+
+cur_name: list[str] = []
+all_names: set[str] = set()
+
+
+def backtrack(prev: str, length: int) -> None:
+    if length == 0:
+        all_names.add("".join(cur_name))
+        return
+
+    if prev not in instructions:
+        return
+
+    for nxt in instructions[prev]:
+        cur_name.append(nxt)
+        backtrack(nxt, length - 1)
+        cur_name.pop()
+
+
+for prefix in prefixes:
+    if not is_valid(prefix):
+        continue
+
+    for length in range(7 - len(prefix), 11 - len(prefix) + 1):
+        cur_name.append(prefix)
+        backtrack(prefix[-1], length)
+        cur_name.pop()
+
+answer3 = len(all_names)
 print(answer3)
