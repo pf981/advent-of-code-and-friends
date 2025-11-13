888db339ffad6d98d25dd541f113aa44b5bec2fe
Paul Foster <paulfstr@gmail.com>
2025-11-13 14:36:39 +1100

    everybody-codes: Simplify 2025 quest 8

--- everybody-codes/2025/08.py
+++ everybody-codes/2025/08.py
@@ -1,4 +1,5 @@
 import collections
+import itertools
 
 
 with open("./2025/input/everybody_codes_e2025_q08_p1.txt") as f:
@@ -7,25 +8,21 @@ with open("./2025/input/everybody_codes_e2025_q08_p1.txt") as f:
 nails = 32
 nums = [int(x) - 1 for x in lines[0].split(",")]
 
-answer1 = 0
-prev = nums[0]
-for num in nums[1:]:
-    d = abs(prev - num)
-    answer1 += d == nails // 2
-    prev = num
-
+answer1 = sum(abs(b - a) == nails // 2 for a, b in itertools.pairwise(nums))
 print(answer1)
 
 
 # Part 2
 
 
-def is_left(num, big, small):
-    return num > big or num < small
+def does_cross(a1: int, b1: int, a2: int, b2: int) -> bool:
+    if a1 in (a2, b2) or b1 in (a2, b2):
+        return False
 
+    b2_is_left = b2 < a1 or b2 > b1
+    a2_is_left = a2 < a1 or a2 > b1
 
-def is_right(num, big, small):
-    return not is_left(num, big, small)
+    return b2_is_left != a2_is_left
 
 
 with open("./2025/input/everybody_codes_e2025_q08_p2.txt") as f:
@@ -35,24 +32,15 @@ nails = 256
 nums = [int(x) - 1 for x in lines[0].split(",")]
 
 answer2 = 0
-pairs: list[tuple[int, int]] = []
-prev = nums[0]
-for num in nums[1:]:
-    a1, b1 = prev, num
-    if a1 < b1:
+seen: list[tuple[int, int]] = []
+for a1, b1 in itertools.pairwise(nums):
+    if a1 > b1:
         a1, b1 = b1, a1
 
-    for a, b in pairs:
-        if a in (a1, b1) or b in (a1, b1):
-            continue
+    for a2, b2 in seen:
+        answer2 += does_cross(a1, b1, a2, b2)
 
-        if (is_left(a, a1, b1) and is_right(b, a1, b1)) or (
-            is_right(a, a1, b1) and is_left(b, a1, b1)
-        ):
-            answer2 += 1
-
-    pairs.append((prev, num))
-    prev = num
+    seen.append((a1, b1))
 
 print(answer2)
 
@@ -66,19 +54,13 @@ with open("./2025/input/everybody_codes_e2025_q08_p3.txt") as f:
 nails = 256
 nums = [int(x) - 1 for x in lines[0].split(",")]
 
-pair_counts = collections.Counter(zip(nums[:-1], nums[1:]))
-
+pair_counts = collections.Counter(itertools.pairwise(nums))
 answer3 = 0
-for a1 in range(1, nails):
-    for b1 in range(a1):
+for a1 in range(nails):
+    for b1 in range(a1 + 1, nails):
         cuts = 0
-        for (a, b), count in pair_counts.items():
-            if a in (a1, b1) or b in (a1, b1):
-                continue
-
-            if (is_left(a, a1, b1) and is_right(b, a1, b1)) or (
-                is_right(a, a1, b1) and is_left(b, a1, b1)
-            ):
+        for (a2, b2), count in pair_counts.items():
+            if does_cross(a1, b1, a2, b2):
                 cuts += count
 
         cuts += pair_counts[(a1, b1)]
