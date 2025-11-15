db049c3741edcb3c37c2c1ae2ca8cd156c16a916
Paul Foster <paulfstr@gmail.com>
2025-11-15 11:14:05 +1100

    everybody-codes: Tidy 2025 quest 10

--- everybody-codes/2025/10.py
+++ everybody-codes/2025/10.py
@@ -1,14 +1,19 @@
+import functools
+
+
 with open("./2025/input/everybody_codes_e2025_q10_p1.txt") as f:
     lines = f.read().splitlines()
 
-ss = set()
+sheep = set()
 dragon = None
 for r, line in enumerate(lines):
     for c, ch in enumerate(line):
-        if ch == "D":
-            dragon = (r, c)
-        if ch == "S":
-            ss.add((r, c))
+        match ch:
+            case "D":
+                dragon = (r, c)
+            case "S":
+                sheep.add((r, c))
+assert dragon
 
 seen = {dragon}
 for _ in range(4):
@@ -30,74 +35,51 @@ for _ in range(4):
                 continue
             seen.add((r2, c2))
 
-answer = 0
-for r, c in ss:
-    answer += (r, c) in seen
-
-print(answer)
+answer1 = sum((r, c) in seen for r, c in sheep)
+print(answer1)
 
 
 # Part 2
+
 
 with open("./2025/input/everybody_codes_e2025_q10_p2.txt") as f:
     lines = f.read().splitlines()
 rounds = 20
 
-# lines = """...SSS##.....
-# .S#.##..S#SS.
-# ..S.##.S#..S.
-# .#..#S##..SS.
-# ..SSSS.#.S.#.
-# .##..SS.#S.#S
-# SS##.#D.S.#..
-# S.S..S..S###.
-# .##.S#.#....S
-# .SSS.#SS..##.
-# ..#.##...S##.
-# .#...#.S#...S
-# SS...#.S.#S..""".splitlines()
-# rounds = 3
-
-ss = set()
-hashes = set()
+sheep = set()
+hideouts = set()
 dragon = None
 for r, line in enumerate(lines):
     for c, ch in enumerate(line):
-        if ch == "D":
-            dragon = (r, c)
-        if ch == "S":
-            ss.add((r, c))
-        if ch == "#":
-            hashes.add((r, c))
+        match ch:
+            case "D":
+                dragon = (r, c)
+            case "S":
+                sheep.add((r, c))
+            case "#":
+                hideouts.add((r, c))
+assert dragon
 
-import collections
-
-seen = {dragon}
-q = collections.deque([dragon])
+dragons = {dragon}
 answer2 = 0
 for rnd in range(rounds + 1):
-    s = set(q)
-    # print(f"{s=}")
-
     if rnd > 0:
-        for r, c in ss.copy():
-            if (r + rnd - 1, c) in s:
-                if (r + rnd - 1, c) in hashes:
+        for r, c in sheep.copy():
+            if (r + rnd - 1, c) in dragons:
+                if (r + rnd - 1, c) in hideouts:
                     continue
-                ss.discard((r, c))
+                sheep.discard((r, c))
                 answer2 += 1
 
-    for r, c in ss.copy():
-        if (r + rnd, c) in s:
-            if (r + rnd, c) in hashes:
+    for r, c in sheep.copy():
+        if (r + rnd, c) in dragons:
+            if (r + rnd, c) in hideouts:
                 continue
-            ss.discard((r, c))
+            sheep.discard((r, c))
             answer2 += 1
 
-    # print(f"{rnd=} {answer2=}")
-    q = collections.deque(s)
-    for _ in range(len(q)):
-        r, c = q.popleft()
+    dragons2 = set()
+    for r, c in dragons:
         for dr, dc in [
             (-2, -1),
             (-2, 1),
@@ -111,86 +93,44 @@ for rnd in range(rounds + 1):
             r2 = r + dr
             c2 = c + dc
 
-            # if (r2, c2) in seen:
-            #     continue
-            # seen.add((r2, c2))
-            q.append((r2, c2))
+            dragons2.add((r2, c2))
+    dragons = dragons2
 
 print(answer2)
 
 
-# # Part 3
+# Part 3
 
 
 with open("./2025/input/everybody_codes_e2025_q10_p3.txt") as f:
     lines = f.read().splitlines()
 
-# # Wrong
-# lines = """SSS
-# ..#
-# #.#
-# #D.""".splitlines()
-
-# lines = """SSS
-# ..#
-# ..#
-# .##
-# .D#""".splitlines()
-# lines = """..S..
-# .....
-# ..#..
-# .....
-# ..D..""".splitlines()
-
-# # Wrong
-# lines = """.SS.S
-# #...#
-# ...#.
-# ##..#
-# .####
-# ##D.#""".splitlines()
-
-ss = set()
-hashes = set()
+sheep = set()
+hideouts = set()
 valid = set()
 dragon = None
 for r, line in enumerate(lines):
     for c, ch in enumerate(line):
-        if ch == "D":
-            dragon = (r, c)
-        if ch == "S":
-            ss.add((r, c))
-        if ch == "#":
-            hashes.add((r, c))
+        match ch:
+            case "D":
+                dragon = (r, c)
+            case "S":
+                sheep.add((r, c))
+            case "#":
+                hideouts.add((r, c))
         valid.add((r, c))
+assert dragon
 
-import functools
-
-target = frozenset(ss)
-max_rnds = len(lines) + 5
-sheep_cols = [c for _, c in ss]
+target = frozenset(sheep)
+sheep_cols = [c for _, c in sheep]
 nrows = len(lines)
-# print(f"{target=}")
-
-visiting = set()
-visited = {}
 
 
-# @functools.cache
+@functools.cache
 def count_ways(r, c, sheep_rows):
-    t = (r, c, sheep_rows)
-    if t in visited:
-        return visited[t]
-    if t in visiting:
-        assert False
-        return 0
-    visiting.add(t)
-
-    # print(f"{r=} {c=} {sheep_rows=}")
-
     # Eat
     sheep_rows = list(sheep_rows)
-    if (r, c) not in hashes:
+    if (r, c) not in hideouts:
         for i, (r2, c2) in enumerate(zip(sheep_rows, sheep_cols)):
             if (r2, c2) == (r, c):
                 sheep_rows[i] = None
@@ -198,10 +138,8 @@ def count_ways(r, c, sheep_rows):
 
     # Check
     if any(r_sheep == nrows for r_sheep in sheep_rows):
-        visited[t] = 0
         return 0
     if all(r_sheep is None for r_sheep in sheep_rows):
-        visited[t] = 1
         return 1
 
     # Sheep move
@@ -209,7 +147,7 @@ def count_ways(r, c, sheep_rows):
     for i, (r2, c2) in enumerate(zip(sheep_rows, sheep_cols)):
         if r2 is None:  # Already eaten
             continue
-        if (r, c) not in hashes and (r2 + 1, c2) == (r, c):  # Would be eaten
+        if (r, c) not in hideouts and (r2 + 1, c2) == (r, c):  # Would be eaten
             continue
 
         nxt = list(sheep_rows)
@@ -237,19 +175,8 @@ def count_ways(r, c, sheep_rows):
         for nxt in next_sheep_rows:
             result += count_ways(r2, c2, nxt)
 
-    # print(f"x")
-    visited[t] = result
     return result
 
 
 answer3 = count_ways(*dragon, tuple([0] * len(sheep_cols)))
 print(answer3)
-
-# 50174007498669
-# Your answer length is: correct
-# The first character of your answer is: correct
-
-
-# for config, cnt in visited.items():
-#     if cnt:
-#         print(config, cnt)
