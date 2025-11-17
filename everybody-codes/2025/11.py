719c087bffad4b622127b0fcbc08a4d2fb3477ec
Paul Foster <paulfstr@gmail.com>
2025-11-18 10:43:40 +1100

    everybody-codes: Tidy 2025 quest 11

--- everybody-codes/2025/11.py
+++ everybody-codes/2025/11.py
@@ -1,17 +1,9 @@
 with open("./2025/input/everybody_codes_e2025_q11_p1.txt") as f:
     lines = f.read().splitlines()
 
-# lines = """9
-# 1
-# 1
-# 4
-# 9
-# 6""".splitlines()
-
 columns = [int(line) for line in lines]
 is_phase_1 = True
 for round in range(11):
-    # print(f"{round=} {columns=}")
     if is_phase_1:
         is_phase_1 = False
         for i in range(0, len(columns) - 1):
@@ -26,123 +18,44 @@ for round in range(11):
                 columns[i + 1] -= 1
 
 answer1 = sum(i * col for i, col in enumerate(columns, 1))
-# answer1 = "todo"
 print(answer1)
 
 
-# # Part 2
+# Part 2
 
 
 with open("./2025/input/everybody_codes_e2025_q11_p2.txt") as f:
     lines = f.read().splitlines()
 
-# lines = """9
-# 1
-# 1
-# 4
-# 9
-# 6""".splitlines()
-
-# lines = """805
-# 706
-# 179
-# 48
-# 158
-# 150
-# 232
-# 885
-# 598
-# 524
-# 423""".splitlines()
-
-import itertools
-
 columns = [int(line) for line in lines]
 is_phase_1 = True
-round = 0
-while True:
-    if is_phase_1:
-        is_phase_1 = False
-        for i in range(0, len(columns) - 1):
-            if columns[i] > columns[i + 1]:
-                is_phase_1 = True
-                columns[i] -= 1
-                columns[i + 1] += 1
+rnd = -1
+while is_phase_1:
+    is_phase_1 = False
+    for i in range(0, len(columns) - 1):
+        if columns[i] > columns[i + 1]:
+            is_phase_1 = True
+            columns[i] -= 1
+            columns[i + 1] += 1
+    rnd += 1
 
-    if not is_phase_1:
-        for i in range(0, len(columns) - 1):
-            if columns[i + 1] > columns[i]:
-                columns[i] += 1
-                columns[i + 1] -= 1
+target = sum(columns) // len(columns)
+for c in columns:
+    if target - c > 0:
+        rnd += target - c
 
-    round += 1
-    # print(f"{round=} {columns=}")
-    if len(set(columns)) == 1:
-        break
-
-answer2 = round - 1
-
+answer2 = rnd
 print(answer2)
 
-# 5109772
 
-# Your answer length is: correct
-# The first character of your answer is: correct
+# Part 3
 
-# Next try 5109773 CORRECT (dont subtract one)
-
-# 49
-
-# Your answer length is: incorrect
-# The first character of your answer is: incorrect
-
-# # Part 3
-
-
-import itertools
-
-# with open("./2025/input/everybody_codes_e2025_q11_p2.txt") as f:
-#     lines = f.read().splitlines()
 
 with open("./2025/input/everybody_codes_e2025_q11_p3.txt") as f:
     lines = f.read().splitlines()
 
-# lines = """9
-# 1
-# 1
-# 4
-# 9
-# 6""".splitlines()
-
-# lines = """805
-# 706
-# 179
-# 48
-# 158
-# 150
-# 232
-# 885
-# 598
-# 524
-# 423""".splitlines()
-
-# non-dec
-# lines = """3 4 4 4 7 8""".split()
-
-
 columns = [int(line) for line in lines]
 
-# There is no phase 1
-# columns_at_phase_2 = columns.copy()  # TODO
-# rounds_in_phase_1 = ...
-
 target = sum(columns) // len(columns)
-max(columns)
-
-# 5 rounds to balance (6+5=11)
-ans = 0
-for c in columns:
-    if target - c > 0:
-        # print(target - c)
-        ans += target - c
-print(ans)
+answer3 = sum(delta for col in columns if (delta := target - col) > 0)
+print(answer3)
