c175ea02dddead125430cdf01c3fb2ac45aed26d
Paul Foster <paulfstr@gmail.com>
2025-11-20 10:08:20 +1100

    everybody-codes: Complete 2025 quest 13

--- everybody-codes/2025/13_1.py
+++ everybody-codes/2025/13_1.py
@@ -1,5 +1,28 @@
+import collections
+
 with open("./2025/input/everybody_codes_e2025_q13_p1.txt") as f:
     lines = f.read().splitlines()
 
-answer1 = "todo"
-print(answer1)
+# lines = """72
+# 58
+# 47
+# 61
+# 67""".splitlines()
+turns = 2025
+
+q = collections.deque([1])
+
+nums = [int(line) for line in lines]
+res = 0
+# i = 0
+for i, num in enumerate(nums):
+    if i % 2 == 0:
+        q.append(num)
+    else:
+        q.appendleft(num)
+
+i = q.index(1)
+ans = q[(i + turns) % len(q)]
+
+# answer1 = "todo"
+# print(answer1)
--- everybody-codes/2025/13_2.py
+++ everybody-codes/2025/13_2.py
@@ -1,5 +1,32 @@
+import collections
+
 with open("./2025/input/everybody_codes_e2025_q13_p2.txt") as f:
     lines = f.read().splitlines()
 
-answer1 = "todo"
-print(answer1)
+# lines = """72
+# 58
+# 47
+# 61
+# 67""".splitlines()
+turns = 20252025
+
+q = collections.deque([1])
+
+res = 0
+for i, lines in enumerate(lines):
+    a, b = lines.split("-")
+    a = int(a)
+    b = int(b)
+    r = range(a, b + 1)
+    for num in r:
+        if i % 2 == 0:
+            q.append(num)
+        else:
+            q.appendleft(num)
+
+i = q.index(1)
+ans = q[(i + turns) % len(q)]
+print(ans)
+
+# answer1 = "todo"
+# print(answer1)
