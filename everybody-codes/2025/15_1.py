21bf3897b8bc2ef68bf6e090b46534f2453b4a36
Paul Foster <paulfstr@gmail.com>
2025-11-21 19:43:26 +1100

    everybody-codes: Add skeleton for 2025 quest 15

---
+++ everybody-codes/2025/15_1.py
@@ -0,0 +1,6 @@
+with open("./2025/input/everybody_codes_e2025_q15_p1.txt") as f:
+    lines = f.read().splitlines()
+
+
+answer = "TODO"
+print(answer)
---
+++ everybody-codes/2025/15_2.py
@@ -0,0 +1,6 @@
+with open("./2025/input/everybody_codes_e2025_q15_p2.txt") as f:
+    lines = f.read().splitlines()
+
+
+answer = "TODO"
+print(answer)
