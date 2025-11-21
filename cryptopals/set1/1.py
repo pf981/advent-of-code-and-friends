5e1a8634e820336514759e42f31ab9c36b149527
Paul Foster <paulfstr@gmail.com>
2025-11-21 20:31:33 +1100

    cryptopals: Complete challenge 1

---
+++ cryptopals/set1/1.py
@@ -0,0 +1,8 @@
+import base64
+
+
+text = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+expected = b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+# b"I'm killing your brain like a poisonous mushroom"
+assert base64.b64encode(bytes.fromhex(text)) == expected
