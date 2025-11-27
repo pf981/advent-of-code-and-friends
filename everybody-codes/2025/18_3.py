f470197dea6ec6295585c09012ee98362029234c
Paul Foster <paulfstr@gmail.com>
2025-11-27 13:23:50 +1100

    everybody-codes: Tidy z3 in 2025 quest 18

--- everybody-codes/2025/18_3.py
+++ everybody-codes/2025/18_3.py
@@ -53,18 +53,14 @@ def run_test(
     return incoming_energy[last_node]
 
 
-def z3_zero_if_below_threshold(value, threshold):
+def z3_zero_below(value, threshold):
     return z3.If(value < threshold, 0, value)
 
 
 def get_max_energy() -> int:
     o = z3.Optimize()
 
-    inputs_z3 = []
-    for i in range(len(edges[0])):
-        input_z3 = z3.Int(f"inputs_{i}")
-        inputs_z3.append(input_z3)
-        o.add(z3.Or(input_z3 == 0, input_z3 == 1))
+    inputs_z3 = [z3.Bool(f"inputs_{i}") for i in range(len(edges[0]))]
 
     incoming_energy_z3 = collections.defaultdict(int)
     incoming_energy_z3[0] = 1
@@ -72,30 +68,23 @@ def get_max_energy() -> int:
     for node in range(len(node_thickness)):
         for to, thickness in edges[node]:
             input_z3 = inputs_z3[to - 1] if node == 0 else 1
-            incoming_energy_z3[to] = z3.Sum(
-                incoming_energy_z3[to],
-                z3_zero_if_below_threshold(
-                    incoming_energy_z3[node], node_thickness[node]
-                )
+            incoming_energy_z3[to] += (
+                z3_zero_below(incoming_energy_z3[node], node_thickness[node])
                 * thickness
-                * input_z3,
+                * input_z3
             )
-
-    outputs = {}
-    for node in range(len(node_thickness)):
-        outputs[node] = z3.Int(f"outputs_{node}")
-        o.add(
-            outputs[node]
-            == z3_zero_if_below_threshold(
-                incoming_energy_z3[node], node_thickness[node]
-            )
-        )
 
     last_node = len(node_thickness) - 1
-    o.maximize(outputs[last_node])
+    output = z3.Int("output")
+
+    o.add(
+        output
+        == z3_zero_below(incoming_energy_z3[last_node], node_thickness[last_node])
+    )
+    o.maximize(output)
 
     assert o.check() == z3.sat
-    return o.model()[outputs[last_node]].as_long()
+    return o.model()[output].py_value()
 
 
 max_energy = get_max_energy()
