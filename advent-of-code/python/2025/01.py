7da25e61a52535fb767c3242e311be6e761c53df
Paul Foster <paulfstr@gmail.com>
2025-12-01 16:11:10 +1100

    advent-of-code: Complete 2025 day 1

--- advent-of-code/python/2025/01.py
+++ advent-of-code/python/2025/01.py
@@ -3,21 +3,19 @@ from aocd import get_data, submit
 
 inp = get_data(day=1, year=2025)
 
-# inp = '''
-# '''
+p = 50
+answer1 = answer2 = 0
+for line in inp.splitlines():
+    n = int(line[1:])
+    sign = 1 if line[0] == "R" else -1
+    for _ in range(n):
+        p = (p + sign) % 100
+        answer2 += p == 0
+    answer1 += p == 0
 
-lines = inp.splitlines()
-
-answer1 = "todo"
-print(answer1)
-
-# submit(answer1, part='a', day=1, year=2025)
+submit(answer1, part="a", day=1, year=2025)
 
 
 # Part 2
 
-
-answer2 = "todo"
-print(answer2)
-
-# submit(answer2, part='b', day=1, year=2025)
+submit(answer2, part="b", day=1, year=2025)
