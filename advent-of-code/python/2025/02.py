70adaf1d2f52c96aa660269d0772b28ebc6c2b26
Paul Foster <paulfstr@gmail.com>
2025-12-02 16:15:03 +1100

    advent-of-code: Complete 2025 day 2

--- advent-of-code/python/2025/02.py
+++ advent-of-code/python/2025/02.py
@@ -1,16 +1,57 @@
+import itertools
+
 from aocd import get_data, submit
 
 
 inp = get_data(day=2, year=2025)
-
 lines = inp.splitlines()
 
-answer1 = None
-# submit(answer1, part="a", day=1, year=2025)
+
+def is_invalid(num: int) -> bool:
+    s = str(num)
+    w = len(s) // 2
+
+    if len(s) % 2 == 1:
+        return False
+
+    return s[:w] == s[w:]
+
+
+answer1 = 0
+for rng in lines[0].split(","):
+    a, b = rng.split("-")
+    a = int(a)
+    b = int(b)
+    for num in range(a, b + 1):
+        if is_invalid(num):
+            answer1 += num
+
+submit(answer1, part="a", day=2, year=2025)
 
 
 # Part 2
 
 
-# answer2 = None
-# submit(answer2, part="b", day=1, year=2025)
+def is_invalid(num: int) -> bool:
+    s = str(num)
+
+    for w in range(1, len(s) // 2 + 1):
+        if len(s) % w != 0:
+            continue
+
+        if len(set(["".join(chars) for chars in itertools.batched(s, w)])) == 1:
+            return True
+
+    return False
+
+
+answer2 = 0
+for rng in lines[0].split(","):
+    a, b = rng.split("-")
+    a = int(a)
+    b = int(b)
+    for num in range(a, b + 1):
+        if is_invalid(num):
+            answer2 += num
+
+submit(answer2, part="b", day=2, year=2025)
