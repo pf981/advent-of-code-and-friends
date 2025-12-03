ab9328c3fa955d695a24be91f45f148dd38bc917
Paul Foster <paulfstr@gmail.com>
2025-12-03 11:08:53 +1100

    advent-of-code: Add submit to 2025 day 1

--- advent-of-code/python/2015/2015_01.py
+++ advent-of-code/python/2015/2015_01.py
@@ -1,14 +1,19 @@
-from aocd import get_data
+from aocd import get_data, submit
+
 
 inp = get_data(day=1, year=2015)
 
-answer = sum(1 if x == '(' else -1 for x in inp)
-answer
+answer1 = sum(1 if x == '(' else -1 for x in inp)
+submit(str(answer1), part="a", day=1, year=2015)
+
+
+# Part 2
+
 
 floor = 0
 for position, x in enumerate(inp, 1):
   floor += 1 if x == '(' else -1
   if (floor == -1):
     break
-answer = position
-answer
+answer2 = position
+submit(str(answer2), part="b", day=1, year=2015)
