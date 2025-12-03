9fed67b1705e5a957317664e32d5542358f126a1
Paul Foster <paulfstr@gmail.com>
2025-12-03 16:13:04 +1100

    advent-of-code: Complete 2025 day 3

--- advent-of-code/python/2025/03.py
+++ advent-of-code/python/2025/03.py
@@ -2,15 +2,73 @@ from aocd import get_data, submit
 
 
 inp = get_data(day=3, year=2025)
+
+# inp = '''
+# '''
+
 lines = inp.splitlines()
+answer1=0
+for line in lines:
+    max_val = 0
+    max_prev = 0
+    for x in line:
+        x = int(x)
+        max_val = max(max_val, 10 * max_prev + x)
+        if x  > max_prev:
+            max_prev = x
+    answer1 += max_val
 
+# answer1 = None
+print(answer1)
 
-answer1 = None
-# submit(answer1, part="a", day=2, year=2025)
+submit(answer1, part='a', day=3, year=2025)
 
 
 # Part 2
+import functools
+
+lines = inp.splitlines()
+
+@functools.cache
+def get_max_val(i, n, word):
+    if n == 0:
+        # if i != len(word):
+        #     return None
+        return 0
+    
+    if i == len(word):
+        return None
+
+    # Discard
+    discard = get_max_val(i + 1, n, word)
+
+    # Keep
+    res = get_max_val(i + 1, n - 1, word)
+    if res is not None:
+        keep = int(word[i]) * (10**(n-1)) + get_max_val(i + 1, n - 1, word)
+    else:
+        keep = None
+
+    # return max(discard or 0, keep or 0)
+    if discard is None and keep is None:
+        return None
+    if discard is None:
+        return keep
+    if keep is None:
+        return discard
+
+    return max(discard, keep)
+
+answer2=0
+for line in lines:
+    # max_val = get_max_val(0, 12, line)
+    max_val = get_max_val(0, 12, line)
+    print(f'{max_val=}')
+    # if max_val is None:
+    #     max_val = 0
+    answer2 += max_val
 
 
-answer2 = None
-# submit(answer2, part="b", day=2, year=2025)
+print(answer2)
+
+submit(answer2, part='b', day=3, year=2025)
