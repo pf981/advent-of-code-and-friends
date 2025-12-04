9264f8834d18706bd3f5ed102068b045e54b766e
pf981 <paulfstr@gmail.com>
2025-12-04 22:38:30 +1100

    advent-of-code: Tidy 2025 day 2

--- advent-of-code/python/2025/02.py
+++ advent-of-code/python/2025/02.py
@@ -1,30 +1,14 @@
-import itertools
-
 from aocd import get_data, submit
 
 
 inp = get_data(day=2, year=2025)
-lines = inp.splitlines()
 
-
-def is_invalid(num: int) -> bool:
-    s = str(num)
-    w = len(s) // 2
-
-    if len(s) % 2 == 1:
-        return False
-
-    return s[:w] == s[w:]
-
+intervals = [[int(x) for x in s.split("-")] for s in inp.split(",")]
 
 answer1 = 0
-for rng in lines[0].split(","):
-    a, b = rng.split("-")
-    a = int(a)
-    b = int(b)
-    for num in range(a, b + 1):
-        if is_invalid(num):
-            answer1 += num
+for num in range(100_000):
+    invalid = int(str(num) * 2)
+    answer1 += sum(invalid for a, b in intervals if a <= invalid <= b)
 
 submit(answer1, part="a", day=2, year=2025)
 
@@ -32,26 +16,16 @@ submit(answer1, part="a", day=2, year=2025)
 # Part 2
 
 
-def is_invalid(num: int) -> bool:
-    s = str(num)
-
-    for w in range(1, len(s) // 2 + 1):
-        if len(s) % w != 0:
-            continue
-
-        if len(set(["".join(chars) for chars in itertools.batched(s, w)])) == 1:
-            return True
-
-    return False
-
-
 answer2 = 0
-for rng in lines[0].split(","):
-    a, b = rng.split("-")
-    a = int(a)
-    b = int(b)
-    for num in range(a, b + 1):
-        if is_invalid(num):
-            answer2 += num
+seen = set()
+for num in range(100_000):
+    for n in range(2, 10):
+        invalid = int(str(num) * n)
+
+        if invalid in seen:
+            continue
+        seen.add(invalid)
+
+        answer2 += sum(invalid for a, b in intervals if a <= invalid <= b)
 
 submit(answer2, part="b", day=2, year=2025)
