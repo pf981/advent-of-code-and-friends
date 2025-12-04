df6738c50cedbb3e28c47ddbedb3bba81664a216
Paul Foster <paulfstr@gmail.com>
2025-12-04 11:47:38 +1100

    advent-of-code: Add skeleton for 2025 day 4

---
+++ advent-of-code/python/2025/04_1.py
@@ -0,0 +1,14 @@
+from aocd import get_data, submit
+
+
+inp = get_data(day=4, year=2025)
+
+# inp = '''
+# '''
+
+lines = inp.splitlines()
+
+answer1 = None
+print(answer1)
+
+# submit(answer1, part='a', day=4, year=2025)
---
+++ advent-of-code/python/2025/04_2.py
@@ -0,0 +1,14 @@
+from aocd import get_data, submit
+
+
+inp = get_data(day=4, year=2025)
+
+# inp = '''
+# '''
+
+lines = inp.splitlines()
+
+answer2 = None
+print(answer2)
+
+# submit(answer1, part='b', day=4, year=2025)
