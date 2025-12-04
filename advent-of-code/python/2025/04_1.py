5765ec4becd7c5a4fcc29a27e314b684c147f73d
Paul Foster <paulfstr@gmail.com>
2025-12-04 16:08:16 +1100

    advent-of-code: Complete 2025 day 4

--- advent-of-code/python/2025/04_1.py
+++ advent-of-code/python/2025/04_1.py
@@ -3,12 +3,42 @@ from aocd import get_data, submit
 
 inp = get_data(day=4, year=2025)
 
-# inp = '''
-# '''
+# inp = """..@@.@@@@.
+# @@@.@.@.@@
+# @@@@@.@.@@
+# @.@@@@..@.
+# @@.@@@@.@@
+# .@@@@@@@.@
+# .@.@.@.@@@
+# @.@@@.@@@@
+# .@@@@@@@@.
+# @.@.@@@.@.
+# """
 
 lines = inp.splitlines()
+ats = {(r, c) for r, line in enumerate(lines) for c, ch in enumerate(line) if ch == "@"}
+dots = {
+    (r, c) for r, line in enumerate(lines) for c, ch in enumerate(line) if ch == "."
+}
 
-answer1 = None
+answer1 = 0
+for r, c in ats:
+    nei = 0
+    for dr, dc in [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ]:
+        r2 = r + dr
+        c2 = c + dc
+        nei += (r2, c2) in ats
+    answer1 += nei < 4
 print(answer1)
 
-# submit(answer1, part='a', day=4, year=2025)
+submit(answer1, part="a", day=4, year=2025)
+# 924 not right
--- advent-of-code/python/2025/04_2.py
+++ advent-of-code/python/2025/04_2.py
@@ -3,12 +3,54 @@ from aocd import get_data, submit
 
 inp = get_data(day=4, year=2025)
 
-# inp = '''
-# '''
+# inp = """..@@.@@@@.
+# @@@.@.@.@@
+# @@@@@.@.@@
+# @.@@@@..@.
+# @@.@@@@.@@
+# .@@@@@@@.@
+# .@.@.@.@@@
+# @.@@@.@@@@
+# .@@@@@@@@.
+# @.@.@@@.@.
+# """
 
 lines = inp.splitlines()
+ats = {(r, c) for r, line in enumerate(lines) for c, ch in enumerate(line) if ch == "@"}
+dots = {
+    (r, c) for r, line in enumerate(lines) for c, ch in enumerate(line) if ch == "."
+}
 
-answer2 = None
+answer2 = 0
+while True:
+    # print("X")
+    l = len(ats)
+    to_pop = []
+    for r, c in ats:
+        nei = 0
+        for dr, dc in [
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        ]:
+            r2 = r + dr
+            c2 = c + dc
+            nei += (r2, c2) in ats
+        if nei < 4:
+            to_pop.append((r, c))
+        answer2 += nei < 4
+
+    # print(to_pop)
+    for p in to_pop:
+        ats.discard(p)
+    if len(ats) == l:
+        break
 print(answer2)
 
-# submit(answer2, part='b', day=4, year=2025)
+submit(answer2, part="b", day=4, year=2025)
+# 924 not right
