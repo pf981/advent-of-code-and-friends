219e30a181fe97601f4bf1677ef5acab6cf789e1
Paul Foster <paulfstr@gmail.com>
2025-12-05 16:09:56 +1100

    advent-of-code: Complete 2025 day 5

--- advent-of-code/python/2025/05_1.py
+++ advent-of-code/python/2025/05_1.py
@@ -5,7 +5,18 @@ inp = get_data(day=5, year=2025)
 # inp = '''
 # '''
 
-lines = inp.splitlines()
+rngs_str, ings = inp.split("\n\n")
+ings = [int(x) for x in ings.splitlines()]
+fresh = set()
+for line in rngs_str.splitlines():
+    a, b = line.split("-")
+    # s = set(range(int(a), int(b)))
+    s = range(int(a), int(b))
+    for ing in ings:
+        if ing in s:
+            fresh.add(ing)
 
-answer1 = None
+
+answer1 = len(fresh)
+print(answer1)
 # submit(answer1, part="a", day=5, year=2025)
--- advent-of-code/python/2025/05_2.py
+++ advent-of-code/python/2025/05_2.py
@@ -2,10 +2,40 @@ from aocd import get_data, submit
 
 
 inp = get_data(day=5, year=2025)
-# inp = '''
-# '''
+# inp = """3-5
+# 10-14
+# 16-20
+# 12-18
 
-lines = inp.splitlines()
+# 1
+# 5
+# 8
+# 11
+# 17
+# 32"""
 
-answer2 = None
-# submit(answer2, part="b", day=5, year=2025)
+rngs_str, ings = inp.split("\n\n")
+intervals = []
+for line in rngs_str.splitlines():
+    a, b = line.split("-")
+    # s = set(range(int(a), int(b)))
+    intervals.append((int(a), int(b)))
+
+
+intervals.sort()
+start = intervals[0][0]
+end = intervals[0][1]
+answer2 = 0
+for a, b in intervals:
+    if a <= end:
+        end = max(end, b)
+    else:
+        answer2 += end - start + 1
+        start = a
+        end = b
+answer2 += end - start + 1
+print(answer2)
+# answer2 = len(fresh)
+# print(answer2)
+submit(answer2, part="b", day=5, year=2025)
+# 440382347383112 wrong.
