cd222174336b59742eae9ad0d7e875592002affe
Paul Foster <paulfstr@gmail.com>
2025-12-05 16:21:09 +1100

    advent-of-code: Simplify 2025 day 5

--- advent-of-code/python/2025/05_1.py
+++ advent-of-code/python/2025/05_1.py
@@ -4,12 +4,14 @@ from aocd import get_data, submit
 inp = get_data(day=5, year=2025)
 
 intervals_str, ingredients_str = inp.split("\n\n")
-intervals = [[int(x) for x in line.split("-")] for line in intervals_str.splitlines()]
+ranges = [
+    range(*(int(x) for x in line.split("-"))) for line in intervals_str.splitlines()
+]
 ingredients = [int(x) for x in ingredients_str.splitlines()]
+
 fresh = set()
-for a, b in intervals:
-    rng = range(int(a), int(b))
-    for ingredient in ingredients:
+for ingredient in ingredients:
+    for rng in ranges:
         if ingredient in rng:
             fresh.add(ingredient)
 
