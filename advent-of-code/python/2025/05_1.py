0ab26dc6d8573e45524880cfc09605a857e44faf
Paul Foster <paulfstr@gmail.com>
2025-12-05 16:13:02 +1100

    advent-of-code: Tidy 2025 day 5

--- advent-of-code/python/2025/05_1.py
+++ advent-of-code/python/2025/05_1.py
@@ -2,21 +2,16 @@ from aocd import get_data, submit
 
 
 inp = get_data(day=5, year=2025)
-# inp = '''
-# '''
 
-rngs_str, ings = inp.split("\n\n")
-ings = [int(x) for x in ings.splitlines()]
+intervals_str, ingredients_str = inp.split("\n\n")
+intervals = [[int(x) for x in line.split("-")] for line in intervals_str.splitlines()]
+ingredients = [int(x) for x in ingredients_str.splitlines()]
 fresh = set()
-for line in rngs_str.splitlines():
-    a, b = line.split("-")
-    # s = set(range(int(a), int(b)))
-    s = range(int(a), int(b))
-    for ing in ings:
-        if ing in s:
-            fresh.add(ing)
-
+for a, b in intervals:
+    rng = range(int(a), int(b))
+    for ingredient in ingredients:
+        if ingredient in rng:
+            fresh.add(ingredient)
 
 answer1 = len(fresh)
-print(answer1)
-# submit(answer1, part="a", day=5, year=2025)
+submit(answer1, part="a", day=5, year=2025)
--- advent-of-code/python/2025/05_2.py
+++ advent-of-code/python/2025/05_2.py
@@ -2,29 +2,12 @@ from aocd import get_data, submit
 
 
 inp = get_data(day=5, year=2025)
-# inp = """3-5
-# 10-14
-# 16-20
-# 12-18
 
-# 1
-# 5
-# 8
-# 11
-# 17
-# 32"""
-
-rngs_str, ings = inp.split("\n\n")
-intervals = []
-for line in rngs_str.splitlines():
-    a, b = line.split("-")
-    # s = set(range(int(a), int(b)))
-    intervals.append((int(a), int(b)))
-
+intervals_str, _ = inp.split("\n\n")
+intervals = [[int(x) for x in line.split("-")] for line in intervals_str.splitlines()]
 
 intervals.sort()
-start = intervals[0][0]
-end = intervals[0][1]
+start, end = intervals[0]
 answer2 = 0
 for a, b in intervals:
     if a <= end:
@@ -34,8 +17,5 @@ for a, b in intervals:
         start = a
         end = b
 answer2 += end - start + 1
-print(answer2)
-# answer2 = len(fresh)
-# print(answer2)
+
 submit(answer2, part="b", day=5, year=2025)
-# 440382347383112 wrong.
