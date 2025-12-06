0fe7c91f97e0a8c387f78e7465bf35ace18fc2e3
Paul Foster <paulfstr@gmail.com>
2025-12-06 16:21:40 +1100

    advent-of-code: Tidy 2025 day 6

--- advent-of-code/python/2025/06_1.py
+++ advent-of-code/python/2025/06_1.py
@@ -1,25 +1,18 @@
-from aocd import get_data, submit
 import math
 
-inp = get_data(day=6, year=2025)
-# inp = """123 328  51 64
-#  45 64  387 23
-#   6 98  215 314
-# *   +   *   + """
+from aocd import get_data, submit
 
-lines = inp.splitlines()
-x = []
-for line in lines:
-    x.append(line.split())
-z = list(zip(*x))
+
+inp = get_data(day=6, year=2025)
+
+rows = [line.split() for line in inp.splitlines()]
+cols = list(zip(*rows))
 
 answer1 = 0
-for r in z:
-    if r[-1] == "+":
-        answer1 += sum(int(f) for f in r[:-1])
-    elif r[-1] == "*":
-        answer1 += math.prod(int(f) for f in r[:-1])
+for col in cols:
+    nums = [int(x) for x in col[:-1]]
+    op = {"+": sum, "*": math.prod}[col[-1]]
+    answer1 += op(nums)
 
 print(answer1)
-# answer1 = None
 submit(answer1, part="a", day=6, year=2025)
--- advent-of-code/python/2025/06_2.py
+++ advent-of-code/python/2025/06_2.py
@@ -1,41 +1,30 @@
-from aocd import get_data, submit
 import math
-import itertools
+
+from aocd import get_data, submit
+
 
 inp = get_data(day=6, year=2025)
-# inp = """123 328  51 64
-#  45 64  387 23
-#   6 98  215 314
-# *   +   *   + """
 
-lines = inp.splitlines()
-x = []
-for line in lines:
-    # x.append(line.split())
-    x.append(line)
-z = list(itertools.zip_longest(*x, fillvalue=" "))
+rows = [line for line in inp.splitlines()]
+cols = list(zip(*rows))
+cols.append(tuple())
 
 answer2 = 0
-cur = []
-op = sum
-for r in z:
-    try:
-        num = int("".join(c for c in r if c.isdigit()))
-    except ValueError:
+nums = []
+op = None
+for col in cols:
+    num_str = "".join(c for c in col if c.isdigit())
+
+    if not num_str:
+        assert op
+        answer2 += op(nums)
+        op = None
+        nums = []
         continue
-    print(num)
-    if r[-1] in "*+":
-        answer2 += op(cur)
 
-    if r[-1] == "+":
-        op = sum
-        cur = []
-    elif r[-1] == "*":
-        op = math.prod
-        cur = []
-    cur.append(num)
+    if op is None:
+        op = {"+": sum, "*": math.prod}[col[-1]]
 
-answer2 += op(cur)
-print(answer2)
-# answer1 = None
+    nums.append(int(num_str))
+
 submit(answer2, part="b", day=6, year=2025)
