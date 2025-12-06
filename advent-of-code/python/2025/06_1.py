4fe540729c24432933025200008555948eea3ad6
Paul Foster <paulfstr@gmail.com>
2025-12-06 16:09:37 +1100

    advent-of-code: Complete 2025 day 6

--- advent-of-code/python/2025/06_1.py
+++ advent-of-code/python/2025/06_1.py
@@ -1,11 +1,25 @@
 from aocd import get_data, submit
-
+import math
 
 inp = get_data(day=6, year=2025)
-# inp = '''
-# '''
+# inp = """123 328  51 64
+#  45 64  387 23
+#   6 98  215 314
+# *   +   *   + """
 
 lines = inp.splitlines()
+x = []
+for line in lines:
+    x.append(line.split())
+z = list(zip(*x))
 
-answer1 = None
-# submit(answer1, part="a", day=6, year=2025)
+answer1 = 0
+for r in z:
+    if r[-1] == "+":
+        answer1 += sum(int(f) for f in r[:-1])
+    elif r[-1] == "*":
+        answer1 += math.prod(int(f) for f in r[:-1])
+
+print(answer1)
+# answer1 = None
+submit(answer1, part="a", day=6, year=2025)
--- advent-of-code/python/2025/06_2.py
+++ advent-of-code/python/2025/06_2.py
@@ -1,11 +1,41 @@
 from aocd import get_data, submit
-
+import math
+import itertools
 
 inp = get_data(day=6, year=2025)
-# inp = '''
-# '''
+# inp = """123 328  51 64
+#  45 64  387 23
+#   6 98  215 314
+# *   +   *   + """
 
 lines = inp.splitlines()
+x = []
+for line in lines:
+    # x.append(line.split())
+    x.append(line)
+z = list(itertools.zip_longest(*x, fillvalue=" "))
 
-answer2 = None
-# submit(answer2, part="b", day=6, year=2025)
+answer2 = 0
+cur = []
+op = sum
+for r in z:
+    try:
+        num = int("".join(c for c in r if c.isdigit()))
+    except ValueError:
+        continue
+    print(num)
+    if r[-1] in "*+":
+        answer2 += op(cur)
+
+    if r[-1] == "+":
+        op = sum
+        cur = []
+    elif r[-1] == "*":
+        op = math.prod
+        cur = []
+    cur.append(num)
+
+answer2 += op(cur)
+print(answer2)
+# answer1 = None
+submit(answer2, part="b", day=6, year=2025)
