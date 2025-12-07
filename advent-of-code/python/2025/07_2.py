0d815a73d7a535138038ad0bdd6fa71ca04f7141
Paul Foster <paulfstr@gmail.com>
2025-12-07 16:10:19 +1100

    advent-of-code: Complete 2025 day 7

--- advent-of-code/python/2025/07_2.py
+++ advent-of-code/python/2025/07_2.py
@@ -1,11 +1,43 @@
+import functools
+
 from aocd import get_data, submit
 
 
 inp = get_data(day=7, year=2025)
-# inp = """
+# inp = """.......S.......
+# ...............
+# .......^.......
+# ...............
+# ......^.^......
+# ...............
+# .....^.^.^.....
+# ...............
+# ....^.^...^....
+# ...............
+# ...^.^...^.^...
+# ...............
+# ..^...^.....^..
+# ...............
+# .^.^.^.^.^...^.
+# ...............
 # """
+
 
 lines = inp.splitlines()
 
-answer2 = None
-# submit(answer2, part="b", day=7, year=2025)
+
+@functools.cache
+def count_ways(r, c):
+    if r == len(lines):
+        return 1
+
+    if lines[r][c] == "^":
+        return count_ways(r, c - 1) + count_ways(r, c + 1)
+    return count_ways(r + 1, c)
+
+
+answer2 = count_ways(0, lines[0].index("S"))
+
+print(answer2)
+
+submit(answer2, part="b", day=7, year=2025)
