395023a0f67e583503f53310022955cfe16743c0
Paul Foster <paulfstr@gmail.com>
2025-12-10 16:43:53 +1100

    advent-of-code: Tidy 2025 day 10

--- advent-of-code/python/2025/10_1.py
+++ advent-of-code/python/2025/10_1.py
@@ -8,7 +8,7 @@ def get_min_presses(i, target, buttons):
         return float("inf")
 
     # Don't press
-    result = get_min_presses(i + 1, target, buttons)
+    dont_press = get_min_presses(i + 1, target, buttons)
 
     # Press
     target2 = target.copy()
@@ -16,25 +16,19 @@ def get_min_presses(i, target, buttons):
         target2[j] = not target2[j]
     press = 1 + get_min_presses(i + 1, target2, buttons)
 
-    result = min(result, press)
-    return result
+    return min(dont_press, press)
 
 
 inp = get_data(day=10, year=2025)
-# inp = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-# [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
-# [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
-# """
 lines = inp.splitlines()
 
 answer1 = 0
 for line in lines:
-    target, *buttons, costs = line.split()
+    target, *buttons, joltages = line.split()
     target = [c == "#" for c in target[1:-1]]
     buttons = [[int(x) for x in part[1:-1].split(",")] for part in buttons]
-    costs = [int(x) for x in costs[1:-1].split(",")]
-    # print(f"{target=} {buttons=} {costs}")
+    joltages = [int(x) for x in joltages[1:-1].split(",")]
+
     answer1 += get_min_presses(0, target, buttons)
 
-print(answer1)
 submit(answer1, part="a", day=10, year=2025)
--- advent-of-code/python/2025/10_2.py
+++ advent-of-code/python/2025/10_2.py
@@ -1,53 +1,42 @@
 from aocd import get_data, submit
-import copy
+import z3
 
 
 def get_min_presses(i, target, buttons):
-    # print(f"{i=} {target=} {buttons=}")
-    if i == len(buttons):
-        if all(not t for t in target):
-            return 0
-        return float("inf")
+    o = z3.Optimize()
 
-    # Don't press
-    result = get_min_presses(i + 1, target, buttons)
+    presses = [z3.Int(f"press_{i}") for i in range(len(buttons))]
+    for p in presses:
+        o.add(p >= 0)
 
-    # Press
-    joltage2 = copy.deepcopy(target)
-    press = float("inf")
-    for press_count in range(1, 200):
-        for j in buttons[i]:
-            joltage2[j] -= 1
-            if joltage2[j] < 0:
-                # if i == 0:
-                #     print(f"Stopping i=0 at {press_count=} {joltage2=} {joltage2[i]=}")
-                break
-        else:
-            press = min(press, press_count + get_min_presses(i + 1, joltage2, buttons))
-            continue
-        break
+    for i, t in enumerate(target):
+        eq = 0
+        for btn, jolt_is in enumerate(buttons):
+            if i not in jolt_is:
+                continue
+            eq = presses[btn] + eq
+        o.add(eq == t)
 
-    result = min(result, press)
-    return result
+    output = z3.Int("output")
+    o.add(output == z3.Sum(presses))
+    o.minimize(output)
+
+    res = o.check()
+    assert res == z3.sat
+
+    return o.model()[output].py_value()
 
 
 inp = get_data(day=10, year=2025)
-# inp = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-# [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
-# [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
-# """
-# inp = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
-# """
 lines = inp.splitlines()
 
 answer2 = 0
 for line in lines:
-    target, *buttons, costs = line.split()
+    target, *buttons, joltages = line.split()
     target = [c == "#" for c in target[1:-1]]
     buttons = [[int(x) for x in part[1:-1].split(",")] for part in buttons]
-    joltage = [int(x) for x in costs[1:-1].split(",")]
-    # print(f"{target=} {buttons=} {costs}")
-    answer2 += get_min_presses(0, joltage, buttons)
+    joltages = [int(x) for x in joltages[1:-1].split(",")]
 
-print(answer2)
+    answer2 += get_min_presses(0, tuple(joltages), tuple(tuple(b) for b in buttons))
+
 submit(answer2, part="b", day=10, year=2025)
