fa9e766484f722f5b3dab10f0a96572f2a243693
Paul Foster <paulfstr@gmail.com>
2025-12-11 16:06:49 +1100

    advent-of-code: Complete 2025 day 11

--- advent-of-code/python/2025/11_1.py
+++ advent-of-code/python/2025/11_1.py
@@ -1,10 +1,41 @@
 from aocd import get_data, submit
+import re
+import functools
 
 inp = get_data(day=11, year=2025)
-# inp = '''
-# '''
+# inp = """aaa: you hhh
+# you: bbb ccc
+# bbb: ddd eee
+# ccc: ddd eee fff
+# ddd: ggg
+# eee: out
+# fff: out
+# ggg: out
+# hhh: ccc fff iii
+# iii: out
+# """
 
-# lines = inp.splitlines()
 
-answer1 = None
-# submit(answer1, part="a", day=11, year=2025)
+lines = inp.splitlines()
+m = {}
+for line in lines:
+    from_, *to = re.findall(r"[a-z]{3}", line)
+    m[from_] = to
+
+
+@functools.cache
+def count_ways(node, parent):
+    if node == "out":
+        return 1
+
+    ways = 0
+    for to in m[node]:
+        if node == parent:
+            continue
+        ways += count_ways(to, node)
+    return ways
+
+
+answer1 = count_ways("you", "")
+print(answer1)
+submit(answer1, part="a", day=11, year=2025)
--- advent-of-code/python/2025/11_2.py
+++ advent-of-code/python/2025/11_2.py
@@ -1,10 +1,51 @@
 from aocd import get_data, submit
+import re
+import functools
 
 inp = get_data(day=11, year=2025)
-# inp = '''
-# '''
+# inp = """svr: aaa bbb
+# aaa: fft
+# fft: ccc
+# bbb: tty
+# tty: ccc
+# ccc: ddd eee
+# ddd: hub
+# hub: fff
+# eee: dac
+# dac: fff
+# fff: ggg hhh
+# ggg: out
+# hhh: out
+# """
 
-# lines = inp.splitlines()
 
-answer2 = None
-# submit(answer2, part="b", day=11, year=2025)
+lines = inp.splitlines()
+m = {}
+for line in lines:
+    from_, *to = re.findall(r"[a-z]{3}", line)
+    m[from_] = to
+
+
+@functools.cache
+def count_ways(node, parent, has_dac, has_fft):
+    if node == "out":
+        return int(has_dac) and int(has_fft)
+
+    if node == "dac":
+        has_dac = True
+    if node == "fft":
+        has_fft = True
+
+    ways = 0
+    for to in m[node]:
+        if node == parent:
+            continue
+        ways += count_ways(to, node, has_dac, has_fft)
+    return ways
+
+
+answer2 = count_ways("svr", "", False, False)
+print(answer2)
+submit(answer2, part="b", day=11, year=2025)
+# 278341040353001229
+# not right
