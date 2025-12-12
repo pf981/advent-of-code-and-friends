68db0235108683c92374bc9e3e93d66ee7319819
Paul Foster <paulfstr@gmail.com>
2025-12-12 16:39:58 +1100

    advent-of-code: Complete 2025 day 12

--- advent-of-code/python/2025/12_1.py
+++ advent-of-code/python/2025/12_1.py
@@ -1,10 +1,14 @@
 from aocd import get_data, submit
-
+import re
 
 inp = get_data(day=12, year=2025)
-# inp = """
-# """
-# lines = inp.splitlines()
+*presents_str, regions = inp.split("\n\n")
 
-answer1 = None
-# submit(answer1, part="a", day=12, year=2025)
+areas = [present_str.count("#") for present_str in presents_str]
+answer1 = 0
+for line in regions.splitlines():
+    w, h, *required = (int(x) for x in re.findall(r"\d+", line))
+    required_area = sum(count * areas[i] for i, count in enumerate(required))
+    answer1 += required_area <= w * h
+
+submit(answer1, part="a", day=12, year=2025)
