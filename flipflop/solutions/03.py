f594bd18d3c2a387edc5951570e626e3f126ea0f
pf981 <paulfstr@gmail.com>
2025-12-16 00:56:35 +1100

    flipflop: Complete puzzle 3

---
+++ flipflop/solutions/03.py
@@ -0,0 +1,25 @@
+import collections
+
+
+with open("./input/03.txt") as f:
+    lines = f.read().splitlines()
+
+answer1 = collections.Counter(lines).most_common(1)[0][0]
+print(answer1)
+
+answer2 = 0
+for line in lines:
+    nums = [int(x) for x in line.split(",")]
+    if len(set(nums)) != 3:
+        continue
+    answer2 += nums[1] == max(nums)
+print(answer2)
+
+answer3 = 0
+for line in lines:
+    nums = [int(x) for x in line.split(",")]
+    if len(set(nums)) != 3:
+        answer3 += 10
+        continue
+    answer3 += [5, 2, 4][nums.index(max(nums))]
+print(answer3)
