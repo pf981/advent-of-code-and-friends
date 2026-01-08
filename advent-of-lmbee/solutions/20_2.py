bf4e3eb4e46ddf2b5885c13c57fe2797b0f46411
Paul Foster <paulfstr@gmail.com>
2026-01-08 20:59:24 +1100

    advent-of-lmbee: Complete day 20 part 2

--- advent-of-lmbee/solutions/20_2.py
+++ advent-of-lmbee/solutions/20_2.py
@@ -1,31 +1,78 @@
-# Too slow
+import math
 import re
 
-import z3
+
+def circle_circle_intersections(
+    x1: int, y1: int, r1: int, x2: int, y2: int, r2: int
+) -> list[tuple[int, int]]:
+    dx = x2 - x1
+    dy = y2 - y1
+    d = math.hypot(dx, dy)
+
+    # No intersection if:
+    # - too far apart
+    # - one circle inside the other
+    # - same centre
+    if d > r1 + r2 or d < abs(r1 - r2) or d == 0:
+        return []
+
+    a = (r1 * r1 - r2 * r2 + d * d) / (2 * d)
+    h_sq = r1 * r1 - a * a
+    if h_sq < 0:
+        return []
+    h = math.sqrt(h_sq)
+
+    x3 = x1 + a * dx / d
+    y3 = y1 + a * dy / d
+
+    rx = -dy * (h / d)
+    ry = dx * (h / d)
+
+    points = [
+        (math.floor(x3 + rx), math.floor(y3 + ry)),
+        (math.floor(x3 - rx), math.floor(y3 - ry)),
+    ]
+    for x, y in points.copy():
+        points.append((x + 1, y))
+        points.append((x, y + 1))
+        points.append((x + 1, y + 1))
+
+    return points
+
+
+def count_overlaps(x: int, y: int, circles: list[list[int]]) -> int:
+    overlaps = 0
+    for x1, y1, r in circles:
+        overlaps += math.dist((x, y), (x1, y1)) <= r
+    return overlaps
+
 
 with open("data/day20.txt") as f:
     text = f.read()
-# text = """(4, 6) r=3
-# (3, 7) r=1
-# (12, 14) r=9
-# (10, 6) r=5"""
 
 circles = [[int(x) for x in re.findall(r"\d+", line)] for line in text.splitlines()]
 
-o = z3.Optimize()
+# I believe there are two possibilities for where the point could be.
+#     1. If a circle is fully contained within the region of most overlap,
+#        then every point in that circle will be the solution
+#     2. Otherwise, the point must lie on the edge intersection of two circles
+#
+# It can't be (1) because we know there is exactly 1 solution. That would only
+# work if there was a tiny circle with only one integer coordinate.
+#
+# There not being a third option is a hand-waving assumption. So it "must" be (2).
+#
+# So just iterate through all of the intesections and count the overlaps at
+# those points. Try integer coordinates around the intersections.
 
-x_z3 = z3.Int("x")
-y_z3 = z3.Int("y")
 
-overlaps = [
-    (x_z3 - x) * (x_z3 - x) + (y_z3 - y) * (y_z3 - y) < r * r for x, y, r in circles
-]
+best = 0, 0, 0  # overlap, x, y
+for c1 in circles:
+    for c2 in circles:
+        if c1 == c2:
+            continue
+        for x, y in circle_circle_intersections(*c1, *c2):
+            best = max(best, (count_overlaps(x, y, circles), x, y))
 
-overlap_sum = z3.Sum(z3.If(overlap, 1, 0) for overlap in overlaps)
-o.maximize(overlap_sum)
-
-check = o.check()
-assert check == z3.sat
-
-answer = o.model()[x_z3].py_value() * o.model()[y_z3].py_value()
+answer = best[1] * best[2]
 print(answer)
