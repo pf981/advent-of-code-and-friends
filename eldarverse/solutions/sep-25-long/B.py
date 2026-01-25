c8db220c291263a5dc9571a88555381f9024aa83
Paul Foster <paulfstr@gmail.com>
2026-01-25 14:08:54 +1100

    eldarverse: Complete sep-25-long B

--- eldarverse/solutions/sep-25-long/B.py
+++ eldarverse/solutions/sep-25-long/B.py
@@ -1,9 +1,32 @@
+import collections
+
+
+def get_sparse_count(n: int, k: int):
+    dp = collections.Counter()  # points -> ways
+    dp[tuple([0] * n)] = 1
+
+    for home in range(n):
+        for away in range(n):
+            if home == away:
+                continue
+
+            dp2 = collections.Counter()
+            for points, ways in dp.items():
+                dp2[points[:home] + (points[home] + 1,) + points[home + 1 :]] += ways
+                dp2[points[:away] + (points[away] + 1,) + points[away + 1 :]] += ways
+
+            dp = dp2
+
+    result = 0
+    for points, ways in dp.items():
+        if max(points) - min(points) > k:
+            result += ways
+
+    return result
+
+
 with open("./input/problem-sep-25-long-B-input.txt") as f:
     text = f.read()
-
-# text = """2
-# 2 0
-# 3 2"""
 
 n_queries, *queries = text.splitlines()
 assert int(n_queries) == len(queries)
@@ -11,33 +34,15 @@ assert int(n_queries) == len(queries)
 result = []
 for i, query in enumerate(queries, 1):
     n, k = (int(x) for x in query.split())
+    print(f"{n=} {k=}")
 
     assert 2 <= n <= 6
     assert 0 <= k <= 9
 
-    sparse_count = 0
-
-    max_permutation = (1 << (n * (n - 1))) - 1
-    for permuation in range(max_permutation + 1):
-        scores = [0] * n
-
-        for home in range(n):
-            games = permuation >> ((n - 1) * home)
-            games &= (1 << n) - 1
-            scores[home] += games.bit_count()
-
-            mask = 1
-            for away in range(n):
-                scores[n - away - 1] += (games & mask) == 0
-                if home == away:
-                    continue
-                mask <<= 1
-
-        sparse_count += max(scores) - min(scores) > k
-
+    sparse_count = get_sparse_count(n, k)
     result.append(f"Case #{i}: {sparse_count}")
 
 print("\n".join(result))
 
-# with open("./output/B.txt", "w") as f:
-#     f.write("\n".join(result))
+with open("./output/problem-sep-25-long-B.txt", "w") as f:
+    f.write("\n".join(result))
