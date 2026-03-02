79a5d6d7f07ab28a9da61291060a44ef5289ec5c
Paul Foster <paulfstr@gmail.com>
2026-03-03 00:48:11 +1100

    everybody-codes: Add skeleton for story 3

---
+++ everybody-codes/story_3/03.py
@@ -0,0 +1,25 @@
+with open("./story_3/input/everybody_codes_e3_q03_p1.txt") as f:
+    lines = f.read().splitlines()
+
+answer1 = "TODO"
+print(answer1)
+
+
+# Part 2
+
+
+with open("./story_3/input/everybody_codes_e3_q03_p2.txt") as f:
+    lines = f.read().splitlines()
+
+answer2 = "TODO"
+print(answer2)
+
+
+# Part 3
+
+
+with open("./story_3/input/everybody_codes_e3_q03_p3.txt") as f:
+    lines = f.read().splitlines()
+
+answer3 = "TODO"
+print(answer3)
