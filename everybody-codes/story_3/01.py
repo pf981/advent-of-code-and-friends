f61402ade4ea57e7520db5ad2c309d4a454b0c41
Paul Foster <paulfstr@gmail.com>
2026-03-04 10:13:31 +1100

    everybody-codes: Complete story 3 day 1

--- everybody-codes/story_3/01.py
+++ everybody-codes/story_3/01.py
@@ -1,7 +1,21 @@
+import collections
+
+
 with open("./story_3/input/everybody_codes_e3_q01_p1.txt") as f:
     lines = f.read().splitlines()
 
-answer1 = "TODO"
+answer1 = 0
+for line in lines:
+    num, rest = line.split(":")
+    r, g, b = rest.split()
+
+    r = int(r.replace("r", "0").replace("R", "1"), 2)
+    g = int(g.replace("g", "0").replace("G", "1"), 2)
+    b = int(b.replace("b", "0").replace("B", "1"), 2)
+
+    if g > r and g > b:
+        answer1 += int(num)
+
 print(answer1)
 
 
@@ -11,7 +25,19 @@ print(answer1)
 with open("./story_3/input/everybody_codes_e3_q01_p2.txt") as f:
     lines = f.read().splitlines()
 
-answer2 = "TODO"
+vals = []
+for line in lines:
+    num, rest = line.split(":")
+    r, g, b, s = rest.split()
+
+    r = int(r.replace("r", "0").replace("R", "1"), 2)
+    g = int(g.replace("g", "0").replace("G", "1"), 2)
+    b = int(b.replace("b", "0").replace("B", "1"), 2)
+    s = int(s.replace("s", "0").replace("S", "1"), 2)
+
+    vals.append((s, -(r + g + b), int(num)))
+
+answer2 = max(vals)[2]
 print(answer2)
 
 
@@ -21,5 +47,36 @@ print(answer2)
 with open("./story_3/input/everybody_codes_e3_q01_p3.txt") as f:
     lines = f.read().splitlines()
 
-answer3 = "TODO"
+counts = collections.Counter()
+id_sum = collections.Counter()
+for line in lines:
+    num, rest = line.split(":")
+    r, g, b, s = rest.split()
+
+    r = int(r.replace("r", "0").replace("R", "1"), 2)
+    g = int(g.replace("g", "0").replace("G", "1"), 2)
+    b = int(b.replace("b", "0").replace("B", "1"), 2)
+    s = int(s.replace("s", "0").replace("S", "1"), 2)
+
+    dom = None
+    if r > b and r > g:
+        dom = "r"
+    elif g > r and g > b:
+        dom = "g"
+    elif b > r and b > g:
+        dom = "b"
+    else:
+        continue
+
+    if s <= 30:
+        shine = "matte"
+    elif s >= 33:
+        shine = "shiny"
+    else:
+        continue
+
+    counts[f"{dom}-{shine}"] += 1
+    id_sum[f"{dom}-{shine}"] += int(num)
+
+answer3 = id_sum[counts.most_common(1)[0][0]]
 print(answer3)
