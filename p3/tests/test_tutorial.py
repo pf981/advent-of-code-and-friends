0e3105578ef743b9124ad9ef927381e367d50786
Paul Foster <paulfstr@gmail.com>
2026-04-05 13:44:19 +1000

    p3: Complete tutorial

--- p3/tests/test_tutorial.py
+++ p3/tests/test_tutorial.py
@@ -1,43 +1,40 @@
 from typing import List
 
-import pytest
 
-
-@pytest.mark.skip(reason="not implemented yet")
 def test_tutorial1():
     def sat(s: str):
         return "Hello " + s == "Hello world"
 
-    assert False
+    assert sat("world")
 
 
-@pytest.mark.skip(reason="not implemented yet")
 def test_tutorial2():
     def sat(s: str):
         return "Hello " + s[::-1] == "Hello world"
 
-    assert False
+    assert sat("world"[::-1])
 
 
-@pytest.mark.skip(reason="not implemented yet")
 def test_tutorial3():
     def sat(x: List[int]):
         return len(x) == 2 and sum(x) == 3
 
-    assert False
+    assert sat([1, 2])
 
 
-@pytest.mark.skip(reason="not implemented yet")
 def test_tutorial4():
     def sat(s: List[str]):
-        return len(set(s)) == 1000 and all((x.count("a") > x.count("b")) and ('b' in x) for x in s)
+        return len(set(s)) == 1000 and all(
+            (x.count("a") > x.count("b")) and ("b" in x) for x in s
+        )
 
-    assert False
+    assert sat(["a" * i + "aab" for i in range(1000)])
 
 
-@pytest.mark.skip(reason="not implemented yet")
 def test_tutorial5():
     def sat(n: int):
         return str(n * n).startswith("123456789")
 
-    assert False
+    import math
+
+    assert sat(math.isqrt(123456789_5555555555))
