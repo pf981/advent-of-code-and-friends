4663042a1bcdc8f39b256f905d0d43e301fd0967
Paul Foster <paulfstr@gmail.com>
2026-06-04 08:38:44 +1000

    practical-python: Complete week 3 part 2

---
+++ practical-python/solutions/2025/03_2.py
@@ -0,0 +1,28 @@
+import collections
+
+with open("./input/2025/03/input2.txt") as f:
+    lines = f.read().splitlines()
+
+arm = int(lines[0])
+hold = collections.deque()
+qs = collections.deque(
+    [collections.deque(c for c in line.split(",") if c) for line in lines[2:]]
+)
+for inst in lines[1].split(","):
+    d = int(inst[1:])
+    match inst[0]:
+        case "L":
+            arm -= d
+        case "R":
+            arm += d
+        case "T":
+            for _ in range(d):
+                hold.appendleft(qs[arm].popleft())
+            qs.rotate(-1)
+        case "D":
+            for _ in range(d):
+                qs[arm].append(hold.pop())
+        case _:
+            assert False
+
+print("".join(q[0] for q in qs if q))
