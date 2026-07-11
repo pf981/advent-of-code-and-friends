7281d599cb0f28c01232c26def054d6cd685cba8
Paul Foster <paulfstr@gmail.com>
2026-07-11 12:32:58 +1000

    practical-python: Tidy 2026 week 6

--- practical-python/solutions/2026/06_1.py
+++ practical-python/solutions/2026/06_1.py
@@ -1,8 +1,8 @@
-def is_cc(p1: tuple[int, int], p2: tuple[int, int], p3: tuple[int, int]) -> bool:
-    x1, y1 = p1
-    x2, y2 = p2
-    x3, y3 = p3
-    return (y2 - y1) * (x3 - x2) - (x2 - x1) * (y3 - y2) < 0
+from functools import reduce
+
+
+def is_ccw(p1, p2, p3) -> bool:
+    return (p2[1] - p1[1]) * (p3[0] - p2[0]) - (p2[0] - p1[0]) * (p3[1] - p2[1]) < 0
 
 
 with open("./input/2026/06/input1.txt") as f:
@@ -11,15 +11,15 @@ with open("./input/2026/06/input1.txt") as f:
 
 points = [tuple(map(int, line.split(","))) for line in lines]
 hull = []
-p = leftmost = min(points)
+p1 = start = min(points)
 while True:
-    hull.append(p)
-    p2 = points[0]
-    for candidate in points:
-        if p2 == p or is_cc(p, candidate, p2):
-            p2 = candidate
-    p = p2
-    if p2 == leftmost:
+    hull.append(p1)
+
+    candidates = (p for p in points if p != p1)
+    p2 = reduce(lambda best, p3: p3 if is_ccw(p1, best, p3) else best, candidates)
+
+    p1 = p2
+    if p1 == start:
         break
 
 answer = len(hull) * len(points)
