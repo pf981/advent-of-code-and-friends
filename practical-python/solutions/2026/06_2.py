a22b17b60804c7511feca197f613829a2a34a1b8
Paul Foster <paulfstr@gmail.com>
2026-07-11 12:19:57 +1000

    practical-python: Tidy 2026 week 6

--- practical-python/solutions/2026/06_2.py
+++ practical-python/solutions/2026/06_2.py
@@ -1,3 +1,10 @@
+from functools import reduce
+
+
+def is_ccw(p1, p2, p3) -> bool:
+    return (p2[1] - p1[1]) * (p3[0] - p2[0]) - (p2[0] - p1[0]) * (p3[1] - p2[1]) < 0
+
+
 with open("./input/2026/06/input2.txt") as f:
     lines = f.read().splitlines()
 
@@ -6,22 +13,18 @@ underground = {p for p, (*_, c) in zip(points, lines) if c == "U"}
 
 hull = []
 points.sort(key=lambda p: (p in underground, p))
-p1 = leftmost = points[0]
+p1 = points[0]
 outside = set()
 while True:
     hull.append(p1)
-    p2 = points[0]
-    for p3 in points:
-        if (
-            p2 == p1
-            or (p2[1] - p1[1]) * (p3[0] - p2[0]) - (p2[0] - p1[0]) * (p3[1] - p2[1]) < 0
-        ):
-            if p3 in underground:
-                outside.add(p3)
-            else:
-                p2 = p3
+
+    candidates = (p for p in points if p != p1 and p not in underground)
+    p2 = reduce(lambda best, p3: p3 if is_ccw(p1, best, p3) else best, candidates)
+
+    outside |= {p3 for p3 in underground if is_ccw(p1, p2, p3)}
+
     p1 = p2
-    if p2 == leftmost:
+    if p1 == points[0]:
         break
 
 answer = len(hull) * (len(points) - len(outside))
