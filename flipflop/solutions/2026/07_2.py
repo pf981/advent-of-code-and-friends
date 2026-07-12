5a0c5949bd089bb5067ce1a5bc7a1abd80fbeaab
Paul Foster <paulfstr@gmail.com>
2026-07-12 14:16:53 +1000

    flipflop: Complete 2026 day 7

---
+++ flipflop/solutions/2026/07_2.py
@@ -0,0 +1,45 @@
+import collections
+
+with open("./input/2026/07.txt") as f:
+    lines = f.read().splitlines()
+# lines = """>>>^>>v<^<^>>>>v<^^>vv>^<^^^^^<^<vv>^^^>
+
+# 3,0
+# 5,1
+# 5,0
+# 3,2
+# 7,1
+# 6,1
+# 7,3
+# 7,1
+# 7,6
+# 5,8
+# 5,7
+# 6,6
+# 7,9""".splitlines()
+ops, _, *lines = lines
+
+sushi = [tuple(map(int, line.split(","))) for line in lines]
+sushi.reverse()
+
+x = y = 0
+length = 1
+q = collections.deque([(0, 0)])
+# for op in ops[: len(ops) // 2]:
+# for op in ops[:2500]:
+for op in ops:
+    x += (op == ">") - (op == "<")
+    y += (op == "^") - (op == "v")
+    if sushi and (x, y) == sushi[-1]:
+        sushi.pop()
+        # eat.add((x, y))
+        length += 1
+    else:
+        q.popleft()
+    if (x, y) in q:
+        break
+    q.append((x, y))
+
+
+answer = length
+print(answer)
---
+++ flipflop/solutions/2026/07_3.py
@@ -0,0 +1,49 @@
+import collections
+
+with open("./input/2026/07.txt") as f:
+    lines = f.read().splitlines()
+# lines = """>>>^>>v<^<^>>>>v<^^>vv>^<^^^^^<^<vv>^^^>
+
+# 3,0
+# 5,1
+# 5,0
+# 3,2
+# 7,1
+# 6,1
+# 7,3
+# 7,1
+# 7,6
+# 5,8
+# 5,7
+# 6,6
+# 7,9""".splitlines()
+ops, _, *lines = lines
+
+sushi = [tuple(map(int, line.split(","))) for line in lines]
+sushi.reverse()
+
+x = y = 0
+# length = 1
+q = collections.deque([(0, 0)])
+self_eats = 0
+# for op in ops[: len(ops) // 2]:
+# for op in ops[:2500]:
+for op in ops:
+    x += (op == ">") - (op == "<")
+    y += (op == "^") - (op == "v")
+    if sushi and (x, y) == sushi[-1]:
+        sushi.pop()
+    else:
+        q.popleft()
+
+    if (x, y) in q:
+        self_eats += 1
+        while (x, y) in q:
+            q.popleft()
+        q.popleft()
+
+    q.append((x, y))
+
+
+answer = len(q) * self_eats
+print(answer)
