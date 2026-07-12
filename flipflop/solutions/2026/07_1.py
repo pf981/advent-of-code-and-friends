22774a14f1e9155827ce8f0793bfba2bfcfa95b3
Paul Foster <paulfstr@gmail.com>
2026-07-12 14:20:01 +1000

    flipflop: Tidy 2026 day 7

--- flipflop/solutions/2026/07_1.py
+++ flipflop/solutions/2026/07_1.py
@@ -1,36 +1,16 @@
 with open("./input/2026/07.txt") as f:
     lines = f.read().splitlines()
-# lines = """>>>^>>v<^<^>>>>v<^^>vv>^<^^^^^<^<vv>^^^>
 
-# 3,0
-# 5,1
-# 5,0
-# 3,2
-# 7,1
-# 6,1
-# 7,3
-# 7,1
-# 7,6
-# 5,8
-# 5,7
-# 6,6
-# 7,9""".splitlines()
-ops, _, *lines = lines
-
-sushi = [tuple(map(int, line.split(","))) for line in lines]
+ops, _, *sushi = lines
+sushi = [tuple(map(int, line.split(","))) for line in sushi]
 sushi.reverse()
 
-x = y = 0
-# eat = set()
-result = 0
-# for op in ops[: len(ops) // 2]:
-for op in ops[:2500]:
+answer = x = y = 0
+for op in ops[: len(ops) // 2]:
     x += (op == ">") - (op == "<")
     y += (op == "^") - (op == "v")
     if sushi and (x, y) == sushi[-1]:
         sushi.pop()
-        # eat.add((x, y))
-        result += 1
+        answer += 1
 
-answer = result
 print(answer)
--- flipflop/solutions/2026/07_3.py
+++ flipflop/solutions/2026/07_3.py
@@ -2,32 +2,13 @@ import collections
 
 with open("./input/2026/07.txt") as f:
     lines = f.read().splitlines()
-# lines = """>>>^>>v<^<^>>>>v<^^>vv>^<^^^^^<^<vv>^^^>
 
-# 3,0
-# 5,1
-# 5,0
-# 3,2
-# 7,1
-# 6,1
-# 7,3
-# 7,1
-# 7,6
-# 5,8
-# 5,7
-# 6,6
-# 7,9""".splitlines()
-ops, _, *lines = lines
-
-sushi = [tuple(map(int, line.split(","))) for line in lines]
+ops, _, *sushi = lines
+sushi = [tuple(map(int, line.split(","))) for line in sushi]
 sushi.reverse()
 
-x = y = 0
-# length = 1
+self_eats = x = y = 0
 q = collections.deque([(0, 0)])
-self_eats = 0
-# for op in ops[: len(ops) // 2]:
-# for op in ops[:2500]:
 for op in ops:
     x += (op == ">") - (op == "<")
     y += (op == "^") - (op == "v")
@@ -43,7 +24,6 @@ for op in ops:
         q.popleft()
 
     q.append((x, y))
-
 
 answer = len(q) * self_eats
 print(answer)
