8184cb3ab7814682aac41d09f925b22f58746deb
Paul Foster <paulfstr@gmail.com>
2026-07-18 13:55:56 +1000

    practical-python: Tidy 2026 week 7 part 2

--- practical-python/solutions/2026/07_2.py
+++ practical-python/solutions/2026/07_2.py
@@ -8,13 +8,13 @@ nums = [int(s) if s.isdigit() else None for s in text.split()]
 
 @functools.cache
 def get_max_score(i: int, used_prev: bool) -> int:
-    if i >= len(nums):
+    if i >= len(nums) or nums[i] is None:
         return 0
 
     discard = get_max_score(2 * i + 1, False) + get_max_score(2 * i + 2, False)
 
     keep = 0
-    if nums[i] is not None and not used_prev:
+    if not used_prev:
         keep = nums[i] + get_max_score(2 * i + 1, True) + get_max_score(2 * i + 2, True)
 
     return max(discard, keep)
