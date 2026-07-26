3b7c69f2ed4a45c2e27ddd9a66dc46b656eeef47
Paul Foster <paulfstr@gmail.com>
2026-07-26 21:57:26 +1000

    systems-check: Complete day 3 part 2

--- systems-check/solutions/3_2.py
+++ systems-check/solutions/3_2.py
@@ -2,22 +2,6 @@ import itertools
 
 with open("./input/3.txt") as f:
     text = f.read()
-# text = """#S X1 Y0
-# #1 X5 Y1
-# #2 X8 Y2
-# #3 X9 Y6
-# #D X9 Y9
-
-# 4,5,2,3,1"""
-# text = """#S X1 Y0
-# #1 X3 Y3
-# #2 X5 Y1
-# #3 X8 Y2
-# #4 X9 Y6
-# #5 X10 Y1
-# #D X9 Y9
-
-# 9,10,5,3,4,1,8,2,6,7"""
 
 part1, part2 = text.split("\n\n")
 
@@ -26,9 +10,14 @@ chunks = list(map(int, part2.split(",")))
 nodes = {}  # id_ -> (x, y)
 for line in part1.splitlines():
     id_, x, y = line.split()
-    id_ = id_[1:]
-    if id_ == "S":
-        id_ = "0"
+
+    if id_ == "#S":
+        id_ = 0
+    elif id_ == "#D":
+        id_ = float("inf")
+    else:
+        id_ = int(id_[1:])
+
     x = int(x[1:])
     y = int(y[1:])
 
@@ -47,13 +36,11 @@ for id_, (x, y) in nodes.items():
             m[id_].append(id2)
 
 buffers = {id_: [] for id_ in m}
-buffers["0"] = chunks
-seen = {("0", chunk) for chunk in chunks}  # {(node, chunk), ...}
+buffers[0] = chunks
+seen = {(0, chunk) for chunk in chunks}  # {(node, chunk), ...}
 from_order = sorted(buffers)
 
 while True:
-    # print(buffers)
-    # print()
     for from_ in from_order:
         to_order = sorted(
             m[from_], key=lambda id_: (dists[(from_, id_)], len(buffers[id_]), id_)
@@ -69,7 +56,6 @@ while True:
                     buffers[to].append(chunk)
                     break
             else:
-                # Put chunk back into cur node
                 new_buffer.append(chunk)
 
         if buffers[from_] != new_buffer:
@@ -78,7 +64,5 @@ while True:
     else:
         break
 
-print(buffers)
-answer = sum(buffers["D"][-5:] * len(buffers["D"]))
+answer = sum(buffers[float("inf")][-5:] * len(buffers[float("inf")]))
 print(answer)
-# 15125 incorrect
