08ba92c279eea1bfa647e83cc4b98ff555347a47
Paul Foster <paulfstr@gmail.com>
2026-08-27 08:44:53 +1000

    everybody-codes: Tidy story 4 quest 2

--- everybody-codes/story_4/02_2.py
+++ everybody-codes/story_4/02_2.py
@@ -1,57 +1,35 @@
-import re
-
 with open("./story_4/input/everybody_codes_e4_q02_p2.txt") as f:
     lines = f.read().splitlines()
-# lines = """START=[5,0]
-# A=[0,0]
-# B=[10,0]
-# C=[5,10]
-# MOVES=ABCCBABCA""".splitlines()
-# lines = """START=[5,0]
-# A=[0,0]
-# B=[10,0]
-# C=[5,10]
-# MOVES=BABCAABBCABCCCBBABCCCAAACABABCBCBBCAABBABBCACCBAABCBCBBBCBBBBBCCCAACAACB""".splitlines()
 
-start, a, b, c, moves = lines
-start = tuple(map(int, re.findall(r"-?\d+", start)))
-a = tuple(map(int, re.findall(r"-?\d+", a)))
-b = tuple(map(int, re.findall(r"-?\d+", b)))
-c = tuple(map(int, re.findall(r"-?\d+", c)))
+*nodes, moves = lines
+
 moves = moves.split("=")[1]
 
-m = {"A": a, "B": b, "C": c}
+m = {}
+for line in nodes:
+    node, pos = line.split("=")
+    x, y = map(int, pos[1:-1].split(","))
+
+    if node == "START":
+        start = x, y
+    else:
+        m[node] = (x, y)
+
 
 x, y = start
 squares = {(x, y)}
-# squares = set()
 for move in moves:
     x2, y2 = m[move]
     x = (x + x2) // 2
     y = (y + y2) // 2
     squares.add((x, y))
-    # print(f"{x=} {y=}")
 
 fire = set()
-for (
-    x,
-    y,
-) in squares:
-    fire.add((x - 1, y))
-    fire.add((x, y - 1))
-    fire.add((x, y + 1))
-    fire.add((x + 1, y))
+for x, y in squares:
+    for dx, dy in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+        if (x + dx, y + dy) in squares:
+            continue
+        fire.add((x + dx, y + dy))
 
-fire2 = set()
-for x, y in fire:
-    # if (x, y) in m.values() or (x, y) in squares:
-    if (x, y) in squares:
-        continue
-    fire2.add((x, y))
-
-answer = len(fire2)
+answer = len(fire)
 print(answer)
-# 75 wrong
-
-# Your answer length is: correct
-# The first character of your answer is: correct
--- everybody-codes/story_4/02_3.py
+++ everybody-codes/story_4/02_3.py
@@ -1,63 +1,44 @@
-import random
-import re
+import sys
+
+sys.setrecursionlimit(1_000_000)
 
 with open("./story_4/input/everybody_codes_e4_q02_p3.txt") as f:
     lines = f.read().splitlines()
-# lines = """START=[5,0]
-# A=[0,0]
-# B=[10,0]
-# C=[5,10]""".splitlines()
-# lines = """START=[0,0]
-# A=[0,0]
-# B=[80,15]
-# C=[5,30]""".splitlines()
 
-start, a, b, c = lines
-start = tuple(map(int, re.findall(r"-?\d+", start)))
-a = tuple(map(int, re.findall(r"-?\d+", a)))
-b = tuple(map(int, re.findall(r"-?\d+", b)))
-c = tuple(map(int, re.findall(r"-?\d+", c)))
+m = {}
+for line in lines:
+    node, pos = line.split("=")
+    x, y = map(int, pos[1:-1].split(","))
 
-m = {"A": a, "B": b, "C": c}
+    if node == "START":
+        start = x, y
+    else:
+        m[node] = (x, y)
 
-x, y = start
-squares = {(x, y)}
-# squares = set()
-# for move in moves:
-cnt = len(squares)
-while True:
-    for _ in range(10_000_000):
-        move = random.choice(list(m))
+squares = {start}
+
+
+def dfs(x: int, y: int) -> int:
+    result = 0
+    for move in "ABC":
         x2, y2 = m[move]
-        x = (x + x2) // 2
-        y = (y + y2) // 2
-        squares.add((x, y))
-        # print(f"{x=} {y=}")
-    if len(squares) == cnt:
-        break
-    cnt = len(squares)
+        x3 = (x + x2) // 2
+        y3 = (y + y2) // 2
+        if (x3, y3) in squares:
+            continue
+        squares.add((x3, y3))
+        result = max(result, dfs(x3, y3))
+    return 1 + result
+
+
+dfs(*start)
 
 fire = set()
-for (
-    x,
-    y,
-) in squares:
-    fire.add((x - 1, y))
-    fire.add((x, y - 1))
-    fire.add((x, y + 1))
-    fire.add((x + 1, y))
+for x, y in squares:
+    for dx, dy in [(-1, 0), (0, -1), (0, 1), (1, 0)]:
+        if (x + dx, y + dy) in squares:
+            continue
+        fire.add((x + dx, y + dy))
 
-fire2 = set()
-for x, y in fire:
-    # if (x, y) in m.values() or (x, y) in squares:
-    if (x, y) in squares:
-        continue
-    fire2.add((x, y))
-
-answer = len(fire2)
+answer = len(fire)
 print(answer)
-# 16167 wrong
-# 15799 wrong
-
-# Your answer length is: correct
-# The first character of your answer is: correct
